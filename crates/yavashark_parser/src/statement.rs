5b1682c79c62e6879263a905ae5710cdc3b98171
Shark <sharktheone@proton.me>
2024-04-14 23:06:33 +0200

    add statements

--- crates/yavashark_parser/src/statement.rs
+++ crates/yavashark_parser/src/statement.rs
@@ -1,3 +1,29 @@
+mod block;
+mod variable;
+mod expression;
+mod r#if;
+mod breakable;
+mod r#continue;
+mod r#break;
+mod r#return;
+mod with;
+mod labelled;
+mod throw;
+mod r#try;
+
+use block::BlockStatement;
+use variable::VariableStatement;
+use expression::ExpressionStatement;
+use r#if::IfStatement;
+use breakable::BreakableStatement;
+use r#continue::ContinueStatement;
+use r#break::BreakStatement;
+use r#return::ReturnStatement;
+use with::WithStatement;
+use labelled::LabelledStatement;
+use throw::ThrowStatement;
+use r#try::TryStatement;
+
 enum Statement {
     // Yield, Await, Return
     BlockStatement(BlockStatement),       // ?Yield, ?Await, ?Return
