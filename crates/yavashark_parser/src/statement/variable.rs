ed697912368756ae2f3fe1f37ab603316c839271
Shark <sharktheone@proton.me>
2024-04-16 23:41:22 +0200

    more binding and shit

--- crates/yavashark_parser/src/statement/variable.rs
+++ crates/yavashark_parser/src/statement/variable.rs
@@ -1,6 +1,9 @@
+use crate::expression::assigment::AssigmentExpression;
+use crate::identifier::Identifier;
+
 pub struct VariableStatement {
     //var
-    decl: VariableDeclarationList
+    decl: VariableDeclarationList,
 }
 
 pub struct VariableDeclarationList {
@@ -11,7 +14,7 @@ pub struct VariableDeclarationList {
 pub enum VariableDeclaration {
     Identifier(BindingIdentifier),
     IdentifierInitializer(BindingIdentifier, Initializer),
-    Pattern(Pattern, Initializer)
+    Pattern(Pattern, Initializer),
 }
 
 struct Initializer {
@@ -19,3 +22,52 @@ struct Initializer {
     expr: AssigmentExpression,
 }
 
+enum BindingIdentifier {
+    Identifier(Identifier),
+    Yield,
+    Await,
+}
+
+enum Pattern {
+    ObjectBindingPattern(ObjectBindingPattern),
+    ArrayBindingPattern(ArrayBindingPattern),
+}
+
+enum ObjectBindingPattern {
+    // { }
+    Empty,
+    // { BindingRestProperty }
+    BindingRestProperty(BindingRestProperty),
+    // { BindingPropertyList }
+    BindingPropertyList(BindingPropertyList),
+    // { BindingPropertyList , }
+    BindingPropertyListRest(BindingPropertyList, BindingRestProperty),
+}
+
+struct BindingRestProperty {
+    // ... BindingIdentifier
+    ident: BindingIdentifier,
+}
+
+struct BindingPropertyList {
+    property: BindingProperty,
+    list: Option<Box<BindingPropertyList>>,
+}
+
+enum BindingProperty {
+    // SingleNameBinding
+    SingleNameBinding(SingleNameBinding),
+    // PropertyName : BindingElement
+    PropertyNameBindingElement(PropertyName, BindingElement),
+}
+
+struct SingleNameBinding {
+    // BindingIdentifier
+    ident: BindingIdentifier,
+    // Initializer
+    init: Initializer,
+}
+
+struct PropertyName {
+//TODO
+}
