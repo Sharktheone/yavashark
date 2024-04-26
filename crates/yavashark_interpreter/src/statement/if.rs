6622046125a2ce01856a374b4c3286eeab10d8d4
Shark <sharktheone@proton.me>
2024-04-26 12:57:10 +0200

    implement if

--- crates/yavashark_interpreter/src/statement/if.rs
+++ crates/yavashark_interpreter/src/statement/if.rs
@@ -6,6 +6,14 @@ use crate::scope::Scope;
 
 impl Context {
     pub fn run_if(&mut self, stmt: &IfStmt, scope: &mut Scope) -> Result<Value, Error> {
-        todo!()
+        let test = self.run_expr(&stmt.test, stmt.span, scope)?;
+        
+        if test.is_truthy() {
+            self.run_statement(&stmt.cons, scope)
+        } else if let Some(alt) = &stmt.alt {
+            self.run_statement(alt, scope)
+        } else {
+            Ok(Value::Undefined)
+        }
     }
 }
