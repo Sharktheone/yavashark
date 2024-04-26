3552f24ee523feb2a4f29d8801a19c071eac1681
Shark <sharktheone@proton.me>
2024-04-26 11:04:51 +0200

    change some scoping things

--- crates/yavashark_interpreter/src/scope.rs
+++ crates/yavashark_interpreter/src/scope.rs
@@ -1,25 +1,60 @@
 use std::collections::HashMap;
 use yavashark_value::Value;
+use std::cell::RefCell;
+use std::ptr::NonNull;
+use std::rc::Rc;
 
-pub struct Scope<'ctx> {
-    parent: Option<&'ctx mut Scope<'ctx>>,
-    variables: HashMap<String, Value>,
+
+pub struct Scope {
+    scope: Rc<NonNull<UnsafeScope>>,
 }
 
 
+struct UnsafeScope {
+    parent: Option<Rc<NonNull<UnsafeScope>>>,
+    variables: HashMap<String, Value>,
+}
 
-impl<'a> Scope<'a> {
-    pub fn new() -> Self {
-        Self {
+impl UnsafeScope {
+    pub fn new() -> NonNull<Self>{
+        let ptr = Box::new(Self {
             parent: None,
             variables: HashMap::new(),
+        });
+
+        //Box::into_raw always returns a non-null pointer (except it was created unsafely, which is not the case here)
+        NonNull::new(Box::into_raw(ptr)).expect("inserted known good pointer into NonNull but it apparently was null")
+    }
+    
+    pub fn with_parent(parent: Rc<NonNull<UnsafeScope>>) -> NonNull<Self> {
+        let ptr = Box::new(Self {
+            parent: Some(parent),
+            variables: HashMap::new(),
+        });
+        
+        //Box::into_raw always returns a non-null pointer (except it was created unsafely, which is not the case here)
+        NonNull::new(Box::into_raw(ptr)).expect("inserted known good pointer into NonNull but it apparently was null")
+    }
+
+}
+
+
+impl Scope {
+    pub fn new() -> Self {
+        Self {
+            scope: Rc::new(UnsafeScope::new()),
         }
     }
     
-    pub fn with_parent(parent: &'a mut Scope<'a>) -> Self {
+    pub fn with_parent(parent: &Scope) -> Self {
         Self {
-            parent: Some(parent),
-            variables: HashMap::new(),
+            scope: Rc::new(UnsafeScope::with_parent(Rc::clone(&parent.scope))),
+        }
+    }
+    
+    pub fn clone(scope: &Self) -> Self {
+        Self {
+            scope: Rc::clone(&scope.scope),
         }
     }
 }
