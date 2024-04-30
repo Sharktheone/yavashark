9fc5e705ac8c3058e8689af48ecb258d9b4f1f15
Shark <sharktheone@proton.me>
2024-04-30 08:54:18 +0200

    return stmt

--- crates/yavashark_interpreter/src/statement/return.rs
+++ crates/yavashark_interpreter/src/statement/return.rs
@@ -1,12 +1,18 @@
 use crate::context::Context;
 use crate::scope::Scope;
-use crate::RuntimeResult;
+use crate::{ControlFlow, RuntimeResult};
 use crate::Value;
 use swc_ecma_ast::ReturnStmt;
 use yavashark_value::error::Error;
 
 impl Context {
     pub fn run_return(&mut self, stmt: &ReturnStmt, scope: &mut Scope) -> RuntimeResult {
-        todo!()
+        let value = if let Some(arg) = &stmt.arg {
+            self.run_expr(arg, stmt.span, scope)?
+        } else {
+            Value::Undefined
+        };
+
+        Err(ControlFlow::Return(value))
     }
 }
