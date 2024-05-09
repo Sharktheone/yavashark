9d421d2cf659db3b7c7d808da3bcc664179cb955
Shark <sharktheone@proton.me>
2024-05-09 22:54:29 +0200

    if tests

--- crates/yavashark_interpreter/src/statement/if.rs
+++ crates/yavashark_interpreter/src/statement/if.rs
@@ -18,3 +18,145 @@ impl Context {
         }
     }
 }
+
+
+
+#[cfg(test)]
+mod tests {
+    use crate::{test_eval, Value};
+
+    #[test]
+    fn run_if_true() {
+        test_eval!(
+            r#"
+            if (true) {
+                mock.values(1);
+            }
+            "#,
+            0,
+            vec![vec![Value::Number(1.0)]],
+            Value::Undefined
+        );
+    }
+    
+    #[test]
+    fn run_if_false() {
+        test_eval!(
+            r#"
+            if (false) {
+                mock.values(1);
+            }
+            "#,
+            0,
+            Vec::<Vec<Value>>::new(),
+            Value::Undefined
+        );
+    }
+    
+    #[test]
+    fn run_if_else() {
+        test_eval!(
+            r#"
+            if (false) {
+                mock.values(1);
+            } else {
+                mock.values(2);
+            };
+            "#,
+            0,
+            vec![vec![Value::Number(2.0)]],
+            Value::Undefined
+        );
+    }
+    
+    #[test]
+    fn run_if_else_if() {
+        test_eval!(
+            r#"
+            if (false) {
+                mock.values(1);
+            } else if (true) {
+                mock.values(2);
+            };
+            "#,
+            0,
+            vec![vec![Value::Number(2.0)]],
+            Value::Undefined
+        );
+    }
+    
+    #[test]
+    fn run_if_else_if_else() {
+        test_eval!(
+            r#"
+            if (false) {
+                mock.values(1);
+            } else if (false) {
+                mock.values(2);
+            } else {
+                mock.values(3);
+            };
+            "#,
+            0,
+            vec![vec![Value::Number(3.0)]],
+            Value::Undefined
+        );
+    }
+    
+    #[test]
+    fn run_if_truthy_number() {
+        test_eval!(
+            r#"
+            if (1) {
+                mock.values(1);
+            }
+            "#,
+            0,
+            vec![vec![Value::Number(1.0)]],
+            Value::Undefined
+        );
+    }
+    
+    #[test]
+    fn run_if_falsy_number() {
+        test_eval!(
+            r#"
+            if (0) {
+                mock.values(1);
+            }
+            "#,
+            0,
+            Vec::<Vec<Value>>::new(),
+            Value::Undefined
+        );
+    }
+    
+    #[test]
+    fn run_if_truthy_string() {
+        test_eval!(
+            r#"
+            if ("") {
+                mock.values(1);
+            }
+            "#,
+            0,
+            Vec::<Vec<Value>>::new(),
+            Value::Undefined
+        );
+    }
+    
+    #[test]
+    fn run_if_falsy_string() {
+        test_eval!(
+            r#"
+            if ("a") {
+                mock.values(1);
+            }
+            "#,
+            0,
+            vec![vec![Value::Number(1.0)]],
+            Value::Undefined
+        );
+    }
+    
+}
