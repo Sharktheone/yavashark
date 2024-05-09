06aef3cf7cff19434eb725ebd4db5dd9a03b7b41
Shark <sharktheone@proton.me>
2024-05-09 22:47:57 +0200

    return tests

--- crates/yavashark_interpreter/src/statement/return.rs
+++ crates/yavashark_interpreter/src/statement/return.rs
@@ -20,3 +20,88 @@ impl Context {
         Err(ControlFlow::Return(value))
     }
 }
+
+
+
+#[cfg(test)]
+mod tests {
+    use crate::{test_eval, Value};
+    
+    #[test]
+    fn run_return() {
+        test_eval!(
+            r#"
+            function foo(){
+                return 1;
+            }
+            foo();
+            "#,
+            0,
+            Vec::<Vec<Value>>::new(),
+            Value::Number(1.0)
+        );
+    }
+    
+    #[test]
+    fn run_return_undefined() {
+        test_eval!(
+            r#"
+            function foo(){
+                return;
+            }
+            foo();
+            "#,
+            0,
+            Vec::<Vec<Value>>::new(),
+            Value::Undefined
+        );
+    }
+    
+    #[test]
+    fn run_return_no_return() {
+        test_eval!(
+            r#"
+            function foo(){
+                1;
+            }
+            foo();
+            "#,
+            0,
+            Vec::<Vec<Value>>::new(),
+            Value::Undefined
+        );
+    }
+    
+    #[test]
+    fn run_return_no_return_null() {
+        test_eval!(
+            r#"
+            function foo(){
+                null;
+            }
+            foo();
+            "#,
+            0,
+            Vec::<Vec<Value>>::new(),
+            Value::Undefined
+        );
+    }
+    
+    #[test]
+    fn run_return_no_return_undefined() {
+        test_eval!(
+            r#"
+            function foo(){
+                mock.send();
+                return 1;
+                mock.send();
+            }
+            foo();
+            "#,
+            1,
+            Vec::<Vec<Value>>::new(),
+            Value::Number(1.0)
+        );
+    }
+    
+}
