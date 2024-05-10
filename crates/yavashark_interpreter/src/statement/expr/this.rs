932dc84030d86d36ba1f10b2258e235617ba787a
Shark <sharktheone@proton.me>
2024-05-10 02:27:07 +0200

    remove debug print in this

--- crates/yavashark_interpreter/src/statement/expr/this.rs
+++ crates/yavashark_interpreter/src/statement/expr/this.rs
@@ -8,7 +8,6 @@ use swc_ecma_ast::ThisExpr;
 impl Context {
     pub fn run_this(&mut self, stmt: &ThisExpr, scope: &mut Scope) -> RuntimeResult {
         let this = scope.this.copy();
-        println!("this: {}", scope.this);
         Ok(scope.this.copy())
     }
 }
