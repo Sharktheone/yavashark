2e8f4673163d1152bdfcf98aff9e7d415134b844
Shark <sharktheone@proton.me>
2024-05-15 02:55:12 +0200

    implement rest of iter

--- crates/yavashark_interpreter/src/object/array.rs
+++ crates/yavashark_interpreter/src/object/array.rs
@@ -3,10 +3,11 @@
 use std::any::Any;
 use std::cell::RefCell;
 use std::rc::Rc;
-use yavashark_macro::{object, properties};
-use yavashark_value::Iter;
 
-use crate::{Context, Error, Value, ValueResult, Variable};
+use yavashark_macro::{object, properties};
+use yavashark_value::{Iter, Obj};
+
+use crate::{Context, Error, ObjectHandle, Value, ValueResult, Variable};
 use crate::object::Object;
 use crate::Symbol;
 
@@ -18,7 +19,6 @@ pub struct Array {}
 #[properties]
 impl Array {
     #[new]
-    #[allow(clippy::unnecessary_wraps)]
     fn new(ctx: &mut Context) -> Result<Self, Error> {
         Ok(Self {
             object: Object::raw_with_proto(ctx.proto.array_prototype.clone().into())
@@ -31,12 +31,30 @@ impl Array {
     }
 
     #[prop(Symbol::ITERATOR)]
-    fn iterator(&self, args: Vec<Value>) -> ValueResult {
+    fn iterator(&self, args: Vec<Value>, ctx: &mut Context, this: Value) -> ValueResult {
+        let obj = match this {
+            Value::Object(obj) => {
+                obj
+            }
+
+            Value::Function(func) => {
+                todo!("Function iterator")
+            }
+
+            _ => return Err(Error::ty_error(format!("Expected object, found {:?}", this)))
+        };
+
+        let iter = ArrayIterator {
+            object: Object::raw(ctx),
+            inner: obj.into(),
+            next: 0,
+            done: false,
+        };
+
         todo!()
     }
 
     #[constructor]
-    #[allow(clippy::unnecessary_wraps)]
     fn construct(&mut self, args: Vec<Value>) -> ValueResult {
         let values = args.into_iter().enumerate()
             .map(|(i, v)| {
@@ -54,39 +72,48 @@ impl Array {
 #[object]
 #[allow(clippy::module_name_repetitions)]
 pub struct ArrayIterator {
-    inner: Rc<RefCell<Array>>,
+    inner: ObjectHandle,
     next: usize,
-    done: bool
+    done: bool,
 }
-
 
 
 #[properties]
 impl ArrayIterator {
-    #[prop]
+    #[prop(requires(ctx))]
     pub fn next(&mut self, args: Vec<Value>, ctx: &mut Context) -> ValueResult {
-        
         if self.done {
             let obj = Object::new(ctx);
             obj.define_property("value".into(), Value::Undefined);
             obj.define_property("done".into(), Value::Boolean(true));
             return Ok(obj.into());
         }
-        
-        let value = self.inner.get_array_or_done(self.next).map(|v| (v.0.copy(), v.1));
+
+        let inner = self.inner.get()?;
+
+        let (done, value) = inner.get_array_or_done(self.next);
+
+        if done {
+            self.done = true;
+            let obj = Object::new(ctx);
+            obj.define_property("value".into(), Value::Undefined);
+            obj.define_property("done".into(), Value::Boolean(true));
+            return Ok(obj.into());
+        }
+
         self.next += 1;
-        
+
         let value = if let Some(value) = value {
             value
         } else {
             self.done = true;
             Value::Undefined
         };
-        
+
         let obj = Object::new(ctx);
         obj.define_property("value".into(), value);
         obj.define_property("done".into(), Value::Boolean(self.done));
-        
+
         Ok(obj.into())
     }
 }
