2f1386817165218ce98164db1d1346f3db7aba4a
Shark <sharktheone@proton.me>
2024-05-21 23:16:46 +0200

    object literal

--- crates/yavashark_interpreter/src/statement/expr/object.rs
+++ crates/yavashark_interpreter/src/statement/expr/object.rs
@@ -1,12 +1,31 @@
+use swc_ecma_ast::{ObjectLit, PropOrSpread};
+
 use crate::context::Context;
-use crate::scope::Scope;
-use crate::Error;
+use crate::object::Object;
 use crate::RuntimeResult;
+use crate::scope::Scope;
 use crate::Value;
-use swc_ecma_ast::ObjectLit;
 
 impl Context {
     pub fn run_object(&mut self, stmt: &ObjectLit, scope: &mut Scope) -> RuntimeResult {
-        todo!()
+        let mut obj = Object::new(self);
+
+        for prop in &stmt.props {
+            match prop {
+                PropOrSpread::Spread(spread) => {
+                    let expr = self.run_expr(&spread.expr, spread.dot3_token, scope)?;
+
+                    if let Ok(props) = expr.properties() {
+                        for (name, value) in props {
+                            obj.define_property(name, value);
+                        }
+                    }
+                }
+
+                PropOrSpread::Prop(prop) => {}
+            }
+        }
+
+        Ok(Value::Object(obj))
     }
 }
