cd014b09ec5d92c3f87decdd34aa1826a3815e68
Shark <sharktheone@proton.me>
2024-05-22 15:33:25 +0200

    array literal

--- crates/yavashark_interpreter/src/statement/expr/array.rs
+++ crates/yavashark_interpreter/src/statement/expr/array.rs
@@ -4,9 +4,35 @@ use crate::Error;
 use crate::RuntimeResult;
 use crate::Value;
 use swc_ecma_ast::ArrayLit;
+use yavashark_value::Obj;
+use crate::object::array::Array;
 
 impl Context {
     pub fn run_array(&mut self, stmt: &ArrayLit, scope: &mut Scope) -> RuntimeResult {
-        todo!()
+        let mut arr = Array::new(self)?;
+        
+        for elem in &stmt.elems {
+            if let Some(elem) = elem {
+                if let Some(spread) = elem.spread {
+                    let iter = self.run_expr(&elem.expr, spread, scope)?;
+                    
+                    let mut iter = iter.iter(self)?;
+                    for value in iter {
+                        arr.push(value?);
+                    }
+                    
+                    
+                    
+                } else {
+                    let value = self.run_expr(&elem.expr, stmt.span, scope)?;
+                    arr.push(value);
+                }
+                
+            } else {
+                arr.push(Value::Undefined);
+            }
+        }
+        
+        Ok(arr.into_value())
     }
 }
