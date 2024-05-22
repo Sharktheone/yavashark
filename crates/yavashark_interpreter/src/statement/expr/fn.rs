5ddb52c795d66f4d812d0cff7b57897c1fcac878
Shark <sharktheone@proton.me>
2024-05-22 23:24:00 +0200

    fn expr

--- crates/yavashark_interpreter/src/statement/expr/fn.rs
+++ crates/yavashark_interpreter/src/statement/expr/fn.rs
@@ -1,12 +1,26 @@
 use crate::context::Context;
 use crate::scope::Scope;
-use crate::Error;
+use crate::{Error, JSFunction};
 use crate::RuntimeResult;
 use crate::Value;
 use swc_ecma_ast::FnExpr;
 
 impl Context {
     pub fn run_fn(&mut self, stmt: &FnExpr, scope: &mut Scope) -> RuntimeResult {
-        todo!()
+        let mut fn_scope = Scope::with_parent(scope);
+
+        fn_scope.state_set_function();
+
+        let name = stmt.ident.as_ref().map_or("anonymous".to_string(), |i| i.sym.to_string());
+        
+        let function = JSFunction::new(
+            name,
+            stmt.function.params.clone(),
+            stmt.function.body.clone(),
+            fn_scope,
+            self,
+        );
+
+        Ok(Value::Function(function))
     }
 }
