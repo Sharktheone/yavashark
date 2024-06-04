5ff6a3af73fa4192ce0d2cb45d4e8ad749d9964c
Shark <sharktheone@proton.me>
2024-06-04 11:19:52 +0200

    function proto functions initialize

--- crates/yavashark_interpreter/src/context/prototypes.rs
+++ crates/yavashark_interpreter/src/context/prototypes.rs
@@ -23,33 +23,52 @@ impl Prototypes {
 
         let func_prototype =
             ObjectHandle::new(FunctionPrototype::new(&obj_prototype.clone().into()));
-
+        
         {
             let mut obj =
                 obj_prototype
                     .get_mut()
                     .map_err(|e| anyhow!(format!("{e:?}")))?;
-
+        
             let obj = obj.as_any_mut();
-
+        
             let proto = obj
                 .downcast_mut::<Prototype>()
                 .ok_or_else(|| anyhow!("downcast_mut::<Prototype> failed"))?;
-
+        
             proto.initialize(func_prototype.clone().into());
         }
+        {
+            let mut func =
+                func_prototype
+                    .get_mut()
+                    .map_err(|e| anyhow!(format!("{e:?}")))?;
 
+            let func = func.as_any_mut();
+
+            let func = func
+                .downcast_mut::<FunctionPrototype>()
+                .ok_or_else(|| anyhow!("downcast_mut::<Prototype> failed"))?;
+
+            func.initialize(func_prototype.clone().into());
+        }
+        
+        
+        thread::sleep(Duration::from_secs(20));
+        
         let array_prototype = Array::initialize_proto(
             Object::raw_with_proto(obj_prototype.clone().into()),
             func_prototype.clone().into(),
         )
         .map_err(|e| anyhow!(format!("{e:?}")))?;
-
+        
         let array_iter_prototype = ArrayIterator::initialize_proto(
             Object::raw_with_proto(obj_prototype.clone().into()),
             func_prototype.clone().into(),
         )
         .map_err(|e| anyhow!(format!("{e:?}")))?;
+        
+        
 
         Ok(Self {
             obj: obj_prototype,
