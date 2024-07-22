79794d9d74b5499edc96d6d58f2f7ffcb9bf2f74
Shark <sharktheone@proton.me>
2024-07-22 22:53:45 +0200

    pull in bincode

--- crates/yavashark_bytecode/src/writer.rs
+++ crates/yavashark_bytecode/src/writer.rs
@@ -1,8 +1,8 @@
 use std::io::Write;
+use bincode::Encode;
 
-/// Own bytecode format, one compact one and one "normal" one
-/// we probably also should use `bincode` so we have something that definetely works...
-pub trait BytecodeWriter {
+/// Own bytecode format, one compact one and one "normal" one, it can also make use of the bincode crate
+pub trait BytecodeWriter: Encode {
     fn write(&self, buffer: &mut impl Write);
 
     fn write_compact(&self, buffer: &mut impl Write);
