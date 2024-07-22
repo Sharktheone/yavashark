5ed0d69e34821f5492177738ba960c46fbf65f70
Shark <sharktheone@proton.me>
2024-07-22 22:54:00 +0200

    add bytecode headers!

--- crates/yavashark_bytecode/src/writer.rs
+++ crates/yavashark_bytecode/src/writer.rs
@@ -5,5 +5,18 @@ use bincode::Encode;
 pub trait BytecodeWriter: Encode {
     fn write(&self, buffer: &mut impl Write);
 
-    fn write_compact(&self, buffer: &mut impl Write);
+    fn write_tight(&self, buffer: &mut impl Write);
 }
+
+
+
+
+
+///adc2-243d
+const NORMAL_HEADER: [u8; 8] = *b"adc2243d";
+
+
+///9961-c49c
+const TIGHT_HEADER: [u8; 8] = *b"9961c49c";
+//Who can find out for what these stand for? Open an issue if you do! (It's a hidden message!)
+
