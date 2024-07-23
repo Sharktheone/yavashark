f7f55cf082b25eb70e24795e884692896dc542a3
Shark <sharktheone@proton.me>
2024-07-23 19:35:35 +0200

    move bytecode and add bytecode struct

--- crates/yavashark_bytecode/src/lib.rs
+++ crates/yavashark_bytecode/src/lib.rs
@@ -4,71 +4,19 @@
 //!
 //!
 
+pub use instructions::*;
+use yavashark_env::Value;
+
 pub mod writer;
+mod instructions;
 
 pub type VarName = u32;
 pub type ConstIdx = u32;
 pub type Reg = u8;
-pub type Lbl = u32;
+pub type JmpOffset = u32;
 
-#[repr(u16)]
-pub enum Instruction {
-    Add(VarName, VarName),
-    AddAcc(f64),
-    AddStatic(VarName, f64),
-    Sub(VarName, VarName),
-    SubAcc(f64),
-    SubStatic(VarName, f64),
-    Div(VarName, VarName),
-    DivAcc(f64),
-    DivStatic(VarName, f64),
-    Mul(VarName, VarName),
-    MulAcc(f64),
-    MulStatic(VarName, f64),
-    LNot(VarName),
-    LNotAcc,
-    LOr(VarName, VarName),
-    LOrAcc(Reg),
-    LAnd(VarName, VarName),
-    LAndAcc(Reg),
-    LXor(VarName, VarName),
-    LXorAcc(Reg),
-    Dec(VarName),
-    DecAcc,
-    Inc(VarName),
-    IncAcc,
-    PushScope,
-    PopScope,
-    Call(VarName),
-    CallReg(Reg),
-    CallAcc,
-    Jmp(Lbl),
-    JmpRel(i32),
-    JmpIf(VarName, Lbl),
-    JmpIfAcc(Lbl),
-    JmpIfRelAcc(i32),
-    JmpIfNot(VarName, Lbl),
-    JmpIfNotAcc(Lbl),
-    JmpIfNotRelAcc(i32),
-    JmpNull(VarName, Lbl),
-    JmpNullAcc(Lbl),
-    JmpNullRelAcc(i32),
-    JmpUndef(VarName, Lbl),
-    JmpUndefAcc(Lbl),
-    JmpUndefRelAcc(i32),
-    JmpNullUndef(VarName, Lbl),
-    JmpNullUndefAcc(Lbl),
-    JmpNullUndefRelAcc(i32),
-    Str(VarName, ConstIdx),
-    StrAcc(ConstIdx),
-    LoadMemberAcc(VarName, VarName),
-    LoadMemberReg(VarName, VarName, Reg),
-    For,
-    TypeOf(VarName, VarName),
-    TypeOfAcc(VarName),
-    InstanceOf(VarName, VarName),
-    Return,
-    Break,
-    Continue,
-    Throw,
+pub struct Bytecode {
+    pub instructions: Vec<Instruction>,
+    pub consts: Vec<Value>,
+    pub vars: Vec<String>,
 }
