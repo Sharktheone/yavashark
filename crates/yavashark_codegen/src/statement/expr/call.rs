5043eb46efc0d68d28bb3369d00783ca01746f9c
Shark <sharktheone@proton.me>
2024-07-30 21:39:49 +0200

    compile call

--- crates/yavashark_codegen/src/statement/expr/call.rs
+++ crates/yavashark_codegen/src/statement/expr/call.rs
@@ -1,8 +1,19 @@
-use swc_ecma_ast::CallExpr;
+use swc_ecma_ast::{Callee, CallExpr};
+use yavashark_bytecode::Instruction;
 use crate::{ByteCodegen, Res};
 
 impl ByteCodegen {
     pub fn compile_call(&mut self, stmt: &CallExpr) -> Res {
-        todo!()
+        let Callee::Expr(expr) = &stmt.callee else {
+            todo!()
+        };
+        
+        self.compile_expr(expr, stmt.span)?;
+        
+        self.instructions.push(Instruction::CallAcc); //TODO: how can we push the args here?
+        
+        Ok(())
+        
+        
     }
 }
