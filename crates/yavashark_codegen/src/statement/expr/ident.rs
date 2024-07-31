c63499ed80178bab683b265bf4dd8dfb2a5032ec
Shark <sharktheone@proton.me>
2024-07-31 23:53:05 +0200

    implement ident

--- crates/yavashark_codegen/src/statement/expr/ident.rs
+++ crates/yavashark_codegen/src/statement/expr/ident.rs
@@ -1,8 +1,14 @@
 use swc_ecma_ast::Ident;
+use yavashark_bytecode::Instruction;
 use crate::{ByteCodegen, Res};
 
 impl ByteCodegen {
     pub fn compile_ident(&mut self, stmt: &Ident) -> Res {
-        todo!()
+        
+        let var_name = self.allocate_variable(stmt.sym.as_str().to_string());
+        
+        self.instructions.push(Instruction::LoadEnv(var_name));
+        
+        Ok(())
     }
 }
