93920eba44e6fd75f211fc3ef97526ea89dcfef3
Shark <sharktheone@proton.me>
2024-08-01 23:38:49 +0200

    compile if

--- crates/yavashark_codegen/src/statement/if.rs
+++ crates/yavashark_codegen/src/statement/if.rs
@@ -1,8 +1,32 @@
 use crate::{ByteCodegen, Res};
 use swc_ecma_ast::{ExprStmt, IfStmt};
+use yavashark_bytecode::Instruction;
 
 impl ByteCodegen {
     pub fn compile_if(&mut self, stmt: &IfStmt) -> Res {
-        todo!()
+        let IfStmt {
+            test,
+            cons,
+            alt,
+            ..
+        } = stmt;
+
+        self.compile_expr(test, stmt.span)?;
+
+        let idx = self.instructions.len();
+        self.instructions.push(Instruction::JmpIfNotAcc(0));
+
+        self.compile_statement(cons)?;
+        
+
+        if let Some(alt) = alt {
+            self.instructions.push(Instruction::Jmp(0));
+            self.instructions[idx] = Instruction::JmpIfNotAcc(self.instructions.len() as i32);
+            self.compile_statement(alt)?;
+        } else {
+            self.instructions[idx] = Instruction::JmpIfNotAcc(self.instructions.len() as i32);
+        }
+
+        Ok(())
     }
 }
