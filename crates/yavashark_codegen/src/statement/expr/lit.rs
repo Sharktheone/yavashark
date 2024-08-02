a0172c5a7072a0ba425996de6e2d25d04c08f3e0
Shark <sharktheone@proton.me>
2024-08-02 04:04:31 +0200

    implement lits

--- crates/yavashark_codegen/src/statement/expr/lit.rs
+++ crates/yavashark_codegen/src/statement/expr/lit.rs
@@ -1,8 +1,26 @@
+use anyhow::anyhow;
 use crate::{ByteCodegen, Res};
 use swc_ecma_ast::Lit;
+use yavashark_bytecode::ConstValue;
 
 impl ByteCodegen {
     pub fn compile_lit(&mut self, stmt: &Lit) -> Res {
-        todo!()
+        let val = match stmt {
+            Lit::Str(s) => ConstValue::String(s.value.to_string()),
+            Lit::Num(n) => ConstValue::Number(n.value),
+            Lit::Bool(b) => ConstValue::Boolean(b.value),
+            Lit::Null(_) => ConstValue::Null,
+            Lit::BigInt(_) => todo!("Big int lit"),
+            Lit::Regex(_) => todo!("Regex lit"),
+            Lit::JSXText(_) => Err(anyhow!("JSXText is not supported"))?,
+        };
+        
+        let lit = self.allocate_literal(val);
+        
+        self.instructions.push(yavashark_bytecode::Instruction::LdaAcc(lit));
+        
+        Ok(())
+        
+        
     }
 }
