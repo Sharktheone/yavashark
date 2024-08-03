58495847cfb42a08c8d99aeacf91a3fb1df33f81
Shark <sharktheone@proton.me>
2024-08-03 20:44:39 +0200

    continue instruction

--- crates/yavashark_codegen/src/statement/continue.rs
+++ crates/yavashark_codegen/src/statement/continue.rs
@@ -1,8 +1,10 @@
 use crate::{ByteCodegen, Res};
 use swc_ecma_ast::{ContinueStmt, ExprStmt};
+use yavashark_bytecode::Instruction;
 
 impl ByteCodegen {
     pub fn compile_continue(&mut self, stmt: &ContinueStmt) -> Res {
-        todo!()
+        self.instructions.push(Instruction::Continue);
+        Ok(())
     }
 }
