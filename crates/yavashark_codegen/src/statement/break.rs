0b4a8309121ea6e742eba857b55b219881a7f201
Shark <sharktheone@proton.me>
2024-08-03 20:44:32 +0200

    break instruction

--- crates/yavashark_codegen/src/statement/break.rs
+++ crates/yavashark_codegen/src/statement/break.rs
@@ -1,9 +1,10 @@
 use swc_ecma_ast::BreakStmt;
-
+use yavashark_bytecode::Instruction;
 use crate::{ByteCodegen, Res};
 
 impl ByteCodegen {
     pub fn compile_break(&mut self, stmt: &BreakStmt) -> Res {
-        todo!()
+        self.instructions.push(Instruction::Break);
+        Ok(())
     }
 }
