1218f653d387c2bcfbd0aa6a89712b0b0783c628
Shark <sharktheone@proton.me>
2024-08-07 23:35:59 +0200

    binary expr

--- crates/yavashark_codegen/src/statement/expr/bin.rs
+++ crates/yavashark_codegen/src/statement/expr/bin.rs
@@ -1,8 +1,33 @@
 use crate::{ByteCodegen, Res};
-use swc_ecma_ast::{BinExpr, ThisExpr};
+use swc_ecma_ast::{BinaryOp, BinExpr, ThisExpr};
+use yavashark_bytecode::Instruction;
 
 impl ByteCodegen {
     pub fn compile_bin(&mut self, stmt: &BinExpr) -> Res {
-        todo!()
+        self.compile_expr(&stmt.right, stmt.span)?;
+        self.instructions.push(Instruction::PushAcc):
+        
+        self.compile_expr(&stmt.left, stmt.span)?;
+        
+        self.instructions.push(Instruction::PopToReg(0));
+        
+        
+        
+        
+        match stmt.op {
+            BinaryOp::Add => self.instructions.push(Instruction::AddAccReg(0)),
+            BinaryOp::Sub => self.instructions.push(Instruction::SubAccReg(0)),
+            BinaryOp::Mul => self.instructions.push(Instruction::MulAccReg(0)),
+            BinaryOp::Div => self.instructions.push(Instruction::DivAccReg(0)),
+            BinaryOp::Mod => todo!(),
+            BinaryOp::BitOr => self.instructions.push(Instruction::LOrAcc(0)),
+            BinaryOp::BitXor => self.instructions.push(Instruction::LXorAcc(0)),
+            BinaryOp::BitAnd => self.instructions.push(Instruction::LAndAcc(0)),
+            
+            o => todo!("{:?}", o),
+        }
+        
+        
+        Ok(())
     }
 }
