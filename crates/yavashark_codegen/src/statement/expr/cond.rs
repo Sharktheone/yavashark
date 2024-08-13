f843578629859ecaf7ff47fdf963424fb1778386
Shark <sharktheone@proton.me>
2024-08-13 20:04:08 +0200

    compile cond expr

--- crates/yavashark_codegen/src/statement/expr/cond.rs
+++ crates/yavashark_codegen/src/statement/expr/cond.rs
@@ -1,8 +1,29 @@
 use crate::{ByteCodegen, Res};
 use swc_ecma_ast::CondExpr;
+use yavashark_bytecode::Instruction;
 
 impl ByteCodegen {
     pub fn compile_cond(&mut self, stmt: &CondExpr) -> Res {
-        todo!()
+        let CondExpr { test, cons, alt, .. } = stmt;
+        
+        self.compile_expr(test, stmt.span)?;
+        
+        let idx = self.instructions.len();
+        self.instructions.push(Instruction::JmpIfNotAccRel(1));
+        
+        self.compile_expr(cons, stmt.span)?;
+        
+        
+        let idx2 = self.instructions.len();
+        self.instructions.push(Instruction::Jmp(0));
+        
+        self.instructions[idx] = Instruction::JmpIfNotAccRel(self.instructions.len() as isize - idx as isize);
+        
+        self.compile_expr(alt, stmt.span)?;
+        
+        self.instructions[idx2] = Instruction::JmpRel(self.instructions.len() as isize - idx2 as isize);
+        
+        
+        Ok(())
     }
 }
