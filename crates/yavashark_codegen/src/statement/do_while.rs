9bd603931a8f8d15ce46249f1796e276197fa688
Shark <sharktheone@proton.me>
2024-08-14 23:32:21 +0200

    compile do while loops

--- crates/yavashark_codegen/src/statement/do_while.rs
+++ crates/yavashark_codegen/src/statement/do_while.rs
@@ -1,8 +1,18 @@
 use crate::{ByteCodegen, Res};
 use swc_ecma_ast::{DoWhileStmt, ExprStmt};
+use yavashark_bytecode::Instruction;
 
 impl ByteCodegen {
     pub fn compile_do_while(&mut self, stmt: &DoWhileStmt) -> Res {
-        todo!()
+        let idx = self.instructions.len();
+
+        self.compile_statement(&stmt.body)?;
+
+        self.compile_expr(&stmt.test, stmt.span)?;
+
+        self.instructions.push(Instruction::JmpIfAccRel(idx as isize - self.instructions.len() as isize));
+
+
+        Ok(())
     }
 }
