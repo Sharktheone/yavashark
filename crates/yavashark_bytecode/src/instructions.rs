95f65535420bea45874e6a3052a57c0043bf36a4
Shark <sharktheone@proton.me>
2024-08-15 23:28:37 +0200

    this instructions

--- crates/yavashark_bytecode/src/instructions.rs
+++ crates/yavashark_bytecode/src/instructions.rs
@@ -183,4 +183,8 @@ pub enum Instruction {
     ThrowAcc,
     ThrowReg,
     Throw(VarName),
+    
+    
+    ThisAcc,
+    ThisReg(Reg),
 }
--- crates/yavashark_codegen/src/statement/expr/this.rs
+++ crates/yavashark_codegen/src/statement/expr/this.rs
@@ -1,8 +1,11 @@
 use crate::{ByteCodegen, Res};
 use swc_ecma_ast::ThisExpr;
+use yavashark_bytecode::Instruction;
 
 impl ByteCodegen {
     pub fn compile_this(&mut self, stmt: &ThisExpr) -> Res {
-        todo!()
+        self.instructions.push(Instruction::ThisAcc):
+
+        Ok(())
     }
 }
