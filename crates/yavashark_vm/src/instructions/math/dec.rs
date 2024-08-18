aa7cf3040890951f4b19de171883ee8ea8ac4e7d
Shark <sharktheone@proton.me>
2024-08-18 23:38:57 +0200

    implement dec

---
+++ crates/yavashark_vm/src/instructions/math/dec.rs
@@ -0,0 +1,27 @@
+use yavashark_bytecode::{Reg, VarName};
+use crate::VM;
+
+pub fn dec(name: VarName, vm: &mut VM) {
+    let val = vm.get_variable(name);
+
+    let result = val - 1.into();
+
+    vm.set_acc(result);
+}
+
+pub fn dec_acc(vm: &mut VM) {
+    let acc = vm.acc();
+
+    let result = acc - 1.into();
+
+    vm.set_acc(result);
+}
+
+pub fn dec_reg(name:Reg, vm: &mut VM) {
+    let val = vm.get_register(name);
+
+    let result = val - 1.into();
+
+    vm.set_acc(result);
+}
+
