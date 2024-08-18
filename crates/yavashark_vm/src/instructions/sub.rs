11f3b3146ef9c32d4f5a0f6d90a225e164fd7dc2
Shark <sharktheone@proton.me>
2024-08-18 22:30:30 +0200

    fix sub

--- crates/yavashark_vm/src/instructions/sub.rs
+++ crates/yavashark_vm/src/instructions/sub.rs
@@ -5,7 +5,7 @@ pub fn sub(lhs: VarName, rhs: VarName, vm: &mut VM) {
     let lhs = vm.get_variable(lhs);
     let rhs = vm.get_variable(rhs);
 
-    let result = lhs + rhs;
+    let result = lhs - rhs;
 
     vm.set_acc(result);
 }
@@ -15,7 +15,7 @@ pub fn sub_acc_reg(reg: Reg, vm: &mut VM) {
     let acc = vm.acc();
     let reg = vm.get_register(reg);
 
-    let result = acc + reg;
+    let result = acc - reg;
 
     vm.set_acc(result);
 }
@@ -24,7 +24,7 @@ pub fn sub_reg(reg1: Reg, reg2: Reg, vm: &mut VM) {
     let reg1 = vm.get_register(reg1);
     let reg2 = vm.get_register(reg2);
 
-    let result = reg1 + reg2;
+    let result = reg1 - reg2;
 
     vm.set_acc(result);
 }
