2199f27520beebc40064c0070c7bd1927be28ded
Shark <sharktheone@proton.me>
2024-08-18 23:34:50 +0200

    move "math" operations

---
+++ crates/yavashark_vm/src/instructions/math.rs
@@ -0,0 +1,19 @@
+// "mathematical" instructions
+
+
+mod add;
+mod sub;
+mod mul;
+mod div;
+mod dec;
+mod inc;
+mod mod_rem;
+
+
+pub use add::*;
+pub use sub::*;
+pub use mul::*;
+pub use div::*;
+pub use mod_rem::*;
+pub use dec::*;
+pub use inc::*;
