e0b5e71a9e9d7ec3f1e89f93ee3662d0019849c7
Shark <sharktheone@proton.me>
2024-08-18 23:39:01 +0200

    implement inc

---
+++ crates/yavashark_vm/src/instructions/math/inc.rs
@@ -0,0 +1,26 @@
+use yavashark_bytecode::{Reg, VarName};
+use crate::VM;
+
+pub fn inc(name: VarName, vm: &mut VM) {
+    let val = vm.get_variable(name);
+
+    let result = val + 1.into();
+
+    vm.set_acc(result);
+}
+
+pub fn inc_acc(vm: &mut VM) {
+    let acc = vm.acc();
+
+    let result = acc + 1.into();
+
+    vm.set_acc(result);
+}
+
+pub fn inc_reg(name: Reg, vm: &mut VM) {
+    let val = vm.get_register(name);
+
+    let result = val + 1.into();
+
+    vm.set_acc(result);
+}
