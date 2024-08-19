f9d2214ee01cde4ba924466f1f21b3e95d19fdac
Shark <sharktheone@proton.me>
2024-08-19 10:23:10 +0200

    group bitwhise instructions

---
+++ crates/yavashark_vm/src/instructions/bit.rs
@@ -0,0 +1,15 @@
+mod and;
+mod or;
+mod xor;
+mod lshift;
+mod rshift;
+mod zero_fill_rshift;
+
+
+
+pub use and::*;
+pub use or::*;
+pub use xor::*;
+pub use lshift::*;
+pub use rshift::*;
+pub use zero_fill_rshift::*;
