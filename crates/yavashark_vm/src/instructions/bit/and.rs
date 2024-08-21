f9e794525724535f5fcb549f2b0c10ddb3ed0722
Shark <sharktheone@proton.me>
2024-08-21 22:25:26 +0200

    bit and

--- crates/yavashark_vm/src/instructions/bit/and.rs
+++ crates/yavashark_vm/src/instructions/bit/and.rs
@@ -1,1 +1,24 @@
+use yavashark_bytecode::{Reg, VarName};
+use crate::VM;
 
+pub fn bitwise_and(lhs: VarName, rhs: VarName, vm: &mut VM) {
+    let lhs = vm.get_variable(lhs);
+    let rhs = vm.get_variable(rhs);
+
+    vm.set_acc(lhs & rhs);
+}
+
+pub fn bitwise_and_acc(reg: Reg, vm: &mut  VM) {
+    let rhs = vm.get_register(reg);
+    let lhs = vm.acc();
+
+    vm.set_acc(lhs & rhs);
+}
+
+
+pub fn bitwise_and_reg(rhs: Reg, lhs: Reg, vm: &mut  VM) {
+    let rhs = vm.get_register(rhs);
+    let lhs = vm.get_register(lhs);
+
+    vm.set_acc(lhs & rhs);
+}
