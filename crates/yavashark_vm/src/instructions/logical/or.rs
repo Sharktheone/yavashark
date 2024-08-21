b3592f3835722415e6e6c5a2578b7961db244fe8
Shark <sharktheone@proton.me>
2024-08-21 19:36:42 +0200

    logical or

--- crates/yavashark_vm/src/instructions/logical/or.rs
+++ crates/yavashark_vm/src/instructions/logical/or.rs
@@ -1,1 +1,16 @@
+use yavashark_bytecode::{Reg, VarName};
+use crate::VM;
 
+pub fn logical_or(lhs: VarName, rhs: VarName, vm: &mut VM) {
+    let lhs = vm.get_variable(lhs);
+    let rhs = vm.get_variable(rhs);
+
+    vm.set_acc(lhs.log_or(rhs));
+}
+
+pub fn logical_or_acc(reg: Reg, vm: &mut  VM) {
+    let rhs = vm.get_register(reg);
+    let lhs = vm.acc();
+
+    vm.set_acc(lhs.log_or(rhs));
+}
