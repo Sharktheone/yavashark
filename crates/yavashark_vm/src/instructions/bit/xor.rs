d8381b6daa346350d267abd6fd14f83a08af919a
Shark <sharktheone@proton.me>
2024-08-21 22:27:52 +0200

    bit xor

--- crates/yavashark_vm/src/instructions/bit/xor.rs
+++ crates/yavashark_vm/src/instructions/bit/xor.rs
@@ -1,1 +1,25 @@
+use yavashark_bytecode::{Reg, VarName};
 
+use crate::VM;
+
+pub fn bitwise_xor(lhs: VarName, rhs: VarName, vm: &mut VM) {
+    let lhs = vm.get_variable(lhs);
+    let rhs = vm.get_variable(rhs);
+
+    vm.set_acc(lhs ^ rhs);
+}
+
+pub fn bitwise_xor_acc(reg: Reg, vm: &mut VM) {
+    let rhs = vm.get_register(reg);
+    let lhs = vm.acc();
+
+    vm.set_acc(lhs ^ rhs);
+}
+
+
+pub fn bitwise_xor_reg(rhs: Reg, lhs: Reg, vm: &mut VM) {
+    let rhs = vm.get_register(rhs);
+    let lhs = vm.get_register(lhs);
+
+    vm.set_acc(lhs ^ rhs);
+}
