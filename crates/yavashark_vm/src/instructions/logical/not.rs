648d2d21bf580712cf695e44a7a4f0fee0f41c56
Shark <sharktheone@proton.me>
2024-08-21 19:35:44 +0200

    logical not

--- crates/yavashark_vm/src/instructions/logical/not.rs
+++ crates/yavashark_vm/src/instructions/logical/not.rs
@@ -1,1 +1,13 @@
+use yavashark_bytecode::VarName;
+use crate::VM;
 
+pub fn logical_not(name: VarName, vm: &mut VM) {
+    let value = vm.get_variable(name);
+    
+    
+    vm.set_acc(!value);
+}   
+
+pub fn logical_not_acc(vm: &mut VM) {
+    vm.set_acc(!vm.acc());
+}
