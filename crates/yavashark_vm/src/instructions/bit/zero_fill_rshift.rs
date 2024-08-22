f52449aec523483230154e3afee957d2791f38e4
Shark <sharktheone@proton.me>
2024-08-22 21:13:43 +0200

    implement zero fill rshift for bytecode

--- crates/yavashark_vm/src/instructions/bit/zero_fill_rshift.rs
+++ crates/yavashark_vm/src/instructions/bit/zero_fill_rshift.rs
@@ -1,1 +1,25 @@
+use yavashark_bytecode::{Reg, VarName};
 
+use crate::VM;
+
+pub fn zero_fill_rshift(lhs: VarName, rhs: VarName, vm: &mut VM) {
+    let lhs = vm.get_variable(lhs);
+    let rhs = vm.get_variable(rhs);
+
+    vm.set_acc(lhs.zero_fill_rshift(&rhs));
+}
+
+pub fn zero_fill_rshift_acc(reg: Reg, vm: &mut VM) {
+    let rhs = vm.get_register(reg);
+    let lhs = vm.acc();
+
+    vm.set_acc(lhs.zero_fill_rshift(&rhs));
+}
+
+
+pub fn zero_fill_rshift_reg(rhs: Reg, lhs: Reg, vm: &mut VM) {
+    let rhs = vm.get_register(rhs);
+    let lhs = vm.get_register(lhs);
+
+    vm.set_acc(lhs.zero_fill_rshift(&rhs));
+}
