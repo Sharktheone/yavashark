11d857b3a79a6dbb9f71939d3d426525ebee051f
Shark <sharktheone@proton.me>
2024-08-23 20:48:50 +0200

    gt instruction

--- crates/yavashark_vm/src/instructions/cmp/gt.rs
+++ crates/yavashark_vm/src/instructions/cmp/gt.rs
@@ -1,1 +1,28 @@
 
+
+use yavashark_bytecode::{Reg, VarName};
+
+use crate::VM;
+
+pub fn gt(lhs: VarName, rhs: VarName, vm: &mut VM) {
+    let lhs = vm.get_variable(lhs);
+    let rhs = vm.get_variable(rhs);
+
+    vm.set_acc((lhs > rhs).into());
+}
+
+pub fn gt_acc(reg: Reg, vm: &mut VM) {
+    let rhs = vm.get_register(reg);
+    let lhs = vm.acc();
+
+    vm.set_acc((lhs > rhs).into());
+}
+
+pub fn gt_reg(rhs: Reg, lhs: Reg, vm: &mut VM) {
+    let rhs = vm.get_register(rhs);
+    let lhs = vm.get_register(lhs);
+
+    vm.set_acc((lhs > rhs).into());
+}
+
+
