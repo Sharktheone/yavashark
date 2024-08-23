eb81fa99feed4cfac68c53e1d72cd617f4852231
Shark <sharktheone@proton.me>
2024-08-23 20:55:05 +0200

    eqeq instruction

--- crates/yavashark_vm/src/instructions/cmp/eqeq.rs
+++ crates/yavashark_vm/src/instructions/cmp/eqeq.rs
@@ -1,1 +1,27 @@
 
+use yavashark_bytecode::{Reg, VarName};
+
+use crate::VM;
+
+pub fn eq_eq(lhs: VarName, rhs: VarName, vm: &mut VM) {
+    let lhs = vm.get_variable(lhs);
+    let rhs = vm.get_variable(rhs);
+
+    vm.set_acc((lhs == rhs).into());
+}
+
+pub fn eq_eq_acc(reg: Reg, vm: &mut VM) {
+    let rhs = vm.get_register(reg);
+    let lhs = vm.acc();
+
+    vm.set_acc((lhs == rhs).into());
+}
+
+pub fn eq_eq_reg(rhs: Reg, lhs: Reg, vm: &mut VM) {
+    let rhs = vm.get_register(rhs);
+    let lhs = vm.get_register(lhs);
+
+    vm.set_acc((lhs == rhs).into());
+}
+
+
