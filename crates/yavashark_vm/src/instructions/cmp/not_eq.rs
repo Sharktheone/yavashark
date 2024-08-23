21173647afd9f5cfc34f4437e0d69286428896f3
Shark <sharktheone@proton.me>
2024-08-23 20:59:20 +0200

    fix normal eq

--- crates/yavashark_vm/src/instructions/cmp/not_eq.rs
+++ crates/yavashark_vm/src/instructions/cmp/not_eq.rs
@@ -7,21 +7,21 @@ pub fn not_eq(lhs: VarName, rhs: VarName, vm: &mut VM) {
     let lhs = vm.get_variable(lhs);
     let rhs = vm.get_variable(rhs);
 
-    vm.set_acc((lhs != rhs).into());
+    vm.set_acc(!lhs.normal_eq(&rhs).into());
 }
 
 pub fn not_eq_acc(reg: Reg, vm: &mut VM) {
     let rhs = vm.get_register(reg);
     let lhs = vm.acc();
 
-    vm.set_acc((lhs != rhs).into());
+    vm.set_acc(!lhs.normal_eq(&rhs).into());
 }
 
 pub fn not_eq_reg(rhs: Reg, lhs: Reg, vm: &mut VM) {
     let rhs = vm.get_register(rhs);
     let lhs = vm.get_register(lhs);
 
-    vm.set_acc((lhs != rhs).into());
+    vm.set_acc(!lhs.normal_eq(&rhs).into());
 }
 
 
