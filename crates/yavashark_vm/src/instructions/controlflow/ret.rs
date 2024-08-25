f3890edc45cf3bf379f9095e32db886bfebb6958
Shark <sharktheone@proton.me>
2024-08-25 23:53:33 +0200

    implement return

--- crates/yavashark_vm/src/instructions/controlflow/ret.rs
+++ crates/yavashark_vm/src/instructions/controlflow/ret.rs
@@ -1,1 +1,24 @@
+use yavashark_bytecode::{Reg, VarName};
+use yavashark_env::{ControlFlow, ControlResult};
+use yavashark_env::value::Value;
+use crate::VM;
 
+pub fn return_() -> ControlResult {
+    Err(ControlFlow::Return(Value::Undefined))
+}
+
+pub fn return_acc(vm: &mut VM) -> ControlResult {
+    let value = vm.acc();
+    Err(ControlFlow::Return(value))
+}
+
+pub fn return_reg(reg: Reg, vm: &mut VM) -> ControlResult {
+    let value = vm.get_register(reg);
+    Err(ControlFlow::Return(value))
+}
+
+
+pub fn return_var(var: VarName, vm: &mut VM) -> ControlResult {
+    let value = vm.get_variable(var);
+    Err(ControlFlow::Return(value))
+}
