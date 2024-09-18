df2ea8daa497e39ff962425cc8a440c18db94878
Shark <sharktheone@proton.me>
2024-09-18 23:41:02 +0200

    fix exp

--- crates/yavashark_vm/src/instructions/math/exp.rs
+++ crates/yavashark_vm/src/instructions/math/exp.rs
@@ -1,23 +1,42 @@
 use crate::VM;
 use yavashark_bytecode::{Reg, VarName};
+use yavashark_env::Res;
 
-pub fn exp(lhs: VarName, rhs: VarName, vm: &mut VM) {
+pub fn exp(lhs: VarName, rhs: VarName, vm: &mut VM) -> Res {
     let lhs = vm.get_variable(lhs);
     let rhs = vm.get_variable(rhs);
+    
+    let ctx = vm.get_context();
+    
+    let res = lhs.pow(&rhs, ctx)?;
 
-    vm.set_acc(lhs.pow(&rhs));
+    vm.set_acc(res);
+    
+    Ok(())
 }
 
-pub fn exp_acc(reg: Reg, vm: &mut VM) {
+pub fn exp_acc(reg: Reg, vm: &mut VM) -> Res {
     let acc = vm.acc();
     let reg = vm.get_register(reg);
+    
+    let ctx = vm.get_context();
+    
+    let res = acc.pow(&reg, ctx)?;
 
-    vm.set_acc(acc.pow(&reg));
+    vm.set_acc(res);
+    
+    Ok(())
 }
 
-pub fn exp_reg(reg1: Reg, reg2: Reg, vm: &mut VM) {
+pub fn exp_reg(reg1: Reg, reg2: Reg, vm: &mut VM) -> Res {
     let reg1 = vm.get_register(reg1);
     let reg2 = vm.get_register(reg2);
+    
+    let ctx = vm.get_context();
+    
+    let res = reg1.pow(&reg2, ctx)?;
 
-    vm.set_acc(reg1.pow(&reg2));
+    vm.set_acc(res);
+    
+    Ok(())
 }
