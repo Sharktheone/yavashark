238c70a4e471024a1f80eeac5c5d2e9c087d6b19
Shark <sharktheone@proton.me>
2024-09-19 21:43:18 +0200

    implement reg and stack functionality

--- crates/yavashark_vm/src/regs.rs
+++ crates/yavashark_vm/src/regs.rs
@@ -1,7 +1,26 @@
-use yavashark_env::Value;
+use yavashark_bytecode::Reg;
+use yavashark_env::{Res, Value};
+use yavashark_env::value::Error;
 
 pub const NUM_REGS: usize = 32;
 
 pub struct Registers {
     regs: [Value; NUM_REGS],
+}
+
+
+impl Registers {
+    pub fn new() -> Self {
+        Self {
+            regs: [const { Value::Undefined }; NUM_REGS],
+        }
+    }
+
+    pub fn get(&self, reg: Reg) -> Option<Value> {
+        self.regs.get(reg as usize).cloned()
+    }
+
+    pub fn set(&mut self, reg: Reg, value: Value) -> Res {
+        self.regs.get_mut(reg as usize).map(|r| *r = value).ok_or(Error::new("Invalid register"))
+    }
 }
--- crates/yavashark_vm/src/stack.rs
+++ crates/yavashark_vm/src/stack.rs
@@ -3,3 +3,46 @@ use yavashark_env::Value;
 pub struct Stack {
     stack: Vec<Value>,
 }
+
+
+
+impl Stack {
+    pub fn new() -> Self {
+        Self {
+            stack: Vec::new(),
+        }
+    }
+
+    pub fn push(&mut self, value: Value) {
+        self.stack.push(value);
+    }
+
+    pub fn pop(&mut self) -> Option<Value> {
+        self.stack.pop()
+    }
+
+    pub fn peek(&self) -> Option<&Value> {
+        self.stack.last()
+    }
+    
+    pub fn get(&self, idx: usize) -> Option<&Value> {
+        self.stack.get(idx)
+    }
+    
+    
+    pub fn pop_n(&mut self, n: usize) -> Vec<Value> {
+        let mut values = Vec::with_capacity(n);
+        for _ in 0..n {
+            if let Some(value) = self.pop() {
+                values.push(value);
+            } else {
+                break;
+            }
+        }
+        values
+    }
+
+    pub fn len(&self) -> usize {
+        self.stack.len()
+    }
+}
