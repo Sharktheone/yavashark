0727a9fa78795fc11e3d9daf77faa0a85f00a59b
Shark <sharktheone@proton.me>
2024-09-20 21:34:18 +0200

    add run method to vm and test

--- crates/yavashark_vm/src/lib.rs
+++ crates/yavashark_vm/src/lib.rs
@@ -6,12 +6,13 @@ mod storage;
 mod value_ext;
 mod data;
 
+use crate::data::DataSection;
 pub use regs::*;
 pub use stack::*;
 use yavashark_bytecode::Instruction;
-use yavashark_env::{Context, Res, Value};
 use yavashark_env::scope::Scope;
-use crate::data::DataSection;
+use yavashark_env::{Context, Res, Value};
+use crate::execute::Execute;
 
 pub struct VM {
     regs: Registers,
@@ -22,7 +23,7 @@ pub struct VM {
     data: DataSection,
 
     current_scope: Scope,
-    
+
     acc: Value,
 
     ctx: Context,
@@ -52,5 +53,44 @@ impl VM {
     pub fn offset_pc(&mut self, offset: isize) {
         // pc won't be above isize::MAX, since this is `Vec`'s length limit
         self.pc = (self.pc as isize + offset) as usize;
+    }
+    
+    pub fn run(&mut self) -> Res {
+        while self.pc < self.code.len() {
+            let instr = self.code[self.pc].clone();
+            self.pc += 1;
+
+            instr.execute(self)?;
+        }
+        
+        Ok(())
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use yavashark_bytecode::{ConstValue, Instruction};
+
+    #[test]
+    fn test_vm() {
+        let ctx = Context::new().unwrap();
+
+        let mut vm = VM {
+            regs: Registers::new(),
+            stack: Stack::new(),
+            pc: 0,
+            code: vec![Instruction::LoadEnv(0), Instruction::LoadMemberAcc(1), Instruction::CallAcc(1), Instruction::LdaAcc(0), Instruction::JmpIfNotAccRel(5), Instruction::LoadEnv(2), Instruction::LoadMemberAcc(3), Instruction::CallAcc(1), Instruction::JmpRel(4), Instruction::LoadEnv(4), Instruction::LoadMemberAcc(5), Instruction::CallAcc(1)],
+            data: DataSection {
+                var_names: vec!["console".to_string(), "log".to_string(), "console".to_string(), "log".to_string(), "console".to_string(), "log".to_string()],
+                constants: vec![ConstValue::Boolean(true)],
+            },
+            current_scope: Scope::new(&ctx),
+            acc: Value::Undefined,
+            ctx,
+        };
+        
+        vm.run().unwrap()
     }
 }
