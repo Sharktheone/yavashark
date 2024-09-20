b34d31f69fc2514b42ca168e5f705c67fd6e9ebb
Shark <sharktheone@proton.me>
2024-09-20 21:47:38 +0200

    fix add

--- crates/yavashark_vm/src/instructions/math/add.rs
+++ crates/yavashark_vm/src/instructions/math/add.rs
@@ -1,29 +1,38 @@
 use crate::VM;
 use yavashark_bytecode::{Reg, VarName};
+use yavashark_env::Res;
 
-pub fn add(lhs: VarName, rhs: VarName, vm: &mut VM) {
-    let lhs = vm.get_variable(lhs);
-    let rhs = vm.get_variable(rhs);
+pub fn add(lhs: VarName, rhs: VarName, vm: &mut VM) -> Res {
+    let lhs = vm.get_variable(lhs)?;
+    let rhs = vm.get_variable(rhs)?;
 
     let result = lhs + rhs;
 
     vm.set_acc(result);
+    
+    
+    Ok(())
 }
 
-pub fn add_acc_reg(reg: Reg, vm: &mut VM) {
+pub fn add_acc_reg(reg: Reg, vm: &mut VM) -> Res {
     let acc = vm.acc();
-    let reg = vm.get_register(reg);
+    let reg = vm.get_register(reg)?;
 
     let result = acc + reg;
 
     vm.set_acc(result);
+    
+    Ok(())
 }
 
-pub fn add_reg(reg1: Reg, reg2: Reg, vm: &mut VM) {
-    let reg1 = vm.get_register(reg1);
-    let reg2 = vm.get_register(reg2);
+pub fn add_reg(reg1: Reg, reg2: Reg, vm: &mut VM) -> Res {
+    let reg1 = vm.get_register(reg1)?;
+    let reg2 = vm.get_register(reg2)?;
 
     let result = reg1 + reg2;
 
     vm.set_acc(result);
+    
+    
+    Ok(())
 }
