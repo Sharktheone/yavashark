a64ee6f0ec5ddde2f174805b8c9495183e8f647c
Shark <sharktheone@proton.me>
2024-09-20 21:47:01 +0200

    first fix: bitwise and

--- crates/yavashark_vm/src/instructions/bit/and.rs
+++ crates/yavashark_vm/src/instructions/bit/and.rs
@@ -1,23 +1,30 @@
 use crate::VM;
 use yavashark_bytecode::{Reg, VarName};
+use yavashark_env::Res;
 
-pub fn bitwise_and(lhs: VarName, rhs: VarName, vm: &mut VM) {
-    let lhs = vm.get_variable(lhs);
-    let rhs = vm.get_variable(rhs);
+pub fn bitwise_and(lhs: VarName, rhs: VarName, vm: &mut VM) -> Res {
+    let lhs = vm.get_variable(lhs)?;
+    let rhs = vm.get_variable(rhs)?;
 
     vm.set_acc(lhs & rhs);
+    
+    Ok(())
 }
 
-pub fn bitwise_and_acc(reg: Reg, vm: &mut VM) {
-    let rhs = vm.get_register(reg);
+pub fn bitwise_and_acc(reg: Reg, vm: &mut VM) -> Res {
+    let rhs = vm.get_register(reg)?;
     let lhs = vm.acc();
 
     vm.set_acc(lhs & rhs);
+    
+    Ok(())
 }
 
-pub fn bitwise_and_reg(rhs: Reg, lhs: Reg, vm: &mut VM) {
-    let rhs = vm.get_register(rhs);
-    let lhs = vm.get_register(lhs);
+pub fn bitwise_and_reg(rhs: Reg, lhs: Reg, vm: &mut VM) -> Res {
+    let rhs = vm.get_register(rhs)?;
+    let lhs = vm.get_register(lhs)?;
 
     vm.set_acc(lhs & rhs);
+    
+    Ok(())
 }
