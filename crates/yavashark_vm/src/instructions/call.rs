72f2ec109142f8319c8515a6b5eedd6b2b5d093b
Shark <sharktheone@proton.me>
2024-09-22 21:37:05 +0200

    fix call

--- crates/yavashark_vm/src/instructions/call.rs
+++ crates/yavashark_vm/src/instructions/call.rs
@@ -4,9 +4,9 @@ use yavashark_bytecode::{Reg, VarName};
 use yavashark_env::ControlResult;
 
 pub fn call(num_args: u16, var_name: VarName, vm: &mut VM) -> ControlResult {
-    let func = vm.get_variable(var_name);
+    let func = vm.get_variable(var_name)?;
 
-    let this = vm.get_this();
+    let this = vm.get_this()?;
 
     let args = vm.get_args(num_args);
 
@@ -18,9 +18,9 @@ pub fn call(num_args: u16, var_name: VarName, vm: &mut VM) -> ControlResult {
 }
 
 pub fn call_reg(num_args: u16, reg: Reg, vm: &mut VM) -> ControlResult {
-    let func = vm.get_register(reg);
+    let func = vm.get_register(reg)?;
 
-    let this = vm.get_this();
+    let this = vm.get_this()?;
 
     let args = vm.get_args(num_args);
 
@@ -34,7 +34,7 @@ pub fn call_reg(num_args: u16, reg: Reg, vm: &mut VM) -> ControlResult {
 pub fn call_acc(num_args: u16, vm: &mut VM) -> ControlResult {
     let func = vm.acc();
 
-    let this = vm.get_this();
+    let this = vm.get_this()?;
 
     let args = vm.get_args(num_args);
 
@@ -46,10 +46,10 @@ pub fn call_acc(num_args: u16, vm: &mut VM) -> ControlResult {
 }
 
 pub fn call_member(num_args: u16, target: VarName, member: VarName, vm: &mut VM) -> ControlResult {
-    let value = vm.get_variable(target);
+    let value = vm.get_variable(target)?;
     let func = value.get_member(member, vm)?;
 
-    let this = vm.get_this();
+    let this = vm.get_this()?;
 
     let args = vm.get_args(num_args);
 
@@ -61,10 +61,10 @@ pub fn call_member(num_args: u16, target: VarName, member: VarName, vm: &mut VM)
 }
 
 pub fn call_member_reg(num_args: u16, target: Reg, member: VarName, vm: &mut VM) -> ControlResult {
-    let value = vm.get_register(target);
+    let value = vm.get_register(target)?;
     let func = value.get_member(member, vm)?;
 
-    let this = vm.get_this();
+    let this = vm.get_this()?;
 
     let args = vm.get_args(num_args);
 
@@ -79,7 +79,7 @@ pub fn call_member_acc(num_args: u16, member: VarName, vm: &mut VM) -> ControlRe
     let value = vm.acc();
     let func = value.get_member(member, vm)?;
 
-    let this = vm.get_this();
+    let this = vm.get_this()?;
 
     let args = vm.get_args(num_args);
 
