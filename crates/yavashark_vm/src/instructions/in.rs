859c43ea242ff384c9bacf8798450c49192c80c4
Shark <sharktheone@proton.me>
2024-09-22 21:38:07 +0200

    fix in

--- crates/yavashark_vm/src/instructions/in.rs
+++ crates/yavashark_vm/src/instructions/in.rs
@@ -3,8 +3,8 @@ use yavashark_bytecode::{Reg, VarName};
 use yavashark_env::ControlResult;
 
 pub fn in_(lhs: VarName, rhs: VarName, vm: &mut VM) -> ControlResult {
-    let lhs = vm.get_variable(lhs);
-    let rhs = vm.get_variable(rhs);
+    let lhs = vm.get_variable(lhs)?;
+    let rhs = vm.get_variable(rhs)?;
 
     vm.set_acc(rhs.contains_key(&lhs)?.into());
 
@@ -12,7 +12,7 @@ pub fn in_(lhs: VarName, rhs: VarName, vm: &mut VM) -> ControlResult {
 }
 
 pub fn in_acc(reg: Reg, vm: &mut VM) -> ControlResult {
-    let rhs = vm.get_register(reg);
+    let rhs = vm.get_register(reg)?;
     let lhs = vm.acc();
 
     vm.set_acc(rhs.contains_key(&lhs)?.into());
@@ -21,8 +21,8 @@ pub fn in_acc(reg: Reg, vm: &mut VM) -> ControlResult {
 }
 
 pub fn in_reg(rhs: Reg, lhs: Reg, vm: &mut VM) -> ControlResult {
-    let rhs = vm.get_register(rhs);
-    let lhs = vm.get_register(lhs);
+    let rhs = vm.get_register(rhs)?;
+    let lhs = vm.get_register(lhs)?;
 
     vm.set_acc(rhs.contains_key(&lhs)?.into());
 
