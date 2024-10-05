ce73f0b10957825b93399d1e5537c34ce5b4fee9
Shark <sharktheone@proton.me>
2024-10-05 23:49:33 +0200

    add error object

--- crates/yavashark_env/src/error.rs
+++ crates/yavashark_env/src/error.rs
@@ -1,15 +1,27 @@
 use yavashark_macro::object;
 
 use crate::context::Context;
-use crate::{Error, NativeFunction, Value};
+use crate::{ControlFlow, Error, NativeFunction, Object, ObjectHandle, Value};
 
 pub fn get_error(ctx: &Context) -> Value {
     NativeFunction::special(
         "error",
-        |args, _, _| {
-            let message = args.first().map(std::string::ToString::to_string);
+        |args, this, ctx| {
 
-            Ok(message.unwrap_or("<error>".to_string()).into()) //TODO: Error object
+            let Value::Object(obj) = this else {
+                return Err(Error::ty_error("Error must be called as a constructor".to_string()));
+            };
+            
+            
+            let Ok(obj) = obj.get_mut() else {
+                return Err(Error::ty_error("Error must be called as a constructor".to_string()));
+            };
+
+            let message = args.first().map(std::string::ToString::to_string).unwrap_or("".to_string());
+            
+            let err = ErrorObj::new_from(message, ctx);
+            
+            Ok(err.into())
         },
         ctx,
     )
@@ -19,6 +31,27 @@ pub fn get_error(ctx: &Context) -> Value {
 #[object]
 #[derive(Debug)]
 #[allow(dead_code)]
-struct ErrorObj {
+pub struct ErrorObj {
     error: Error,
+}
+
+
+impl ErrorObj {
+    pub fn new(error: Error, ctx: &Context) -> ObjectHandle {
+        let this = Self {
+            object: Object::raw(ctx),
+            error,
+        };
+        
+        ObjectHandle::new(this)
+    }
+
+    pub fn new_from(message: String, ctx: &Context) -> ObjectHandle {
+        let this = Self {
+            object: Object::raw(ctx),
+            error: Error::unknown_error(message),
+        };
+        
+        ObjectHandle::new(this)
+    }
 }
