1bbbce54ba43c3c0224968a97316e51c66034c9c
Shark <sharktheone@proton.me>
2024-10-06 21:48:42 +0200

    fix error constructor (once again)

--- crates/yavashark_env/src/error.rs
+++ crates/yavashark_env/src/error.rs
@@ -1,31 +1,22 @@
-use yavashark_macro::object;
-
 use crate::context::Context;
-use crate::{ControlFlow, Error, NativeFunction, Object, ObjectHandle, Result, Value};
+use crate::{Error, NativeFunction, Object, ObjectHandle, Result, Value};
+use yavashark_macro::object;
 
 pub fn get_error(ctx: &Context) -> Value {
     NativeFunction::special(
         "error",
         |args, this, ctx| {
-
-            let Value::Object(obj) = this else {
-                return Err(Error::ty_error("Error must be called as a constructor".to_string()));
-            };
-
-
-            let Ok(obj) = obj.get_mut() else {
-                return Err(Error::ty_error("Error must be called as a constructor".to_string()));
-            };
-
             let message = args.first().map(std::string::ToString::to_string).unwrap_or("".to_string());
 
-            let err = ErrorObj::new_from(message, ctx);
-
-            Ok(err.into())
+            let err = ErrorObj::raw_from(message, ctx);
+            
+            this.exchange(Box::new(err))?;
+            
+            Ok(Value::Undefined)
         },
         ctx,
     )
-    .into()
+        .into()
 }
 
 #[object(to_string)]
@@ -54,11 +45,19 @@ impl ErrorObj {
 
         ObjectHandle::new(this)
     }
-    
+
+
+    pub fn raw_from(message: String, ctx: &Context) -> Self {
+        Self {
+            object: Object::raw(ctx),
+            error: Error::unknown_error(message),
+        }
+    }
+
     pub fn override_to_string(&self, _: &mut Context) -> Result<String> {
         Ok(self.error.to_string())
     }
-    
+
     pub fn override_to_string_internal(&self) -> String {
         self.error.to_string()
     }
