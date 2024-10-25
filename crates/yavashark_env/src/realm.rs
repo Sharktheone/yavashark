4f63f5da41bf424ff926f478f0fd4f48cbcb64c8
Shark <sharktheone@proton.me>
2024-10-25 23:03:41 +0200

    realm constructor

--- crates/yavashark_env/src/realm.rs
+++ crates/yavashark_env/src/realm.rs
@@ -1,7 +1,7 @@
 mod intrinsics;
 mod env;
 
-use crate::ObjectHandle;
+use crate::{Object, ObjectHandle};
 use crate::realm::env::Environment;
 use crate::realm::intrinsics::Intrinsics;
 
@@ -9,4 +9,23 @@ pub struct Realm {
     intrinsics: Intrinsics,// [[Intrinsics]]
     global: ObjectHandle, // [[GlobalObject]]
     env: Environment, // [[GlobalEnv]]
+}
+
+
+
+impl Realm {
+    fn new() -> anyhow::Result<Self> {
+        
+        
+        let intrinsics = Intrinsics::new()?;
+        
+        let global = Object::with_proto(intrinsics.obj.clone().into());
+        
+        
+        Ok(Self {
+            env: Environment {},
+            intrinsics,
+            global,
+        })
+    }
 }
