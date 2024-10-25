cc1b3d0f292abce9241f2b03e7509a71dc6be566
Shark <sharktheone@proton.me>
2024-10-25 22:50:41 +0200

    copy intrinsics from prototypes

--- crates/yavashark_test262/src/lib.rs
+++ crates/yavashark_test262/src/lib.rs
@@ -2,8 +2,6 @@ use yavashark_env::Context;
 
 mod test262;
 
-
-
 pub type Value = yavashark_value::Value<Context>;
 pub type Error = yavashark_value::Error<Context>;
 pub type ObjectHandle = yavashark_value::Object<Context>;
@@ -12,8 +10,6 @@ pub type Symbol = yavashark_value::Symbol<Context>;
 
 pub type ObjectProperty = yavashark_value::ObjectProperty<Context>;
 
-
 pub type NativeFunction = yavashark_env::NativeFunction;
-
 
 pub use yavashark_env::object;
