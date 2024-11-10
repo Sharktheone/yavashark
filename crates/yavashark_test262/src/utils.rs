6f3e18cf60c4bd277f001baee5900d19fda0ae34
Shark <sharktheone@proton.me>
2024-11-10 23:59:29 +0100

    try to get the comments for metadata

--- crates/yavashark_test262/src/utils.rs
+++ crates/yavashark_test262/src/utils.rs
@@ -1,8 +1,10 @@
 use std::path::Path;
+use swc_common::comments::{CommentKind, SingleThreadedComments};
 use swc_common::input::StringInput;
 use swc_common::BytePos;
 use swc_ecma_ast::Stmt;
 use swc_ecma_parser::{EsSyntax, Parser, Syntax};
+use yaml_rust2::yaml::YamlDecoder;
 
 pub(crate) fn parse_file(f: &Path) -> Vec<Stmt> {
     let input = std::fs::read_to_string(f).unwrap();
@@ -11,18 +13,47 @@ pub(crate) fn parse_file(f: &Path) -> Vec<Stmt> {
         return Vec::new();
     }
 
-    let input = StringInput::new(&input, BytePos(0), BytePos(input.len() as u32 - 1));
+    let max = BytePos(input.len() as u32 - 1);
+
+    let input = StringInput::new(&input, BytePos(0), max);
 
     let c = EsSyntax::default();
 
-    let mut p = Parser::new(Syntax::Es(c), input, None);
-    
+    let comments = SingleThreadedComments::default();
 
-    match p.parse_script() {
+    let mut p = Parser::new(Syntax::Es(c), input, Some(&comments));
+
+    let s = match p.parse_script() {
         Ok(s) => s,
         Err(e) => {
             println!("PARSE_ERROR:\n{e:?}");
             panic!()
         }
-    }.body
+    };
+
+    // comments.with_leading(BytePos(0), |comments| {
+    //     dbg!(comments);
+    // });
+
+    let (leading, _) = comments.take_all();
+
+    let meta = leading
+        .borrow()
+        .iter()
+        .map(|(_, x)| x)
+        .flatten()
+        .filter(|comment| {
+            if comment.kind != CommentKind::Block {
+                return false;
+            }
+
+            comment.text.starts_with("---\n")
+        })
+        .filter_map(|c| YamlDecoder::read(c.text.as_bytes()).decode().ok())
+        .flatten()
+        .collect::<Vec<_>>();
+
+    dbg!(meta);
+
+    s.body
 }
