9fb659349291e55b5292ea3429dfa15bc53c700a
Shark <sharktheone@proton.me>
2024-11-10 02:45:16 +0100

    don't add test262 dir prefix to test

--- crates/yavashark_test262/src/run.rs
+++ crates/yavashark_test262/src/run.rs
@@ -1,12 +1,10 @@
 use crate::harness::setup_global;
 use crate::utils::parse_file;
-use crate::TEST262_DIR;
-use std::path::{Path, PathBuf};
+use std::path::{PathBuf};
 use yavashark_env::ValueResult;
 use yavashark_interpreter::Interpreter;
 
 pub fn run_file(file: PathBuf) -> ValueResult {
-    let path = Path::new(TEST262_DIR).join("test").join(&file);
     let (mut realm, mut scope) = setup_global(file.clone())?;
 
     let stmt = parse_file(&file);
