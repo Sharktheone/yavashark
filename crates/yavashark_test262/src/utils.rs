e25f31894d1eecacff2c8f029436d65c94960289
Shark <sharktheone@proton.me>
2024-11-13 20:51:37 +0100

    use metadata for parse failures

--- crates/yavashark_test262/src/utils.rs
+++ crates/yavashark_test262/src/utils.rs
@@ -1,3 +1,4 @@
+use crate::metadata::{Metadata, NegativePhase};
 use std::path::Path;
 use swc_common::comments::{CommentKind, SingleThreadedComments};
 use swc_common::input::StringInput;
@@ -12,48 +13,69 @@ pub(crate) fn parse_file(f: &Path) -> Vec<Stmt> {
     if input.is_empty() {
         return Vec::new();
     }
-
-    let max = BytePos(input.len() as u32 - 1);
-
-    let input = StringInput::new(&input, BytePos(0), max);
-
+    
     let c = EsSyntax::default();
 
-    let comments = SingleThreadedComments::default();
+    let metadata;
 
-    let mut p = Parser::new(Syntax::Es(c), input, Some(&comments));
+    {
+        let end = input.find("\n---*/\n").map(|x| x + 7).unwrap_or(input.len());
+        
+
+        let input = &input[..end];
+        let max = BytePos(input.len() as u32);
+
+        let input = StringInput::new(&input[..end], BytePos(0), max);
+
+
+        let comments = SingleThreadedComments::default();
+
+        let mut p = Parser::new(Syntax::Es(c), input, Some(&comments));
+
+        let x = p.parse_script();
+
+        let (leading, a) = comments.take_all();
+
+        let meta = leading
+            .borrow()
+            .iter()
+            .map(|(_, x)| x)
+            .flatten()
+            .filter(|comment| {
+                if comment.kind != CommentKind::Block {
+                    return false;
+                }
+
+                comment.text.starts_with("---\n")
+            })
+            .filter_map(|c| YamlDecoder::read(c.text.as_bytes()).decode().ok())
+            .flatten()
+            .collect::<Vec<_>>();
+
+
+        metadata = meta.first().map(Metadata::parse).unwrap_or(Metadata::default());
+    };
+    
+    
+    let end = BytePos(input.len() as u32 - 1);
+    
+    let input = StringInput::new(&input, BytePos(0), end);
+    
+    let mut p = Parser::new(Syntax::Es(c), input, None);
+
 
     let s = match p.parse_script() {
         Ok(s) => s,
         Err(e) => {
+            if let Some(neg) = &metadata.negative {
+                if neg.phase == NegativePhase::Parse {
+                    return Vec::new();
+                }
+            }
+            
             println!("PARSE_ERROR:\n{e:?}");
             panic!()
         }
     };
-
-    // comments.with_leading(BytePos(0), |comments| {
-    //     dbg!(comments);
-    // });
-
-    let (leading, _) = comments.take_all();
-
-    let meta = leading
-        .borrow()
-        .iter()
-        .map(|(_, x)| x)
-        .flatten()
-        .filter(|comment| {
-            if comment.kind != CommentKind::Block {
-                return false;
-            }
-
-            comment.text.starts_with("---\n")
-        })
-        .filter_map(|c| YamlDecoder::read(c.text.as_bytes()).decode().ok())
-        .flatten()
-        .collect::<Vec<_>>();
-
-    dbg!(meta);
-
     s.body
 }
