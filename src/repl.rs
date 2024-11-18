09fd17c33db5b5e592e3917cae2b6b9cc061d4ad
Shark <sharktheone@proton.me>
2024-11-18 22:14:28 +0100

    repl braces and callback

--- src/repl.rs
+++ src/repl.rs
@@ -2,19 +2,19 @@ use std::io;
 use std::io::Write;
 
 struct Repl {
-    callback: Box<dyn Fn(String)>,
+    callback: Box<dyn Fn(&str)>,
 }
 
 const OPEN_BRACES: &[char] = &['(', '{', '['];
 const CLOSE_BRACES: &[char] = &[')', '}', ']'];
 
 impl Repl {
-    fn new(callback: Box<dyn Fn(String)>) -> Self {
+    fn new(callback: Box<dyn Fn(&str)>) -> Self {
         Self { callback }
     }
 
     fn run(&self) {
-        let braces_open = 0u8;
+        let mut braces_open = 0u8;
 
         let mut input = String::new();
 
@@ -28,8 +28,26 @@ impl Repl {
                 print!("...");
                 let _ = io::stdout().flush();
             }
+            
+            let offset = input.len() - 1;
 
             io::stdin().read_line(&mut input).unwrap();
+            
+            for i in input[offset..].chars() {
+                if OPEN_BRACES.contains(&i) {
+                    braces_open += 1;
+                }
+                
+                if CLOSE_BRACES.contains(&i) {
+                    braces_open -= 1;
+                }
+                
+            }
+            
+            
+            if braces_open == 0 {
+                (self.callback)(&input);
+            }
         }
     }
 }
