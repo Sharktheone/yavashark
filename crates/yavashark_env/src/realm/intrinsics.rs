dbeb6e563a3a2618bdfb5e44cab4ee4d92291a15
Shark <sharktheone@proton.me>
2024-11-24 22:31:04 +0100

    fix Math new in intrinsics

--- crates/yavashark_env/src/realm/intrinsics.rs
+++ crates/yavashark_env/src/realm/intrinsics.rs
@@ -93,7 +93,10 @@ impl Intrinsics {
         )?;
         
         
-        let math_obj = Math::new(obj_prototype.clone().into());
+        let math_obj = Math::new(
+            obj_prototype.clone().into(),
+            func_prototype.clone().into(),
+        )?;
 
         Ok(Self {
             obj: obj_prototype,
