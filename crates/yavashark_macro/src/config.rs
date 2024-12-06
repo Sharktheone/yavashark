34fd32a76f31c8a219d9b88b61633a56392b42b5
Shark <sharktheone@proton.me>
2024-12-06 23:43:14 +0100

    fix braces

--- crates/yavashark_macro/src/config.rs
+++ crates/yavashark_macro/src/config.rs
@@ -16,22 +16,22 @@ struct Config {
 
 impl Config {
     fn new(span: Span) -> Self {
-        let crate_path = Path::from(Ident::new("crate", span);
+        let crate_path = Path::from(Ident::new("crate", span));
 
         let mut realm = crate_path.clone();
         realm
             .segments
-            .push(PathSegment::from(Ident::new("Realm", span));
+            .push(PathSegment::from(Ident::new("Realm", span)));
 
         let mut error = crate_path.clone();
         error
             .segments
-            .push(PathSegment::from(Ident::new("Error", span));
+            .push(PathSegment::from(Ident::new("Error", span)));
 
         let mut native_function = crate_path.clone();
         native_function
             .segments
-            .push(PathSegment::from(Ident::new("NativeFunction", span));
+            .push(PathSegment::from(Ident::new("NativeFunction", span)));
 
         let mut native_constructor = crate_path.clone();
         native_constructor
@@ -44,25 +44,25 @@ impl Config {
         let mut variable = crate_path.clone();
         variable
             .segments
-            .push(PathSegment::from(Ident::new("Variable", span));
+            .push(PathSegment::from(Ident::new("Variable", span)));
 
         let mut object_handle = crate_path.clone();
         object_handle
             .segments
-            .push(PathSegment::from(Ident::new("ObjectHandle", span));
+            .push(PathSegment::from(Ident::new("ObjectHandle", span)));
 
         let mut object = crate_path.clone();
         object
             .segments
-            .push(PathSegment::from(Ident::new("object", span));
+            .push(PathSegment::from(Ident::new("object", span)));
         object
             .segments
-            .push(PathSegment::from(Ident::new("Object", span));
+            .push(PathSegment::from(Ident::new("Object", span)));
 
         let mut value = crate_path.clone();
         value
             .segments
-            .push(PathSegment::from(Ident::new("Value", span));
+            .push(PathSegment::from(Ident::new("Value", span)));
         
         
         Self {
