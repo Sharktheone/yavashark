bc99a9d7677b8e35954bb96de326ea76880df81a
Shark <sharktheone@proton.me>
2024-12-11 15:49:32 +0100

    fix use of just `crate` instead of `yavashark_env` when it would be needed

--- crates/yavashark_macro/src/config.rs
+++ crates/yavashark_macro/src/config.rs
@@ -2,7 +2,7 @@ use proc_macro2::{Ident, Span};
 use syn::{Path, PathSegment};
 
 pub struct Config {
-    pub crate_path: Path,
+    pub env_path: Path,
     pub realm: Path,
     pub error: Path,
     pub native_function: Path,
@@ -17,39 +17,39 @@ pub struct Config {
 
 impl Config {
     pub fn new(span: Span) -> Self {
-        let crate_path = Path::from(Ident::new("crate", span));
+        let env_path = crate::env_path();
 
-        let mut realm = crate_path.clone();
+        let mut realm = env_path.clone();
         realm
             .segments
             .push(PathSegment::from(Ident::new("Realm", span)));
 
-        let mut error = crate_path.clone();
+        let mut error = env_path.clone();
         error
             .segments
             .push(PathSegment::from(Ident::new("Error", span)));
 
-        let mut native_function = crate_path.clone();
+        let mut native_function = env_path.clone();
         native_function
             .segments
             .push(PathSegment::from(Ident::new("NativeFunction", span)));
 
-        let mut native_constructor = crate_path.clone();
+        let mut native_constructor = env_path.clone();
         native_constructor
             .segments
             .push(PathSegment::from(Ident::new("NativeConstructor", span)));
 
-        let mut variable = crate_path.clone();
+        let mut variable = env_path.clone();
         variable
             .segments
             .push(PathSegment::from(Ident::new("Variable", span)));
 
-        let mut object_handle = crate_path.clone();
+        let mut object_handle = env_path.clone();
         object_handle
             .segments
             .push(PathSegment::from(Ident::new("ObjectHandle", span)));
 
-        let mut object = crate_path.clone();
+        let mut object = env_path.clone();
         object
             .segments
             .push(PathSegment::from(Ident::new("object", span)));
@@ -57,23 +57,23 @@ impl Config {
             .segments
             .push(PathSegment::from(Ident::new("Object", span)));
 
-        let mut value = crate_path.clone();
+        let mut value = env_path.clone();
         value
             .segments
             .push(PathSegment::from(Ident::new("Value", span)));
 
-        let mut value_result = crate_path.clone();
+        let mut value_result = env_path.clone();
         value_result
             .segments
             .push(PathSegment::from(Ident::new("ValueResult", span)));
 
-        let mut object_property = crate_path.clone();
+        let mut object_property = env_path.clone();
         object_property
             .segments
             .push(PathSegment::from(Ident::new("ObjectProperty", span)));
 
         Self {
-            crate_path,
+            env_path: env_path,
             realm,
             error,
             native_function,
