04c7620379c80b76fbbb90a4089d7b8a9910d7d3
Shark <sharktheone@proton.me>
2024-12-29 03:44:09 +0100

    dude! it FUCKING works!

--- crates/yavashark_env/src/builtins/math.rs
+++ crates/yavashark_env/src/builtins/math.rs
@@ -1,5 +1,5 @@
 use crate::{Error, Object, ObjectHandle, Res, Result, Value, ValueResult};
-use yavashark_macro::{object, properties};
+use yavashark_macro::{object, properties, properties_new};
 use yavashark_value::Obj;
 
 #[object]
@@ -19,51 +19,64 @@ impl Math {
     }
 }
 
-#[properties]
+
+
+#[properties_new(raw)]
 impl Math {
-    #[prop("pow")]
-    #[allow(clippy::unused_self, clippy::needless_pass_by_value)]
-    fn pow(&self, args: Vec<Value>, _: Value) -> ValueResult {
-        if args.len() < 2 {
-            return Ok(Value::Number(f64::NAN));
-        }
-
-        let base = args[0].as_number();
-        let exponent = args[1].as_number();
-
-        Ok(base.powf(exponent).into())
+    const PI: f64 = std::f64::consts::PI;
+    const E: f64 = std::f64::consts::E;
+    
+    fn pow(left: f64, right: f64) -> f64 {
+        left.powf(right)
     }
-    pub(crate) fn initialize(&mut self, func_proto: Value) -> Res {
-        use yavashark_value::Obj;
-        let function = crate::NativeFunction::with_proto(
-            stringify!(pow),
-            |args, this, _| match this.copy() {
-                Value::Object(ref x) => {
-                    let x = x.get()?;
-                    let deez = (***x)
-                        .as_any()
-                        .downcast_ref::<Self>()
-                        .ok_or(Error::ty_error(format!(
-                            "Function {:?} was not called with a valid this value: {:?}",
-                            stringify!(pow),
-                            this
-                        )))?;
-                    deez.pow(args, this)
-                }
-                _ => Err(Error::ty_error(format!(
-                    "Function {:?} was not called with a valid this value: {:?}",
-                    stringify!(pow),
-                    this
-                ))),
-            },
-            func_proto,
-        )
-        .into();
-        self.define_variable(
-            stringify!(pow).into(),
-            crate::Variable::new_with_attributes(function, true, false, false),
-        );
-
-        Ok(())
-    }
+    
 }
+
+// #[properties]
+// impl Math {
+//     #[prop("pow")]
+//     #[allow(clippy::unused_self, clippy::needless_pass_by_value)]
+//     fn pow(&self, args: Vec<Value>, _: Value) -> ValueResult {
+//         if args.len() < 2 {
+//             return Ok(Value::Number(f64::NAN));
+//         }
+// 
+//         let base = args[0].as_number();
+//         let exponent = args[1].as_number();
+// 
+//         Ok(base.powf(exponent).into())
+//     }
+//     pub(crate) fn initialize(&mut self, func_proto: Value) -> Res {
+//         use yavashark_value::Obj;
+//         let function = crate::NativeFunction::with_proto(
+//             stringify!(pow),
+//             |args, this, _| match this.copy() {
+//                 Value::Object(ref x) => {
+//                     let x = x.get()?;
+//                     let deez = (***x)
+//                         .as_any()
+//                         .downcast_ref::<Self>()
+//                         .ok_or(Error::ty_error(format!(
+//                             "Function {:?} was not called with a valid this value: {:?}",
+//                             stringify!(pow),
+//                             this
+//                         )))?;
+//                     deez.pow(args, this)
+//                 }
+//                 _ => Err(Error::ty_error(format!(
+//                     "Function {:?} was not called with a valid this value: {:?}",
+//                     stringify!(pow),
+//                     this
+//                 ))),
+//             },
+//             func_proto,
+//         )
+//         .into();
+//         self.define_variable(
+//             stringify!(pow).into(),
+//             crate::Variable::new_with_attributes(function, true, false, false),
+//         );
+// 
+//         Ok(())
+//     }
+// }
