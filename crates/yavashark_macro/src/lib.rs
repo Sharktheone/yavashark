facbb5e66492a40de42b5320268892261302b697
Shark <sharktheone@proton.me>
2025-01-11 19:09:16 +0100

    value crate detection

--- crates/yavashark_macro/src/lib.rs
+++ crates/yavashark_macro/src/lib.rs
@@ -48,3 +48,12 @@ fn env_path() -> syn::Path {
         syn::parse_str("yavashark_env").unwrap()
     }
 }
+
+fn value_path() -> syn::Path {
+    let name = env::var("CARGO_PKG_NAME").unwrap_or("".to_string());
+    if name == "yavashark_value" {
+        syn::parse_str("crate").unwrap()
+    } else {
+        syn::parse_str("yavashark_value").unwrap()
+    }
+}
