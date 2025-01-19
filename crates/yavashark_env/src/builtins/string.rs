1fd76eff3a8b252b66b87bfb1a646f6b0c40ee7e
Shark <sharktheone@proton.me>
2025-01-19 17:51:09 +0100

    begin implement String Object and String constructor

--- crates/yavashark_env/src/builtins/string.rs
+++ crates/yavashark_env/src/builtins/string.rs
@@ -1,18 +1,56 @@
-use crate::{MutObject, ObjectHandle, Realm, ValueResult};
+use crate::{MutObject, ObjectHandle, Realm, ValueResult, Value};
 use std::cell::RefCell;
-use yavashark_macro::{object, properties};
-use yavashark_value::Obj;
+use yavashark_macro::{object, properties_new};
+use yavashark_value::{Constructor, Error, Func, Obj};
 
 #[object]
 #[derive(Debug)]
+pub struct StringObj {
+    #[mutable]
+    string: String,
+}
+
+
+#[object(constructor, function)]
+#[derive(Debug)]
 pub struct StringConstructor {}
 
-impl StringConstructor {
+impl Constructor<Realm> for StringConstructor {
+    fn construct(&self, realm: &mut Realm, args: Vec<Value>) -> ValueResult {
+        let str = match args.first() {
+            Some(v) => v.to_string(realm)?,
+            None => String::new(),
+        };
+        
+        let obj = StringObj::with_string(realm, str)?;
+        
+        
+        Ok(obj.into())
+    }
+}
+
+impl Func<Realm> for StringConstructor {
+    fn call(&self, realm: &mut Realm, args: Vec<Value>, this: Value) -> ValueResult {
+        let str = match args.first() {
+            Some(v) => v.to_string(realm)?,
+            None => String::new(),
+        };
+        
+        Ok(str.into())
+    }
+}
+
+impl StringObj {
     #[allow(clippy::new_ret_no_self, dead_code)]
-    pub fn new(proto: ObjectHandle, _func: ObjectHandle) -> crate::Result<ObjectHandle> {
+    pub fn new(realm: &mut Realm) -> crate::Result<ObjectHandle> {
+        Self::with_string(realm, String::new())
+    }
+    
+    pub fn with_string(realm: &mut Realm, string: String) -> crate::Result<ObjectHandle> {
         let this = Self {
-            inner: RefCell::new(MutableStringConstructor {
-                object: MutObject::with_proto(proto.into()),
+            inner: RefCell::new(MutableStringObj {
+                object: MutObject::with_proto(realm.intrinsics.string_proto.clone().into()),
+                string,
             }),
         };
 
@@ -20,10 +58,6 @@ impl StringConstructor {
     }
 }
 
-#[properties]
-impl StringConstructor {
-    #[new]
-    pub fn create(realm: &Realm) -> ValueResult {
-        Ok(Self::new(realm.intrinsics.obj.clone(), realm.intrinsics.func.clone())?.into())
-    }
+#[properties_new]
+impl StringObj {
 }
