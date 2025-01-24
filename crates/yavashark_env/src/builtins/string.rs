c44a5ae3948beccc497398b22fd88a33c6f033df
Shark <sharktheone@proton.me>
2025-01-24 23:45:15 +0100

    more string prototype methods

--- crates/yavashark_env/src/builtins/string.rs
+++ crates/yavashark_env/src/builtins/string.rs
@@ -92,10 +92,99 @@ impl StringObj {
 
         Ok(this.into_object())
     }
+    
+    pub fn get(&self, index: isize, to: isize) -> Option<String> {
+        let inner = self.inner.borrow();
+        let len = inner.string.len() as isize;
+
+        let start = if index < 0 {
+            (len + index) as usize
+        } else {
+            index as usize
+        };
+
+        let end = if to < 0 {
+            (len + to) as usize
+        } else {
+            to as usize
+        };
+
+        if start > end {
+            return None;
+        }
+
+        let string = inner.string.get(start..end);
+
+        string.map(|s| s.to_string())
+    }
 }
 
 #[properties_new(constructor(StringConstructor::new))]
 impl StringObj {
+    
+    pub fn anchor(&self, name: String) -> ValueResult {
+        Ok(format!("<a name=\"{}\">{}</a>", name, self.inner.borrow().string).into())
+    }
+    
+    pub fn at(&self, index: isize) -> Value {
+        self.get(index, index + 1).map_or(Value::Undefined, Into::into)
+    }
+    
+    pub fn big(&self) -> ValueResult {
+        Ok(format!("<big>{}</big>", self.inner.borrow().string).into())
+    }
+    
+    pub fn blink(&self) -> ValueResult {
+        Ok(format!("<blink>{}</blink>", self.inner.borrow().string).into())
+    }
+    
+    pub fn bold(&self) -> ValueResult {
+        Ok(format!("<b>{}</b>", self.inner.borrow().string).into())
+    }
+    
+    #[prop("charAt")]
+    pub fn char_at(&self, index: isize) -> Value {
+        self.get(index, index + 1).map_or(Value::Undefined, Into::into)
+    }
+    
+    #[prop("charCodeAt")]
+    pub fn char_code_at(&self, index: isize) -> Value {
+        self.get(index, index + 1)
+            .map(|s| s.chars().next().map(|c| c as u32).unwrap_or_default())
+            .unwrap_or_default()
+            .into()
+    }
+    
+    #[prop("codePointAt")]
+    pub fn code_point_at(&self, index: isize) -> Value {
+        self.get(index, index + 1)
+            .map(|s| s.chars().next().map(|c| c as u32).unwrap_or_default())
+            .unwrap_or_default()
+            .into()
+    }
+    
+    #[prop("concat")]
+    pub fn concat(&self, #[variadic] args: Vec<Value>, #[realm] realm: &mut Realm) -> ValueResult {
+        let inner = self.inner.borrow();
+        let mut string = inner.string.clone();
+
+        for arg in args {
+            string.push_str(&arg.to_string(realm)?);
+        }
+
+        Ok(string.into())
+    }
+    
+    #[prop("endsWith")]
+    pub fn ends_with(&self, search: String) -> Value {
+        let inner = self.inner.borrow();
+
+        inner.string.ends_with(&search).into()
+    }
+    
+    
+    
+    
     pub fn substr(&self, start: isize) -> ValueResult {
         let inner = self.inner.borrow();
 
