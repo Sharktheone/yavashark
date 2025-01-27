d191ad262d99ef657e49c12635a09a49ba91c991
Shark <sharktheone@proton.me>
2025-01-28 00:05:17 +0100

    implement some more Object.prototype functions

--- crates/yavashark_env/src/object/prototype/common.rs
+++ crates/yavashark_env/src/object/prototype/common.rs
@@ -1,26 +1,68 @@
 #![allow(clippy::needless_pass_by_value, unused)]
 
 use crate::realm::Realm;
-use crate::{Object, Value, ValueResult};
+use crate::{Error, Object, Value, ValueResult};
 
 pub fn define_getter(args: Vec<Value>, this: Value, realm: &mut Realm) -> ValueResult {
-    todo!()
+    if args.len() < 2 {
+        return Ok(Value::Undefined);
+    }
+    
+    let name = args[0].copy();
+    let get = args[1].copy();
+    
+    let this = this.as_object()?;
+    
+    this.define_getter(name, get)?;
+    
+    Ok(Value::Undefined)
 }
 
 pub fn define_setter(args: Vec<Value>, this: Value, realm: &mut Realm) -> ValueResult {
-    todo!()
+    if args.len() < 2 {
+        return Ok(Value::Undefined);
+    }
+    
+    let name = args[0].copy();
+    let set = args[1].copy();
+    
+    let this = this.as_object()?;
+    
+    this.define_setter(name, set)?;
+    
+    Ok(Value::Undefined)
 }
 
 pub fn lookup_getter(args: Vec<Value>, this: Value, realm: &mut Realm) -> ValueResult {
-    todo!()
+    if args.is_empty() {
+        return Ok(Value::Undefined);
+    }
+    
+    let name = &args[0];
+    
+    let this = this.as_object()?;
+    
+    let getter = this.get_getter(name)?;
+    
+    Ok(getter.unwrap_or(Value::Undefined))
 }
 
 pub fn lookup_setter(args: Vec<Value>, this: Value, realm: &mut Realm) -> ValueResult {
-    todo!()
+    if args.is_empty() {
+        return Ok(Value::Undefined);
+    }
+    
+    let name = &args[0];
+    
+    let this = this.as_object()?;
+    
+    let setter = this.get_setter(name)?;
+    
+    Ok(setter.unwrap_or(Value::Undefined))
 }
 
 pub fn object_constructor(args: Vec<Value>, this: Value, realm: &mut Realm) -> ValueResult {
-    todo!()
+    todo!("Object constructor")
 }
 
 pub fn has_own_property(args: Vec<Value>, this: Value, realm: &mut Realm) -> ValueResult {
@@ -61,16 +103,37 @@ pub fn get_own_property_descriptor(args: Vec<Value>, this: Value, realm: &Realm)
     Ok(desc.into())
 }
 
-pub fn is_prototype_of(args: Vec<Value>, this: Value, realm: &mut Realm) -> ValueResult {
-    todo!()
+pub fn is_prototype_of(args: Vec<Value>, mut this: Value, realm: &mut Realm) -> ValueResult {
+    if args.is_empty() {
+        return Ok(Value::Undefined);
+    }
+    
+    let search = &args[0];
+    
+    
+    loop {
+        let mut obj = this.as_object()?;
+        let proto = obj.prototype()?;
+        let proto = proto.get(this.clone(), realm)?;
+        
+        if proto.is_nullish() {
+            return Ok(false.into());
+        }
+        
+        if &proto == search {
+            return Ok(true.into());
+        }
+        
+        this = proto;
+    }
 }
 
 pub fn property_is_enumerable(args: Vec<Value>, this: Value, realm: &mut Realm) -> ValueResult {
-    todo!()
+    Ok(false.into())
 }
 
 pub fn to_locale_string(args: Vec<Value>, this: Value, realm: &mut Realm) -> ValueResult {
-    todo!()
+    Err(Error::new("Not implemented"))
 }
 
 pub fn to_string(args: Vec<Value>, this: Value, realm: &mut Realm) -> ValueResult {
@@ -78,5 +141,5 @@ pub fn to_string(args: Vec<Value>, this: Value, realm: &mut Realm) -> ValueResul
 }
 
 pub fn value_of(args: Vec<Value>, this: Value, realm: &mut Realm) -> ValueResult {
-    todo!()
+    Ok(this)
 }
