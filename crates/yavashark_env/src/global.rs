53a36579d1c4cfea4d88c4ac3c346c50afab9efe
Shark <sharktheone@proton.me>
2025-01-30 19:18:53 +0100

    add regex to global

--- crates/yavashark_env/src/global.rs
+++ crates/yavashark_env/src/global.rs
@@ -23,6 +23,7 @@ pub fn init_global_obj(handle: &ObjectHandle, realm: &Realm) -> Res {
     obj.define_variable("Boolean".into(), realm.intrinsics.boolean_constructor())?;
     obj.define_variable("Symbol".into(), realm.intrinsics.symbol_constructor())?;
     obj.define_variable("BigInt".into(), realm.intrinsics.bigint_constructor())?;
+    obj.define_variable("RegExp".into(), realm.intrinsics.regexp_constructor())?;
 
     #[cfg(feature = "out-of-spec-experiments")]
     crate::experiments::init(handle, realm)?;
