d3b24eedde9049430f3a5b71e55c80493f738851
Shark <sharktheone@proton.me>
2025-02-02 22:16:10 +0100

    Scope::get_variable_names

--- crates/yavashark_env/src/scope.rs
+++ crates/yavashark_env/src/scope.rs
@@ -1,5 +1,5 @@
 use std::cell::RefCell;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::path::PathBuf;
 use std::rc::Rc;
 
@@ -531,6 +531,38 @@ impl ScopeInternal {
     pub fn copy_path(&mut self) {
         self.file = self.get_current_file().ok();
     }
+    
+    pub fn get_variables(&self) -> Result<HashMap<String, Variable>> {
+        let mut variables: HashMap<String, Variable> = match &self.parent {
+            ParentOrGlobal::Parent(p) => p.borrow()?.get_variables()?,
+            ParentOrGlobal::Global(g) => g.properties()?.iter().filter_map(|(k, v)| match k {
+                Value::String(s) => Some((s.clone(), Variable::new(v.clone()))),
+                _ => None,
+            }).collect(),
+        };
+        
+        
+        for (name, variable) in &self.variables {
+            variables.insert(name.clone(), variable.clone());
+        }
+        
+        Ok(variables)
+    }
+    
+    pub fn get_variable_names(&self) -> Result<HashSet<String>> {
+        let mut variables = match &self.parent {
+            ParentOrGlobal::Parent(p) => p.borrow()?.get_variable_names()?,
+            ParentOrGlobal::Global(g) => g.properties()?.iter().filter_map(|(k, _)| match k {
+                Value::String(s) => Some(s.clone()),
+                _ => None,
+                
+            }).collect(),
+        };
+        
+        variables.extend(self.variables.keys().cloned());
+        
+        Ok(variables)
+    }
 }
 
 impl Scope {
@@ -722,6 +754,14 @@ impl Scope {
         self.scope.borrow_mut()?.copy_path();
 
         Ok(())
+    }
+    
+    pub fn get_variables(&self) -> Result<HashMap<String, Variable>> {
+        self.scope.borrow()?.get_variables()
+    }
+    
+    pub fn get_variable_names(&self) -> Result<HashSet<String>> {
+        self.scope.borrow()?.get_variable_names()
     }
 }
 
