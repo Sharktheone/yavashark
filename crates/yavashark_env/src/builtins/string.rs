e52d68d362ba9cf044b6b5ae7548a7b2b0ed54f9
Shark <sharktheone@proton.me>
2025-02-19 00:01:01 +0100

    implement more string methods

--- crates/yavashark_env/src/builtins/string.rs
+++ crates/yavashark_env/src/builtins/string.rs
@@ -1,8 +1,10 @@
-use crate::{MutObject, Object, ObjectHandle, Realm, Value, ValueResult};
+use crate::{Error, MutObject, Object, ObjectHandle, Realm, Value, ValueResult};
 use std::cell::RefCell;
 use std::cmp;
+use unicode_normalization::UnicodeNormalization;
 use yavashark_macro::{object, properties_new};
 use yavashark_value::{Constructor, Func, Obj};
+use crate::array::Array;
 
 #[object]
 #[derive(Debug)]
@@ -195,7 +197,7 @@ impl StringObj {
             color,
             self.inner.borrow().string
         )
-        .into())
+            .into())
     }
 
     #[prop("fontsize")]
@@ -205,7 +207,7 @@ impl StringObj {
             size,
             self.inner.borrow().string
         )
-        .into())
+            .into())
     }
 
     #[prop("includes")]
@@ -237,8 +239,7 @@ impl StringObj {
     pub fn is_well_formed(&self) -> bool {
         // check if we have any lone surrogates => between 0xD800-0xDFFF or 0xDC00-0xDFFF
         self.inner.borrow().string.chars().all(|c| {
-            let c = c as u32;
-            !(0xD800..=0xDFFF).contains(&c) || (0xDC00..=0xDFFF).contains(&c)
+            !is_lone_surrogate(c)
         })
     }
 
@@ -250,7 +251,7 @@ impl StringObj {
     #[prop("lastIndexOf")]
     pub fn last_index_of(&self, search: &str, from: Option<isize>) -> isize {
         let inner = self.inner.borrow();
-        
+
         let from = from.unwrap_or(-1);
 
         let from = if from < 0 {
@@ -274,7 +275,141 @@ impl StringObj {
     //TODO: localization
     // }
 
-    //TODO: match, matchAll
+    // #[prop("match")]
+    // pub fn match_(&self, pattern: &RegExp, #[realm] realm: &mut Realm) -> ValueResult {
+    //     //TODO: Symbol.search
+    //     let inner = self.inner.borrow();
+    //
+    //     pattern.exec(&inner.string, realm)
+    // }
+
+    // #[prop("matchAll")]
+    // pub fn match_all(&self, pattern: &RegExp, #[realm] realm: &mut Realm) -> ValueResult {
+    //     //TODO
+    // }
+
+    pub fn normalize(&self, form: &str) -> ValueResult {
+        let inner = self.inner.borrow();
+
+        let form = match form {
+            "NFC" => inner.string.nfc().to_string(),
+            "NFD" => inner.string.nfd().to_string(),
+            "NFKC" => inner.string.nfkc().to_string(),
+            "NFKD" => inner.string.nfkd().to_string(),
+            _ => return Err(Error::range("Invalid normalization form")),
+        };
+
+
+        Ok(form.into())
+    }
+
+    #[prop("padEnd")]
+    pub fn pad_end(&self, target_length: usize, pad_string: Option<String>) -> ValueResult {
+        let inner = self.inner.borrow();
+
+        let pad_string = pad_string.as_deref().unwrap_or(" ");
+
+        let pad_len = target_length.saturating_sub(inner.string.len());
+
+        let pad = pad_string.repeat(pad_len);
+
+        Ok(format!("{}{}", inner.string, pad).into())
+    }
+
+    #[prop("padStart")]
+    pub fn pad_start(&self, target_length: usize, pad_string: Option<String>) -> ValueResult {
+        let inner = self.inner.borrow();
+
+        let pad_string = pad_string.as_deref().unwrap_or(" ");
+
+        let pad_len = target_length.saturating_sub(inner.string.len());
+
+        let pad = pad_string.repeat(pad_len);
+
+        Ok(format!("{}{}", pad, inner.string).into())
+    }
+
+    pub fn repeat(&self, count: usize) -> ValueResult {
+        let inner = self.inner.borrow();
+
+        Ok(inner.string.repeat(count).into())
+    }
+
+    pub fn replace(&self, search: &str, replace: &str) -> ValueResult {
+        let inner = self.inner.borrow();
+
+        Ok(inner.string.replace(search, replace).into())
+    }
+
+    // pub fn search(&self, pattern: &RegExp, #[realm] realm: &mut Realm) -> ValueResult {
+    //     //TODO: Symbol.search
+    //
+    //     let inner = self.inner().try_borrow()?;
+    //
+    //     Ok(pattern.regex.find(&inner.string)
+    //         .map(|m| m.start() as isize)
+    //         .unwrap_or(-1)
+    //         .into())
+    // }
+
+    pub fn slice(&self, start: isize, end: Option<isize>) -> ValueResult {
+        let inner = self.inner.borrow();
+
+        // negative numbers are counted from the end of the string
+        let start = if start < 0 {
+            (inner.string.len() as isize + start) as usize
+        } else {
+            start as usize
+        };
+
+        let end = end.map_or(inner.string.len(), |end| {
+            if end < 0 {
+                (inner.string.len() as isize + end) as usize
+            } else {
+                end as usize
+            }
+        });
+
+        let end = cmp::min(end, inner.string.len());
+
+        let string = inner.string.get(start..end);
+
+        Ok(string.unwrap_or_default().into())
+    }
+
+    pub fn small(&self) -> ValueResult {
+        Ok(format!("<small>{}</small>", self.inner.borrow().string).into())
+    }
+
+    pub fn split(&self, separator: &str, limit: Option<usize>, #[realm] realm: &mut Realm) -> ValueResult {
+        let inner = self.inner.borrow();
+
+        let limit = limit.unwrap_or(usize::MAX);
+
+        let mut parts = inner.string.splitn(limit, separator);
+
+        let mut array = Vec::new();
+
+        while let Some(part) = parts.next() {
+            array.push(part.into());
+        }
+
+        Ok(Array::with_elements(realm, array)?.into_value())
+    }
+
+    pub fn starts_with(&self, search: &str) -> bool {
+        let inner = self.inner.borrow();
+
+        inner.string.starts_with(search)
+    }
+
+    pub fn strike(&self) -> ValueResult {
+        Ok(format!("<strike>{}</strike>", self.inner.borrow().string).into())
+    }
+
+    pub fn sub(&self) -> ValueResult {
+        Ok(format!("<sub>{}</sub>", self.inner.borrow().string).into())
+    }
 
     pub fn substr(&self, start: isize, len: Option<isize>) -> ValueResult {
         let inner = self.inner.borrow();
@@ -285,13 +420,107 @@ impl StringObj {
         } else {
             start as usize
         };
-        
+
         let end = len.map_or(inner.string.len(), |len| start + len as usize);
-        
+
         let end = cmp::min(end, inner.string.len());
 
         let string = inner.string.get(start..end);
 
         Ok(string.unwrap_or_default().into())
     }
+
+    pub fn substring(&self, start: isize, end: Option<isize>) -> ValueResult {
+        let inner = self.inner.borrow();
+
+        // negative numbers are counted from the end of the string
+        let start = if start < 0 {
+            (inner.string.len() as isize + start) as usize
+        } else {
+            start as usize
+        };
+
+        let end = end.map_or(inner.string.len(), |end| {
+            if end < 0 {
+                (inner.string.len() as isize + end) as usize
+            } else {
+                end as usize
+            }
+        });
+
+        let end = cmp::min(end, inner.string.len());
+
+        let string = inner.string.get(start..end);
+
+        Ok(string.unwrap_or_default().into())
+    }
+
+    pub fn sup(&self) -> ValueResult {
+        Ok(format!("<sup>{}</sup>", self.inner.borrow().string).into())
+    }
+
+    #[prop("toLowerCase")]
+    pub fn to_lower_case(&self) -> ValueResult {
+        let inner = self.inner.borrow();
+
+        Ok(inner.string.to_lowercase().into())
+    }
+
+    #[prop("toString")]
+    pub fn to_string(&self) -> Value {
+        self.inner.borrow().string.clone().into()
+    }
+
+    #[prop("toUpperCase")]
+    pub fn to_upper_case(&self) -> ValueResult {
+        let inner = self.inner.borrow();
+
+        Ok(inner.string.to_uppercase().into())
+    }
+
+    #[prop("toWellFormed")]
+    pub fn to_well_formed(&self) -> ValueResult {
+        let inner = self.inner.borrow();
+
+        let well_formed = inner.string.chars().map(|c| {
+            if is_lone_surrogate(c) {
+                '\u{FFFD}'
+            } else {
+                c
+            }
+        }).collect::<String>();
+
+        Ok(well_formed.into())
+    }
+
+    pub fn trim(&self) -> ValueResult {
+        let inner = self.inner.borrow();
+
+        Ok(inner.string.trim().into())
+    }
+
+    #[prop("trimEnd")]
+    pub fn trim_end(&self) -> ValueResult {
+        let inner = self.inner.borrow();
+
+        Ok(inner.string.trim_end().into())
+    }
+
+    #[prop("trimStart")]
+    pub fn trim_start(&self) -> ValueResult {
+        let inner = self.inner.borrow();
+
+        Ok(inner.string.trim_start().into())
+    }
+
+    #[prop("valueOf")]
+    pub fn value_of(&self) -> Value {
+        self.inner.borrow().string.clone().into()
+    }
+}
+
+
+fn is_lone_surrogate(c: char) -> bool {
+    let c = c as u32;
+    (0xD800..=0xDFFF).contains(&c) || (0xDC00..=0xDFFF).contains(&c)
 }
