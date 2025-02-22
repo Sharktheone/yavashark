a2132cb85db4d11caec0a4cdc6a5c347016583ea
Shark <sharktheone@proton.me>
2025-02-22 21:59:40 +0100

    implement delete

--- crates/yavashark_interpreter/src/statement/expr/unary.rs
+++ crates/yavashark_interpreter/src/statement/expr/unary.rs
@@ -1,10 +1,33 @@
 use crate::Interpreter;
-use swc_ecma_ast::{UnaryExpr, UnaryOp};
+use swc_ecma_ast::{Expr, UnaryExpr, UnaryOp};
 use yavashark_env::scope::Scope;
 use yavashark_env::{Realm, RuntimeResult, Value};
 
 impl Interpreter {
     pub fn run_unary(realm: &mut Realm, stmt: &UnaryExpr, scope: &mut Scope) -> RuntimeResult {
+        if stmt.op == UnaryOp::Delete {
+            match &*stmt.arg {
+                Expr::Ident(i) => {
+                    return Ok(false.into())
+                }
+                Expr::Member(m) => {
+                    let obj = Self::run_expr(realm, &m.obj, m.span, scope)?;
+                    if let Value::Object(obj) = obj {
+                        let name = match &m.prop {
+                            swc_ecma_ast::MemberProp::Ident(i) => Value::String(i.sym.to_string()),
+                            swc_ecma_ast::MemberProp::PrivateName(p) => Value::String(p.name.to_string()),
+                            swc_ecma_ast::MemberProp::Computed(c) => Self::run_expr(realm, &c.expr, c.span, scope)?,
+                        };
+
+                        obj.delete_property(&name);
+                        return Ok(Value::Boolean(true));
+                    }
+                }
+                _ => {}
+            }
+        }
+        
+        
         let value = Self::run_expr(realm, &stmt.arg, stmt.span, scope).or_else(|v| {
             if stmt.op == UnaryOp::TypeOf {
                 Ok(Value::String("undefined".to_string()))
@@ -20,9 +43,7 @@ impl Interpreter {
             UnaryOp::Tilde => Value::Number((!(value.to_int_or_null())) as f64),
             UnaryOp::TypeOf => Value::String(value.type_of().into()),
             UnaryOp::Void => Value::Undefined,
-            UnaryOp::Delete => {
-                todo!()
-            }
+            UnaryOp::Delete => Value::Boolean(false),
         })
     }
 }
