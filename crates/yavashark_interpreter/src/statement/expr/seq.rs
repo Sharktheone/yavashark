54e6c428a78f64d0114f354a37daf2cdd088b7bf
Shark <sharktheone@proton.me>
2025-02-22 21:43:05 +0100

    implement sequence expression (i guess?)

--- crates/yavashark_interpreter/src/statement/expr/seq.rs
+++ crates/yavashark_interpreter/src/statement/expr/seq.rs
@@ -1,10 +1,17 @@
+use swc_common::Spanned;
 use crate::Interpreter;
 use swc_ecma_ast::SeqExpr;
 use yavashark_env::scope::Scope;
-use yavashark_env::{Realm, RuntimeResult};
+use yavashark_env::{Realm, RuntimeResult, Value};
 
 impl Interpreter {
     pub fn run_seq(realm: &mut Realm, stmt: &SeqExpr, scope: &mut Scope) -> RuntimeResult {
-        todo!()
+        let mut last = None;
+        
+        for expr in &stmt.exprs {
+            last = Some(Self::run_expr(realm, expr, expr.span(), scope)?);
+        }
+
+        Ok(last.unwrap_or(Value::Undefined))
     }
 }
