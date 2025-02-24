77a2aedf8e38428fc0157dc875ebcedc3a918f87
Shark <sharktheone@proton.me>
2025-02-24 23:33:49 +0100

    some more SmallString things

--- crates/yavashark_string/src/smallstring.rs
+++ crates/yavashark_string/src/smallstring.rs
@@ -1,5 +1,6 @@
 use crate::smallvec::SmallVec;
 use std::mem;
+use std::rc::Rc;
 
 #[derive(Debug, Clone, Default)]
 pub struct SmallString {
@@ -19,6 +20,13 @@ impl SmallString {
         })
     }
 
+    pub fn as_str(&self) -> &str {
+        #[allow(clippy::expect_used)]
+        std::str::from_utf8(&self.inner).expect(
+            "SmallString contained invalid utf8 (impossible since it was created from a String)",
+        )
+    }
+
     pub fn into_string(self) -> String {
         unsafe {
             let str = String::from_raw_parts(
@@ -26,11 +34,34 @@ impl SmallString {
                 self.inner.len_cap.len(),
                 self.inner.len_cap.cap(),
             );
-            
+
             mem::forget(self.inner); // we transferred ownership to the String
-            
+
             str
         }
+    }
+
+    pub fn into_rc(self) -> Rc<str> {
+        let vec = self.into_string().into_boxed_str();
+
+        Rc::from(vec)
+    }
+    
+    pub fn into_rc_if_fit(self) -> Result<Rc<str>, Self> {
+        let vec = self.into_string();
+        
+        if vec.capacity() != vec.len() {
+            #[allow(clippy::expect_used)]
+            return Err(Self::from_string(vec).expect("unreachable"));
+        }
+        
+        Ok(Rc::from(vec.into_boxed_str()))
+    }
+    
+    pub fn copy_rc(&self) -> Rc<str> {
+        let vec = self.as_str().to_string().into_boxed_str();
+
+        Rc::from(vec)
     }
 }
 
