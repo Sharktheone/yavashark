5984adc12094e76d5a0559d56ca5732f3d066dd1
Shark <sharktheone@proton.me>
2025-03-02 00:02:03 +0100

    fix implementation of negative tests

--- crates/yavashark_test262/src/run.rs
+++ crates/yavashark_test262/src/run.rs
@@ -1,7 +1,9 @@
 use crate::harness::setup_global;
+use crate::metadata::NegativePhase;
 use crate::utils::parse_file;
 use crate::TEST262_DIR;
 use std::path::{Path, PathBuf};
+use yavashark_env::error::ErrorObj;
 use yavashark_env::scope::Scope;
 use yavashark_env::{Error, Realm};
 use yavashark_interpreter::Interpreter;
@@ -25,5 +27,36 @@ pub fn run_file_in(file: PathBuf, realm: &mut Realm, scope: &mut Scope) -> Resul
 
     scope.set_path(file)?;
 
-    Interpreter::run_in(&stmt, realm, scope).and_then(|v| v.to_string(realm))
+    let mut res = Interpreter::run_in(&stmt, realm, scope).and_then(|v| v.to_string(realm));
+
+
+    if let Some(negative) = metadata.negative {
+        if negative.phase == NegativePhase::Runtime {
+            let e = match res {
+                Ok(v) => {
+                    return Err(Error::new_error(format!("Expected error but got {:?}", v)));
+                }
+                Err(e) => e,
+            };
+
+            if negative.ty.is_empty() {
+                res = Ok("".to_string());
+            } else {
+                let Some(err) = scope.resolve(&negative.ty, realm)? else {
+                    return Err(Error::ty("Error type not found"));
+                };
+
+                let e = ErrorObj::error_to_value(e, realm);
+
+                if !e.instance_of(&err, realm)? {
+                    return Err(Error::new_error(format!("Expected error of type {:?} but got {:?}", err, e)));
+                }
+
+                res = Ok("".to_string());
+            }
+        }
+    }
+
+
+    res
 }
