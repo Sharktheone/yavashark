4f4b546c99ddd9e4e587de9e55b39bcd1dd5593e
Shark <sharktheone@proton.me>
2025-03-02 00:22:53 +0100

    implement with

--- crates/yavashark_interpreter/src/statement/with.rs
+++ crates/yavashark_interpreter/src/statement/with.rs
@@ -1,11 +1,24 @@
 use crate::Interpreter;
 use swc_ecma_ast::WithStmt;
-use yavashark_env::{Realm, RuntimeResult};
+use yavashark_env::{Realm, RuntimeResult, Value};
 
 use crate::scope::Scope;
 
 impl Interpreter {
     pub fn run_with(realm: &mut Realm, stmt: &WithStmt, scope: &mut Scope) -> RuntimeResult {
-        todo!()
+        let obj = Self::run_expr(realm, &stmt.obj, stmt.span, scope)?;
+        
+        let mut scope = scope.child()?;
+        
+        for (key, value) in obj.properties()? {
+            let Value::String(key) = key else {
+                continue;
+            };
+            
+            
+            scope.declare_var(key, value)?;
+        }
+        
+        Self::run_statement(realm, &stmt.body, &mut scope)
     }
 }
