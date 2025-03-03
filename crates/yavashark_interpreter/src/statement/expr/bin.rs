c0d6c5e9f59dd859618fbdf9f7baae279a90ff4b
Shark <sharktheone@proton.me>
2025-03-03 01:35:42 +0100

    fix bin evaluating second and before first one

--- crates/yavashark_interpreter/src/statement/expr/bin.rs
+++ crates/yavashark_interpreter/src/statement/expr/bin.rs
@@ -2,41 +2,153 @@ use swc_ecma_ast::{BinExpr, BinaryOp};
 
 use yavashark_env::scope::Scope;
 use yavashark_env::{Realm, RuntimeResult, Value};
-use yavashark_value::Error;
 
 use crate::Interpreter;
 
 impl Interpreter {
     pub fn run_bin(realm: &mut Realm, stmt: &BinExpr, scope: &mut Scope) -> RuntimeResult {
-        let left = Self::run_expr(realm, &stmt.left, stmt.span, scope)?;
-        let right = Self::run_expr(realm, &stmt.right, stmt.span, scope)?;
-
+        
+        
+        
+        
+        
+        
         Ok(match stmt.op {
-            BinaryOp::EqEq => Value::Boolean(left.normal_eq(&right)),
-            BinaryOp::NotEq => Value::Boolean(!left.normal_eq(&right)),
-            BinaryOp::EqEqEq => Value::Boolean(left == right),
-            BinaryOp::NotEqEq => Value::Boolean(left != right),
-            BinaryOp::Lt => Value::Boolean(left < right),
-            BinaryOp::LtEq => Value::Boolean(left <= right),
-            BinaryOp::Gt => Value::Boolean(left > right),
-            BinaryOp::GtEq => Value::Boolean(left >= right),
-            BinaryOp::LShift => left << right,
-            BinaryOp::RShift => left >> right,
-            BinaryOp::ZeroFillRShift => left.zero_fill_rshift(&right),
-            BinaryOp::Add => left + right,
-            BinaryOp::Sub => left - right,
-            BinaryOp::Mul => left * right,
-            BinaryOp::Div => left / right,
-            BinaryOp::Mod => left % right,
-            BinaryOp::BitOr => left | right,
-            BinaryOp::BitXor => left ^ right,
-            BinaryOp::BitAnd => left & right,
-            BinaryOp::LogicalOr => left.log_or(right),
-            BinaryOp::LogicalAnd => left.log_and(right),
-            BinaryOp::In => right.contains_key(&left)?.into(),
-            BinaryOp::InstanceOf => left.instance_of(&right, realm)?.into(),
-            BinaryOp::Exp => left.pow(&right, realm)?,
+            BinaryOp::EqEq => {
+                let left = Self::run_expr(realm, &stmt.left, stmt.span, scope)?;
+                let right = Self::run_expr(realm, &stmt.right, stmt.span, scope)?;
+
+                Value::Boolean(left.normal_eq(&right))
+            }
+            BinaryOp::NotEq => {
+                let left = Self::run_expr(realm, &stmt.left, stmt.span, scope)?;
+                let right = Self::run_expr(realm, &stmt.right, stmt.span, scope)?;
+                Value::Boolean(!left.normal_eq(&right))
+            }
+            BinaryOp::EqEqEq => {
+                let left = Self::run_expr(realm, &stmt.left, stmt.span, scope)?;
+                let right = Self::run_expr(realm, &stmt.right, stmt.span, scope)?;
+                Value::Boolean(left == right)
+            }
+            BinaryOp::NotEqEq => {
+                let left = Self::run_expr(realm, &stmt.left, stmt.span, scope)?;
+                let right = Self::run_expr(realm, &stmt.right, stmt.span, scope)?;
+                Value::Boolean(left != right)
+            }
+            BinaryOp::Lt => {
+                let left = Self::run_expr(realm, &stmt.left, stmt.span, scope)?;
+                let right = Self::run_expr(realm, &stmt.right, stmt.span, scope)?;
+                Value::Boolean(left < right)
+            }
+            BinaryOp::LtEq => {
+                let left = Self::run_expr(realm, &stmt.left, stmt.span, scope)?;
+                let right = Self::run_expr(realm, &stmt.right, stmt.span, scope)?;
+                Value::Boolean(left <= right)
+            }
+            BinaryOp::Gt => {
+                let left = Self::run_expr(realm, &stmt.left, stmt.span, scope)?;
+                let right = Self::run_expr(realm, &stmt.right, stmt.span, scope)?;
+                Value::Boolean(left > right)
+            }
+            BinaryOp::GtEq => {
+                let left = Self::run_expr(realm, &stmt.left, stmt.span, scope)?;
+                let right = Self::run_expr(realm, &stmt.right, stmt.span, scope)?;
+                Value::Boolean(left >= right)
+            }
+            BinaryOp::LShift => {
+                let left = Self::run_expr(realm, &stmt.left, stmt.span, scope)?;
+                let right = Self::run_expr(realm, &stmt.right, stmt.span, scope)?;
+                left << right
+            }
+            BinaryOp::RShift => {
+                let left = Self::run_expr(realm, &stmt.left, stmt.span, scope)?;
+                let right = Self::run_expr(realm, &stmt.right, stmt.span, scope)?;
+                left >> right
+            }
+            BinaryOp::ZeroFillRShift => {
+                let left = Self::run_expr(realm, &stmt.left, stmt.span, scope)?;
+                let right = Self::run_expr(realm, &stmt.right, stmt.span, scope)?;
+                left.zero_fill_rshift(&right)
+            }
+            BinaryOp::Add => {
+                let left = Self::run_expr(realm, &stmt.left, stmt.span, scope)?;
+                let right = Self::run_expr(realm, &stmt.right, stmt.span, scope)?;
+                left + right
+            }
+            BinaryOp::Sub => {
+                let left = Self::run_expr(realm, &stmt.left, stmt.span, scope)?;
+                let right = Self::run_expr(realm, &stmt.right, stmt.span, scope)?;
+                left - right
+            }
+            BinaryOp::Mul => {
+                let left = Self::run_expr(realm, &stmt.left, stmt.span, scope)?;
+                let right = Self::run_expr(realm, &stmt.right, stmt.span, scope)?;
+                left * right
+            }
+            BinaryOp::Div => {
+                let left = Self::run_expr(realm, &stmt.left, stmt.span, scope)?;
+                let right = Self::run_expr(realm, &stmt.right, stmt.span, scope)?;
+                left / right
+            }
+            BinaryOp::Mod => {
+                let left = Self::run_expr(realm, &stmt.left, stmt.span, scope)?;
+                let right = Self::run_expr(realm, &stmt.right, stmt.span, scope)?;
+                left % right
+            }
+            BinaryOp::BitOr => {
+                let left = Self::run_expr(realm, &stmt.left, stmt.span, scope)?;
+                let right = Self::run_expr(realm, &stmt.right, stmt.span, scope)?;
+                left | right
+            }
+            BinaryOp::BitXor => {
+                let left = Self::run_expr(realm, &stmt.left, stmt.span, scope)?;
+                let right = Self::run_expr(realm, &stmt.right, stmt.span, scope)?;
+                left ^ right
+            }
+            BinaryOp::BitAnd => {
+                let left = Self::run_expr(realm, &stmt.left, stmt.span, scope)?;
+                let right = Self::run_expr(realm, &stmt.right, stmt.span, scope)?;
+                left & right
+            }
+            BinaryOp::LogicalOr => {
+                let left = Self::run_expr(realm, &stmt.left, stmt.span, scope)?;
+
+                if left.is_truthy() {
+                    return Ok(left);
+                }
+
+                let right = Self::run_expr(realm, &stmt.right, stmt.span, scope)?;
+
+                return Ok(right);
+            }
+            BinaryOp::LogicalAnd => {
+                let left = Self::run_expr(realm, &stmt.left, stmt.span, scope)?;
+                if left.is_falsey() {
+                    return Ok(left);
+                }
+                
+                let right = Self::run_expr(realm, &stmt.right, stmt.span, scope)?;
+                
+                return Ok(right);
+            }
+            BinaryOp::In => {
+                let left = Self::run_expr(realm, &stmt.left, stmt.span, scope)?;
+                let right = Self::run_expr(realm, &stmt.right, stmt.span, scope)?;
+                right.contains_key(&left)?.into()
+            }
+            BinaryOp::InstanceOf => {
+                let left = Self::run_expr(realm, &stmt.left, stmt.span, scope)?;
+                let right = Self::run_expr(realm, &stmt.right, stmt.span, scope)?;
+                left.instance_of(&right, realm)?.into()
+            }
+            BinaryOp::Exp => {
+                let left = Self::run_expr(realm, &stmt.left, stmt.span, scope)?;
+                let right = Self::run_expr(realm, &stmt.right, stmt.span, scope)?;
+                left.pow(&right, realm)?
+            }
             BinaryOp::NullishCoalescing => {
+                let left = Self::run_expr(realm, &stmt.left, stmt.span, scope)?;
+                let right = Self::run_expr(realm, &stmt.right, stmt.span, scope)?;
                 if left.is_nullish() {
                     right
                 } else {
