12958c43e5a66948c95fa4db336669b26ca46c67
Shark <sharktheone@proton.me>
2025-03-04 00:11:11 +0100

    implement super prop

--- crates/yavashark_interpreter/src/statement/expr/super_prop.rs
+++ crates/yavashark_interpreter/src/statement/expr/super_prop.rs
@@ -1,5 +1,5 @@
 use crate::Interpreter;
-use swc_ecma_ast::SuperPropExpr;
+use swc_ecma_ast::{SuperProp, SuperPropExpr};
 use yavashark_env::scope::Scope;
 use yavashark_env::{Realm, RuntimeResult};
 
@@ -9,6 +9,24 @@ impl Interpreter {
         stmt: &SuperPropExpr,
         scope: &mut Scope,
     ) -> RuntimeResult {
-        todo!()
+        let this = scope.this()?;
+
+        let obj = this.as_object()?;
+
+        let proto = obj.prototype()?;
+        let sup = proto.resolve(this, realm)?;
+        
+        match &stmt.prop {
+            SuperProp::Ident(i) => {
+                let name = i.sym.to_string();
+
+                Ok(sup.get_property(&name.into(), realm)?)
+            }
+            SuperProp::Computed(p) => {
+                let name = Self::run_expr(realm, &p.expr, stmt.span, scope)?;
+
+                Ok(sup.get_property(&name, realm)?)
+            }
+        }
     }
 }
