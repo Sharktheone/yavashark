bc7c3f98ffc4ebe85ad0dab0e149d38e1746a3cb
Shark <sharktheone@proton.me>
2025-03-13 23:35:25 +0100

    begin instant

--- crates/yavashark_env/src/builtins/temporal/instant.rs
+++ crates/yavashark_env/src/builtins/temporal/instant.rs
@@ -1,12 +1,33 @@
-use std::cell::Cell;
+use std::cell::RefCell;
+use crate::{MutObject, ObjectHandle, Realm};
+use num_bigint::BigInt;
 use yavashark_macro::{object, props};
+use yavashark_value::Obj;
 
 #[object]
 #[derive(Debug)]
 pub struct Instant {
-    dur: Cell<std::time::Instant>,
-    negative: Cell<bool>,
+    #[mutable]
+    stamp: BigInt,
+}
+
+
+impl Instant {
+    #[must_use]
+    pub fn new(stamp: BigInt, realm: &Realm) -> Self {
+        Self {
+            inner: RefCell::new(MutableInstant {
+                object: MutObject::with_proto(realm.intrinsics.temporal_instant.clone().into()),
+                stamp,
+            }),
+        }
+    }
 }
 
 #[props]
-impl Instant {}
+impl Instant {
+    #[constructor]
+    fn construct(epoch: BigInt, #[realm] realm: &Realm) -> ObjectHandle {
+        Self::new(epoch, realm).into_object()
+    }
+}
