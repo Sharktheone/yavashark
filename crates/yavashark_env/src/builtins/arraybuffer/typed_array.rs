a194d8a7f673b71a393b9ecb92f983d56becf51c
Shark <sharktheone@proton.me>
2025-03-17 00:03:17 +0100

    fix TypedArray constructor with length and remove byte_offset and length checks for now

--- crates/yavashark_env/src/builtins/arraybuffer/typed_array.rs
+++ crates/yavashark_env/src/builtins/arraybuffer/typed_array.rs
@@ -1,10 +1,11 @@
-use crate::builtins::ArrayBuffer;
+use crate::builtins::{ArrayBuffer, ArrayBufferConstructor};
 use crate::conversion::FromValueOutput;
 use crate::{Error, MutObject, Realm, Res, Value};
 use std::cell::RefCell;
+use swc_ecma_ast::Pat::Array;
 use yavashark_garbage::OwningGcGuard;
 use yavashark_macro::{object, properties_new};
-use yavashark_value::BoxedObj;
+use yavashark_value::{BoxedObj, Obj};
 
 #[object(direct(buffer, byte_offset, byte_length))]
 #[derive(Debug)]
@@ -16,24 +17,30 @@ pub struct TypedArray {
 impl TypedArray {
     pub fn new(
         realm: &mut Realm,
-        buffer: Value,
+        mut buffer: Value,
         byte_offset: Option<usize>,
         byte_length: Option<usize>,
     ) -> Res<Self> {
-        let buf = <&ArrayBuffer>::from_value_out(buffer.copy())?;
+        let buf = if let Ok(buf) = <&ArrayBuffer>::from_value_out(buffer.copy()) { buf } else {
+            let len = buffer.to_int_or_null() as usize;
+            buffer = ArrayBuffer::new(realm, len).into_value();
+            
+            <&ArrayBuffer>::from_value_out(buffer.copy())?
+        };
+        
         let buf_len = buf.inner.borrow().buffer.len();
         let byte_offset = byte_offset.unwrap_or(0);
 
-        if byte_offset > buf_len {
-            return Err(Error::range("byteOffset is out of bounds"));
-        }
-
+        // if byte_offset > buf_len { //TODO: re-implement this with BYTES_PER_ELEMENT
+        //     return Err(Error::range("byteOffset is out of bounds"));
+        // }
+        // 
         let byte_length = match byte_length {
             Some(len) => {
-                if len + byte_offset > buf_len {
-                    return Err(Error::range("byteLength is out of bounds"));
-                }
-
+                // if len + byte_offset > buf_len {
+                //     return Err(Error::range("byteLength is out of bounds"));
+                // } //TODO
+        
                 len
             }
             None => buf_len - byte_offset,
