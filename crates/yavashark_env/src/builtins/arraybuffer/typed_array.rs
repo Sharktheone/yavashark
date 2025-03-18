c92cac2f46e3f2e468ec697143a7fc67820b0bff
Shark <sharktheone@proton.me>
2025-03-18 23:23:56 +0100

    allow typed array creation from an array of numbers

--- crates/yavashark_env/src/builtins/arraybuffer/typed_array.rs
+++ crates/yavashark_env/src/builtins/arraybuffer/typed_array.rs
@@ -41,26 +41,40 @@ impl TypedArray {
         byte_length: Option<usize>,
         ty: Type,
     ) -> Res<Self> {
-        let buf = if let Ok(buf) = <&ArrayBuffer>::from_value_out(buffer.copy()) { buf } else {
+        let buf = if let Ok(buf) = <&ArrayBuffer>::from_value_out(buffer.copy()) {
+            buf
+        } else if buffer.contains_key(&"length".into())? {
+            let iter = ValueIterator::new(&buffer, realm)?;
+
+            let mut items = Vec::new();
+
+            while let Some(item) = iter.next(realm)? {
+                items.push(item);
+            }
+
+            buffer = convert_buffer(items, ty, realm)?.into_value();
+
+            <&ArrayBuffer>::from_value_out(buffer.copy())?
+        } else {
             let len = buffer.to_int_or_null() as usize;
             buffer = ArrayBuffer::new(realm, len).into_value();
-            
+
             <&ArrayBuffer>::from_value_out(buffer.copy())?
         };
-        
+
         let buf_len = buf.inner.borrow().buffer.len();
         let byte_offset = byte_offset.unwrap_or(0);
 
         // if byte_offset > buf_len { //TODO: re-implement this with BYTES_PER_ELEMENT
         //     return Err(Error::range("byteOffset is out of bounds"));
         // }
-        // 
+        //
         let byte_length = match byte_length {
             Some(len) => {
                 // if len + byte_offset > buf_len {
                 //     return Err(Error::range("byteLength is out of bounds"));
                 // } //TODO
-        
+
                 len
             }
             None => buf_len - byte_offset,
@@ -74,6 +88,7 @@ impl TypedArray {
                 byte_length: byte_length.into(),
             }),
             byte_offset,
+            byte_length,
             ty,
         })
     }
@@ -85,6 +100,64 @@ impl TypedArray {
 
         <&ArrayBuffer>::from_value_out(buf)
     }
+
+fn convert_buffer(items: Vec<Value>, ty: Type, realm: &mut Realm) -> Res<ArrayBuffer> {
+    let len = items.len()
+        * match ty {
+            Type::U8 => 1,
+            Type::U16 => 2,
+            Type::U32 => 4,
+            Type::U64 => 8,
+            Type::I8 => 1,
+            Type::I16 => 2,
+            Type::I32 => 4,
+            Type::I64 => 8,
+            Type::F16 => 2,
+            Type::F32 => 4,
+            Type::F64 => 8,
+        };
+
+    let mut buffer = Vec::with_capacity(len);
+
+    for item in items {
+        match ty {
+            Type::U8 => {
+                buffer.push(item.to_number(realm)? as u8);
+            }
+            Type::U16 => {
+                buffer.extend_from_slice(&(item.to_number(realm)? as u16).to_le_bytes());
+            }
+            Type::U32 => {
+                buffer.extend_from_slice(&(item.to_number(realm)? as u32).to_le_bytes());
+            }
+            Type::U64 => {
+                buffer.extend_from_slice(&(item.to_number(realm)? as u64).to_le_bytes());
+            }
+            Type::I8 => {
+                buffer.extend_from_slice(&(item.to_number(realm)? as i8).to_le_bytes());
+            }
+            Type::I16 => {
+                buffer.extend_from_slice(&(item.to_number(realm)? as i16).to_le_bytes());
+            }
+            Type::I32 => {
+                buffer.extend_from_slice(&(item.to_number(realm)? as i32).to_le_bytes());
+            }
+            Type::I64 => {
+                buffer.extend_from_slice(&(item.to_number(realm)? as i64).to_le_bytes());
+            }
+            Type::F16 => {
+                buffer.extend_from_slice(&(f16::from_f64(item.to_number(realm)?)).to_le_bytes());
+            }
+            Type::F32 => {
+                buffer.extend_from_slice(&(item.to_number(realm)? as f32).to_le_bytes());
+            }
+            Type::F64 => {
+                buffer.extend_from_slice(&(item.to_number(realm)? as f64).to_le_bytes());
+            }
+        }
+    }
+
+    Ok(ArrayBuffer::from_buffer(realm, buffer))
 }
 
 #[properties_new]
