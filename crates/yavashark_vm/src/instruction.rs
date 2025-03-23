c5a008c6c837ed4ac552143978f5ad8fb46e14f4
Shark <sharktheone@proton.me>
2025-03-23 16:50:17 +0100

    implement bitwise instructions

--- crates/yavashark_vm/src/instruction.rs
+++ crates/yavashark_vm/src/instruction.rs
@@ -1,5 +1,7 @@
 mod math;
 mod logical;
+mod bit;
 
 pub use math::*;
 pub use logical::*;
+pub use bit::*;
---
+++ crates/yavashark_vm/src/instruction/bit.rs
@@ -0,0 +1,28 @@
+use yavashark_env::Res;
+use crate::data::{Data, OutputData};
+use crate::VM;
+
+
+pub fn b_xor(left: impl Data, right: impl Data, output: impl OutputData, vm: &mut impl VM) -> Res {
+    let left = left.get(vm)?;
+    let right = right.get(vm)?;
+    let result = left.xor(&right, vm.get_realm())?;
+
+    output.set(result, vm)
+}
+
+pub fn b_or(left: impl Data, right: impl Data, output: impl OutputData, vm: &mut impl VM) -> Res {
+    let left = left.get(vm)?;
+    let right = right.get(vm)?;
+    let result = left.or(&right, vm.get_realm())?;
+
+    output.set(result, vm)
+}
+
+pub fn b_and(left: impl Data, right: impl Data, output: impl OutputData, vm: &mut impl VM) -> Res {
+    let left = left.get(vm)?;
+    let right = right.get(vm)?;
+    let result = left.and(&right, vm.get_realm())?;
+
+    output.set(result, vm)
+}
