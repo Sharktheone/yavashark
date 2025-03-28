75a1196bdc25187dd72d1f775bda71881118fbfd
Shark <sharktheone@proton.me>
2025-03-28 19:59:38 +0100

    implement full AST has_call check

--- crates/yavashark_compiler/src/node.rs
+++ crates/yavashark_compiler/src/node.rs
@@ -1,7 +1,4 @@
-use swc_ecma_ast::{
-    BlockStmt, Class, ClassDecl, ClassMember, DebuggerStmt, Decl, Expr, ExprStmt, ForHead, Pat,
-    PropName, Stmt, UsingDecl, VarDeclOrExpr, WithStmt,
-};
+use swc_ecma_ast::{AssignTarget, AssignTargetPat, BlockStmt, Class, ClassDecl, ClassMember, DebuggerStmt, Decl, Expr, ExprStmt, ForHead, MemberProp, OptChainBase, Pat, Prop, PropName, PropOrSpread, SimpleAssignTarget, Stmt, SuperProp, UsingDecl, VarDeclOrExpr, WithStmt};
 
 pub trait ASTNode {
     fn has_call(&self) -> bool;
@@ -272,6 +269,107 @@ impl ASTNode for PropName {
 
 impl ASTNode for Expr {
     fn has_call(&self) -> bool {
-        todo!()
+        match self {
+            Self::Array(a) => a.elems.iter().any(|i| i.as_ref().is_some_and(|expr| expr.expr.has_call())),
+            Self::Object(o) => o.props.iter().any(ASTNode::has_call),
+            Self::Unary(u) => u.arg.has_call(),
+            Self::Update(u) => u.arg.has_call(),
+            Self::Bin(b) => b.left.has_call() || b.right.has_call(),
+            Self::Assign(a) => a.left.has_call() || a.right.has_call(),
+            Self::Member(m) => m.obj.has_call(),
+            Self::SuperProp(s) => s.prop.has_call(),
+            Self::Cond(c) => c.test.has_call() || c.cons.has_call() || c.alt.has_call(),
+            Self::Seq(s) => s.exprs.iter().any(|e| e.has_call()),
+            Self::Tpl(tpl) => tpl.exprs.iter().any(|e| e.has_call()),
+            Self::TaggedTpl(_) => true,
+            Self::Class(c) => c.class.has_call(),
+            Self::Yield(y) => y.arg.as_deref().is_some_and(ASTNode::has_call),
+            Self::Await(a) => a.arg.has_call(),
+            Self::Paren(p) => p.expr.has_call(),
+            Self::OptChain(o) => o.base.has_call(),
+            
+            _ => false,
+        }
+    }
+}
+
+
+impl ASTNode for PropOrSpread {
+    fn has_call(&self) -> bool {
+        match self {
+            Self::Spread(s) => s.expr.has_call(),
+            Self::Prop(e) => e.has_call(),
+        }
+    }
+}
+
+impl ASTNode for Prop {
+    fn has_call(&self) -> bool {
+        match self {
+            Self::Shorthand(_) => false,
+            Self::KeyValue(kv) => kv.key.has_call() || kv.value.has_call(),
+            Self::Assign(a) => a.value.has_call(),
+            Self::Getter(g) => g.key.has_call(), 
+            Self::Setter(s) => s.key.has_call(),
+            Self::Method(m) => m.key.has_call(),
+        }
+    }
+}
+
+impl ASTNode for AssignTarget {
+    fn has_call(&self) -> bool {
+        match self {
+            Self::Simple(s) => s.has_call(),
+            Self::Pat(p) => p.has_call(),
+        }
+    }
+}
+
+impl ASTNode for SimpleAssignTarget {
+    fn has_call(&self) -> bool {
+        match self {
+            Self::Member(m) => m.obj.has_call() | m.prop.has_call(),
+            Self::SuperProp(s) => s.prop.has_call(),
+            Self::Paren(p) => p.expr.has_call(),
+            Self::OptChain(o) => o.base.has_call(),
+            _ => false
+        }
+    }
+}
+
+impl ASTNode for AssignTargetPat {
+    fn has_call(&self) -> bool {
+        match self {
+            Self::Array(a) => a.has_call(),
+            Self::Object(o) => o.has_call(),
+            Self::Invalid(_) => false,
+        }
+    }
+}
+
+impl ASTNode for SuperProp {
+    fn has_call(&self) -> bool {
+        match self { 
+            Self::Ident(_) => false,
+            Self::Computed(c) => c.expr.has_call(),
+        }
+    }
+}
+
+impl ASTNode for OptChainBase {
+    fn has_call(&self) -> bool {
+        match self {
+            Self::Member(m) => m.obj.has_call() || m.prop.has_call(),
+            Self::Call(_) => true,
+        }
+    }
+}
+
+impl ASTNode for MemberProp {
+    fn has_call(&self) -> bool {
+        match self {
+            Self::Ident(_) | Self::PrivateName(_) => false,
+            Self::Computed(c) => c.expr.has_call(),
+        }
     }
 }
