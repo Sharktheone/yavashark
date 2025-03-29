2eab72365453eba718d056d130b64e42cef01e71
Shark <sharktheone@proton.me>
2025-03-29 01:02:13 +0100

    fix length property with #[props]

--- crates/yavashark_macro/src/properties_new/method.rs
+++ crates/yavashark_macro/src/properties_new/method.rs
@@ -79,7 +79,16 @@ impl Method {
             .clone()
             .map(|js| quote! { #js })
             .unwrap_or_else(|| quote! { stringify!(#name_ident) });
-        let length = self.args.len();
+        let mut length = self.args.len();
+        
+        if self.this.is_some() {
+            length -= 1;
+        }
+        if self.realm.is_some() {
+            length -= 1;
+        }
+        
+        
 
         quote! {
             #native_function::with_proto_and_len(#js_name.as_ref(), |mut args, mut this, realm| {
