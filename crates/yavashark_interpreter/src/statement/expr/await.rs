f37bbf56487c2f809090b1e92c034bd02cd1cadb
Shark <sharktheone@proton.me>
2025-04-01 19:36:23 +0200

    implement await (slightly)

--- crates/yavashark_interpreter/src/statement/expr/await.rs
+++ crates/yavashark_interpreter/src/statement/expr/await.rs
@@ -1,3 +1,4 @@
+use swc_common::Spanned;
 use crate::Interpreter;
 use swc_ecma_ast::AwaitExpr;
 use yavashark_env::scope::Scope;
@@ -5,6 +6,6 @@ use yavashark_env::{Realm, RuntimeResult};
 
 impl Interpreter {
     pub fn run_await(realm: &mut Realm, stmt: &AwaitExpr, scope: &mut Scope) -> RuntimeResult {
-        todo!()
+        Self::run_expr(realm, &stmt.arg, stmt.arg.span(), scope)
     }
 }
