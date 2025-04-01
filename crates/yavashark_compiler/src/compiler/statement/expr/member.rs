7eb34671aae5c6f052ce372593308d3ab2b1f980
Shark <sharktheone@proton.me>
2025-04-02 01:59:47 +0200

    member

--- crates/yavashark_compiler/src/compiler/statement/expr/member.rs
+++ crates/yavashark_compiler/src/compiler/statement/expr/member.rs
@@ -1,9 +1,35 @@
-use swc_ecma_ast::MemberExpr;
+use swc_ecma_ast::{MemberExpr, MemberProp};
+use yavashark_bytecode::ConstValue;
 use yavashark_bytecode::data::OutputData;
+use yavashark_bytecode::instructions::Instruction;
 use crate::{Compiler, Res};
 
 impl Compiler {
     pub fn compile_member(&mut self, expr: &MemberExpr, out: Option<impl OutputData>) -> Res {
-        todo!()
+        let Some(out) = out else {
+            return Ok(())
+        };
+        
+        let member = match &expr.prop {
+            MemberProp::Ident(ident) => {
+                self.alloc_const(ConstValue::String(ident.sym.as_str().to_string())).into() //TODO: this should rather be stored in var names
+            }
+            MemberProp::Computed(expr) => {
+                let out = self.alloc_reg_or_stack();
+                self.compile_expr_data(&expr.expr, Some(out))?
+            }
+            MemberProp::PrivateName(_) => {
+                todo!()
+            }
+        };
+        
+        
+        let prop = self.compile_expr_data_acc(&expr.obj)?;
+        
+        self.instructions.push(Instruction::load_member(prop, member, out));
+        self.dealloc(prop);
+        self.dealloc(member);
+        
+        Ok(())
     }
 }
