2a2eea62e62da77e6eee7a9209652a03ee6df6e1
Shark <sharktheone@proton.me>
2025-04-05 10:55:52 +0200

    fn decl

--- crates/yavashark_compiler/src/compiler/statement/decl/fn_.rs
+++ crates/yavashark_compiler/src/compiler/statement/decl/fn_.rs
@@ -1,8 +1,15 @@
 use swc_ecma_ast::FnDecl;
+use yavashark_bytecode::instructions::Instruction;
 use crate::Compiler;
 
 impl Compiler {
     pub fn decl_fn(&mut self, func: &FnDecl) -> crate::Res {
-        todo!()
+        let f = self.create_function(&func.function, Some(func.ident.sym.to_string()))?;
+        
+        let name = self.alloc_var(func.ident.sym.as_str());
+        
+        self.instructions.push(Instruction::move_(f, name));
+        
+        Ok(())
     }
 }
