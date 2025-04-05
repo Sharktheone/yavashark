8212e9f5358575f7fd1b5f1c1adbb4b71c218842
Shark <sharktheone@proton.me>
2025-04-05 10:55:58 +0200

    fn statement

--- crates/yavashark_compiler/src/compiler/statement/expr/fn_.rs
+++ crates/yavashark_compiler/src/compiler/statement/expr/fn_.rs
@@ -2,6 +2,7 @@ use super::MoveOptimization;
 use crate::{Compiler, Res};
 use swc_ecma_ast::FnExpr;
 use yavashark_bytecode::data::OutputData;
+use yavashark_bytecode::instructions::Instruction;
 
 impl Compiler {
     pub fn compile_fn(
@@ -9,6 +10,12 @@ impl Compiler {
         expr: &FnExpr,
         out: Option<impl OutputData>,
     ) -> Res<Option<MoveOptimization>> {
-        todo!()
+        let Some(out)  = out else {
+            return Ok(None);
+        };
+        
+        let f = self.create_function(&expr.function, None)?;
+        
+        Ok(Some(MoveOptimization::new(f, vec![Instruction::move_(f, out)])))
     }
 }
