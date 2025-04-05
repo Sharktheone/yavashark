c7e0eef35430ce28a70b054afaf9a241a72330c9
Shark <sharktheone@proton.me>
2025-04-05 11:28:38 +0200

    compile new

--- crates/yavashark_compiler/src/compiler/statement/expr/call.rs
+++ crates/yavashark_compiler/src/compiler/statement/expr/call.rs
@@ -6,22 +6,8 @@ use yavashark_bytecode::instructions::Instruction;
 
 impl Compiler {
     pub fn compile_call(&mut self, expr: &CallExpr, out: Option<impl OutputData>) -> Res {
-        let args_have_call = expr.args.iter().any(|arg| arg.expr.has_call());
-
-        if args_have_call {
-            todo!()
-        } else {
-            for arg in &expr.args {
-                let out = self.compile_expr_data_acc(&arg.expr)?;
-
-                if arg.spread.is_some() {
-                    self.instructions.push(Instruction::spread_call(out));
-                } else {
-                    self.instructions.push(Instruction::push_call(out));
-                }
-            }
-        }
-
+        self.compile_call_args(&expr.args)?;
+        
         let callee = match &expr.callee {
             Callee::Expr(expr) => self.compile_expr_data_acc(expr)?,
             Callee::Super(_) => {
@@ -40,5 +26,29 @@ impl Compiler {
         }
 
         Ok(())
+    }
+    
+    pub fn compile_call_args(
+        &mut self,
+        args: &[swc_ecma_ast::ExprOrSpread],
+    ) -> Res {
+        let args_have_call = args.iter().any(|arg| arg.expr.has_call());
+
+        if args_have_call {
+            todo!()
+        } else {
+            for arg in args {
+                let out = self.compile_expr_data_acc(&arg.expr)?;
+
+                if arg.spread.is_some() {
+                    self.instructions.push(Instruction::spread_call(out));
+                } else {
+                    self.instructions.push(Instruction::push_call(out));
+                }
+            }
+        }
+        
+        Ok(())
+
     }
 }
--- crates/yavashark_compiler/src/compiler/statement/expr/new.rs
+++ crates/yavashark_compiler/src/compiler/statement/expr/new.rs
@@ -1,9 +1,27 @@
 use crate::{Compiler, Res};
 use swc_ecma_ast::NewExpr;
 use yavashark_bytecode::data::OutputData;
+use yavashark_bytecode::instructions::Instruction;
 
 impl Compiler {
     pub fn compile_new(&mut self, expr: &NewExpr, out: Option<impl OutputData>) -> Res {
-        todo!()
+        if let Some(args) = &expr.args {
+            self.compile_call_args(args)?;
+        }
+        
+        let callee = self.compile_expr_data_acc(&expr.callee)?;
+        
+        match out {
+            Some(out) => {
+                self.instructions.push(Instruction::construct(callee, out));
+            }
+            None => {
+                self.instructions.push(Instruction::construct_no_output(callee));
+            }
+        }
+        
+        Ok(())
+        
+        
     }
 }
