e44cedd948795ebefc26cfad8cf0ce7ae138ec1e
Shark <sharktheone@proton.me>
2025-04-23 17:45:06 +0200

    compile yield

--- crates/yavashark_compiler/src/compiler/statement/expr/yield_.rs
+++ crates/yavashark_compiler/src/compiler/statement/expr/yield_.rs
@@ -1,9 +1,16 @@
 use crate::{Compiler, Res};
 use swc_ecma_ast::YieldExpr;
 use yavashark_bytecode::data::OutputData;
+use yavashark_bytecode::instructions::Instruction;
 
 impl Compiler {
     pub fn compile_yield(&mut self, expr: &YieldExpr, out: Option<impl OutputData>) -> Res {
-        todo!()
+        if let Some(arg) = &expr.arg {
+            let arg = self.compile_expr_data_acc(arg)?;
+            
+            self.instructions.push(Instruction::yield_(arg));
+        }
+        
+        Ok(())
     }
 }
