36540b637ceeddeeb9c819bebb1dcb0172427dd5
Shark <sharktheone@proton.me>
2025-04-24 23:44:10 +0200

    compile while

--- crates/yavashark_compiler/src/compiler/statement/while_.rs
+++ crates/yavashark_compiler/src/compiler/statement/while_.rs
@@ -1,8 +1,31 @@
 use crate::{Compiler, Res};
 use swc_ecma_ast::WhileStmt;
+use yavashark_bytecode::instructions::Instruction;
+use yavashark_bytecode::jmp::Test;
 
 impl Compiler {
     pub fn compile_while(&mut self, w: &WhileStmt) -> Res {
-        todo!()
+        let addr = self.instructions.len();
+        let cond = self.compile_test_expr(&w.test)?;
+
+        if cond == Test::Always {
+            self.compile_stmt(&w.body)?;
+            self.instructions.push(Instruction::jmp(addr));
+        } else if cond == Test::Never {
+            return Ok(());
+        } else {
+            let jmp = self.instructions.len();
+            self.instructions.push(Instruction::JmpRel(0));
+
+            self.compile_stmt(&w.body)?;
+
+            if let Some(inst) = cond.get(self.instructions.len() as yavashark_bytecode::JmpAddr) {
+                self.instructions[jmp] = inst;
+            }
+            
+            self.instructions.push(Instruction::jmp(addr));
+        }
+
+        Ok(())
     }
 }
