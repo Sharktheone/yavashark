12ebf638deacca5850b2ab9250b493ea38242a7a
Shark <sharktheone@proton.me>
2025-04-26 23:59:51 +0200

    implement do-while

--- crates/yavashark_compiler/src/compiler/statement/do_while.rs
+++ crates/yavashark_compiler/src/compiler/statement/do_while.rs
@@ -1,8 +1,28 @@
 use crate::{Compiler, Res};
 use swc_ecma_ast::DoWhileStmt;
+use yavashark_bytecode::instructions::Instruction;
+use yavashark_bytecode::jmp::Test;
 
 impl Compiler {
     pub fn compile_do_while(&mut self, d: &DoWhileStmt) -> Res {
-        todo!()
+        let start = self.instructions.len();
+
+        self.compile_stmt(&d.body)?;
+
+        let cond = self.compile_test_expr(&d.test)?;
+
+        match cond {
+            Test::Always => {
+                self.instructions.push(Instruction::jmp(start));
+            }
+            Test::Never => {},
+            _ => {
+                if let Some(inst) = cond.get(start as yavashark_bytecode::JmpAddr) {
+                    self.instructions.push(inst);
+                }
+            }
+        }
+
+        Ok(())
     }
 }
