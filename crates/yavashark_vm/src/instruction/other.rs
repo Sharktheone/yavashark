2cd707a1a1a1c760d510635f00083ed62123bd68
Shark <sharktheone@proton.me>
2025-05-01 16:52:20 +0200

    me stupid

--- crates/yavashark_vm/src/instruction/other.rs
+++ crates/yavashark_vm/src/instruction/other.rs
@@ -233,39 +233,39 @@ pub fn leave_try(vm: &mut impl VM) -> Res {
 }
 
 
-fn pat_begin_rest(len: usize, vm: &mut impl VM) -> Res {
+pub fn pat_begin_rest(len: usize, vm: &mut impl VM) -> Res {
     todo!()
 }
-fn pat_void_next(vm: &mut impl VM) -> Res {
-    todo!()
-}
-
-fn pat_move_let(_: impl Data, _: impl Data, vm: &mut impl VM) -> Res {
-    todo!()
-}
-fn pat_move_const(_: impl Data, _: impl Data, vm: &mut impl VM) -> Res {
-    todo!()
-}
-fn pat_move_var(_: impl Data, _: impl Data, vm: &mut impl VM) -> Res {
+pub fn pat_void_next(vm: &mut impl VM) -> Res {
     todo!()
 }
 
-fn pat_rest_let(_: impl Data, vm: &mut impl VM) -> Res {
+pub fn pat_move_let(_: impl Data, _: impl Data, vm: &mut impl VM) -> Res {
     todo!()
 }
-fn pat_rest_const(_: impl Data, vm: &mut impl VM) -> Res {
+pub fn pat_move_const(_: impl Data, _: impl Data, vm: &mut impl VM) -> Res {
     todo!()
 }
-fn pat_rest_var(_: impl Data, vm: &mut impl VM) -> Res {
+pub fn pat_move_var(_: impl Data, _: impl Data, vm: &mut impl VM) -> Res {
     todo!()
 }
 
-fn pat_array_move_let(_: impl Data, vm: &mut impl VM) -> Res {
+pub fn pat_rest_let(_: impl Data, vm: &mut impl VM) -> Res {
     todo!()
 }
-fn pat_array_move_const(_: impl Data, vm: &mut impl VM) -> Res {
+pub fn pat_rest_const(_: impl Data, vm: &mut impl VM) -> Res {
     todo!()
 }
-fn pat_array_move_var(_: impl Data, vm: &mut impl VM) -> Res {
+pub fn pat_rest_var(_: impl Data, vm: &mut impl VM) -> Res {
+    todo!()
+}
+
+pub fn pat_array_move_let(_: impl Data, vm: &mut impl VM) -> Res {
+    todo!()
+}
+pub fn pat_array_move_const(_: impl Data, vm: &mut impl VM) -> Res {
+    todo!()
+}
+pub fn pat_array_move_var(_: impl Data, vm: &mut impl VM) -> Res {
     todo!()
 }
