8028374920cd81fa8b3abbbb44c402dac702cbe1
Shark <sharktheone@proton.me>
2025-06-08 17:39:13 +0200

    allow easier testing in yavashark_test262

--- crates/yavashark_test262/src/main.rs
+++ crates/yavashark_test262/src/main.rs
@@ -2,14 +2,22 @@ use std::path::PathBuf;
 use yavashark_env::print::PrettyPrint;
 use yavashark_test262::run::run_file;
 
+const TEST262_ROOT: &str = "../../test262";
+
 fn main() {
     let mut args = std::env::args();
 
     args.next();
 
     let f = args.next().expect("please provide a test path");
+    
+    let path = if f.starts_with("test/") {
+        PathBuf::from(TEST262_ROOT).join(f)
+    } else {
+        PathBuf::from(f)
+        
+    };
 
-    let path = PathBuf::from(f);
 
     match run_file(path) {
         Err(e) => println!("FAIL:\n {}", e.pretty_print()),
