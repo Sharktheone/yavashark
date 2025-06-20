b09b095147cd4d3318893f9781ff0dfda13ad719
Shark <sharktheone@proton.me>
2025-06-20 20:23:05 +0200

    begin plain year month

--- crates/yavashark_env/src/builtins/temporal/plain_year_month.rs
+++ crates/yavashark_env/src/builtins/temporal/plain_year_month.rs
@@ -1,8 +1,44 @@
+use std::cell::RefCell;
+use temporal_rs::Calendar;
 use yavashark_macro::{object, props};
+use yavashark_string::YSString;
+use yavashark_value::Obj;
+use crate::{Error, MutObject, ObjectHandle, Realm, Res};
+use crate::builtins::temporal::utils::calendar_opt;
 
 #[object]
 #[derive(Debug)]
-pub struct PlainYearMonth {}
+pub struct PlainYearMonth {
+    year_month: temporal_rs::PlainYearMonth,
+}
+
+impl PlainYearMonth {
+    pub fn new(year_month: temporal_rs::PlainYearMonth, realm: &Realm) -> Self {
+        Self {
+            inner: RefCell::new(MutablePlainYearMonth {
+                object: MutObject::with_proto(realm.intrinsics.temporal_plain_year_month.clone().into()),
+            }),
+            year_month,
+        }
+    }
+
+}
 
 #[props]
-impl PlainYearMonth {}
+impl PlainYearMonth {
+    #[constructor]
+    pub fn construct(
+        year: i32,
+        month: u8,
+        calendar: Option<String>,
+        reference_day: Option<u8>,
+        #[realm] realm: &Realm,
+    ) -> Res<ObjectHandle> {
+        let calendar = calendar_opt(calendar)?;
+
+        let year_month = temporal_rs::PlainYearMonth::new(year, month, reference_day, calendar)
+            .map_err(Error::from_temporal)?;
+
+        Ok(Self::new(year_month, realm).into_object())
+    }
+}
