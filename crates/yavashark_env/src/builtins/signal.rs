2df4ea1d51337b71552cf58e1615fa93367e1da3
Shark <sharktheone@proton.me>
2025-06-22 14:11:47 +0200

    fix every test panicking

--- crates/yavashark_env/src/builtins/signal.rs
+++ crates/yavashark_env/src/builtins/signal.rs
@@ -1,22 +1,28 @@
 mod computed;
 mod state;
 
+use crate::{Object, ObjectHandle, Realm, Res};
 use yavashark_garbage::Gc;
 use yavashark_value::BoxedObj;
-use crate::{Object, ObjectHandle, Realm, Res};
 
 pub struct Protos {
     pub state: ObjectHandle,
     pub computed: ObjectHandle,
 }
 
-pub fn get_signal(obj_proto: ObjectHandle, func_proto: ObjectHandle) -> Res<(ObjectHandle, Protos)> {
+pub fn get_signal(
+    obj_proto: ObjectHandle,
+    func_proto: ObjectHandle,
+) -> Res<(ObjectHandle, Protos)> {
     let obj = Object::with_proto(obj_proto.clone().into());
 
-    todo!()
+    let protos = Protos {
+        state: Object::null(),
+        computed: Object::null(),
+    };
 
+    Ok((obj, protos))
 }
-
 
 pub fn notify_dependent(dep: Gc<BoxedObj<Realm>>, realm: &mut Realm) -> Res<()> {
     Ok(())
