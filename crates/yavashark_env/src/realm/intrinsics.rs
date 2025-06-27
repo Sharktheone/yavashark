1f2588ec6e17b3d037ecbe9e98ba3e0d8b11ccb3
Shark <sharktheone@proton.me>
2025-06-27 23:14:28 +0200

    fix set initialization

--- crates/yavashark_env/src/realm/intrinsics.rs
+++ crates/yavashark_env/src/realm/intrinsics.rs
@@ -13,11 +13,7 @@ use crate::builtins::uint16array::Uint16Array;
 use crate::builtins::uint32array::Uint32Array;
 use crate::builtins::uint8clampedarray::Uint8ClampedArray;
 use crate::builtins::unit8array::Uint8Array;
-use crate::builtins::{
-    get_eval_error, get_range_error, get_reference_error, get_syntax_error, get_temporal,
-    get_type_error, get_uri_error, ArrayBuffer, BigIntObj, BooleanObj, Date, Map, Math, NumberObj,
-    Promise, Reflect, RegExp, StringObj, SymbolObj, JSON,
-};
+use crate::builtins::{get_eval_error, get_range_error, get_reference_error, get_syntax_error, get_temporal, get_type_error, get_uri_error, ArrayBuffer, BigIntObj, BooleanObj, Date, Map, Math, NumberObj, Promise, Reflect, RegExp, Set, StringObj, SymbolObj, JSON};
 use crate::error::ErrorObj;
 use crate::{Error, FunctionPrototype, Object, ObjectHandle, Prototype, Res, Value, Variable};
 use rustc_hash::FxHashMap;
@@ -341,7 +337,7 @@ impl Intrinsics {
             func_prototype.clone().into(),
         )?;
 
-        let set = Map::initialize_proto(
+        let set = Set::initialize_proto(
             Object::raw_with_proto(obj_prototype.clone().into()),
             func_prototype.clone().into(),
         )?;
