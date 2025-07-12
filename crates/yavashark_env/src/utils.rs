64d470d474eb030ecbd1e4ba4fac5d263d648f13
Shark <sharktheone@proton.me>
2025-07-12 15:28:08 +0200

    add coerce object

--- crates/yavashark_env/src/utils.rs
+++ crates/yavashark_env/src/utils.rs
@@ -3,3 +3,18 @@ mod protodefault;
 
 pub use iterator::*;
 pub use protodefault::*;
+use yavashark_value::ObjectImpl;
+use crate::{Object, ObjectHandle, Realm, Res, Value};
+use crate::builtins::{BigIntObj, BooleanObj, NumberObj, StringObj, SymbolObj};
+
+pub fn coerce_object(value: Value, realm: &mut Realm) -> Res<ObjectHandle> {
+    Ok(match value {
+        Value::Object(obj) => obj,
+        Value::Number(num) => NumberObj::with_number(realm, num)?,
+        Value::String(string) => StringObj::with_string(realm, string).into_object(),
+        Value::Boolean(boolean) => BooleanObj::new(realm, boolean),
+        Value::Symbol(symbol) => SymbolObj::new(realm, symbol),
+        Value::BigInt(bigint) => BigIntObj::new(realm, bigint),
+        Value::Undefined | Value::Null => Object::new(realm),
+    })
+}
