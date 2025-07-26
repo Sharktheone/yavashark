bbbe39e4d482a24fffcea5689665a20e66eb8415
Shark <sharktheone@proton.me>
2025-07-26 23:20:28 +0200

    Signal.Computed get

--- crates/yavashark_env/src/builtins/signal/computed.rs
+++ crates/yavashark_env/src/builtins/signal/computed.rs
@@ -1,8 +1,9 @@
-use crate::{ObjectHandle, Realm, Value};
+use crate::{ObjectHandle, Realm, Value, ValueResult};
 use std::cell::Cell;
 use yavashark_garbage::Weak;
 use yavashark_macro::{object, props};
 use yavashark_value::BoxedObj;
+use crate::builtins::signal::notify_dependent;
 
 #[object]
 #[derive(Debug)]
@@ -17,4 +18,33 @@ pub struct Computed {
 }
 
 #[props]
-impl Computed {}
+impl Computed {
+    pub fn get(&self, realm: &mut Realm) -> ValueResult {
+        if self.dirty.get() {
+            self.dirty.set(false);
+            
+            let new = self.compute_fn.call(realm, Vec::new(), Value::Undefined)?;
+            
+            let mut inner = self.inner.try_borrow_mut()?;
+            
+            inner.value = new;
+            
+            // TODO: what to do if the value is the same?
+            
+            drop(inner);
+            
+            for dep in &self.dependents {
+                if let Some(dep) = dep.upgrade() {
+                    notify_dependent(&dep.into(), realm)?;
+                }
+            }
+            
+        }
+        
+        
+        let inner = self.inner.try_borrow()?;
+        
+        Ok(inner.value.clone())
+    }
+    
+}
