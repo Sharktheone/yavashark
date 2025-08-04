0f729bb5674916e6c69b937236578acca9a2380e
Shark <sharktheone@proton.me>
2025-08-04 19:42:11 +0200

    suppress stupid lints from Intrinsic traits

--- crates/yavashark_env/src/realm/initialize.rs
+++ crates/yavashark_env/src/realm/initialize.rs
@@ -1,5 +1,6 @@
 use crate::{ObjectHandle, Realm, Res};
 
+#[allow(unused_variables)]
 pub trait Intrinsic {
     fn initialize(&self, realm: &mut Realm) -> Res;
     fn get_prototype(&self, realm: &Realm) -> ObjectHandle;
