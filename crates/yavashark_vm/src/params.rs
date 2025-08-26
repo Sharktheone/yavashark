0181d284c028cf84b1160e17aeb04b8629968325
Shark <sharktheone@proton.me>
2025-08-26 23:39:58 +0200

    fix cargo check lints

--- crates/yavashark_vm/src/params.rs
+++ crates/yavashark_vm/src/params.rs
@@ -2,7 +2,7 @@ use std::ops::Deref;
 use yavashark_bytecode::BytecodeFunctionParams;
 use yavashark_env::scope::Scope;
 use yavashark_env::{Error, Realm, Res, Value};
-use crate::{BorrowedVM, VmState, VM};
+use crate::{BorrowedVM, VM};
 
 #[derive(Debug, PartialEq, Clone, Default)]
 pub struct VMParams(BytecodeFunctionParams);
@@ -24,25 +24,25 @@ impl Deref for VMParams {
 impl VMParams {
     pub fn execute(&self, args: &[Value], mut scope: Scope, realm: &mut Realm) -> Res {
         let mut prev_i = 0;
-        
+
         for (i, param) in self.0.defs.iter().enumerate() {
-            
+
             let Some(instructions) = self.instructions.get(prev_i..*param as usize) else {
                 return Err(Error::new("Invalid parameter instructions"));
             };
-            
+
             prev_i = *param as usize;
-            
+
             let mut vm = BorrowedVM::with_scope(instructions, &self.ds, realm, scope);
             let arg = args.get(i).cloned().unwrap_or(Value::Undefined);
-            
+
             vm.set_acc(arg);
-            
+
             vm.run()?;
-            
+
             scope = vm.current_scope;
         }
-        
+
         Ok(())
     }
 }
