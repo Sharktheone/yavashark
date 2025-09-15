8079f1a18f3aacb2fb94ab3f7091ebd438ba71f7
Shark <sharktheone@proton.me>
2025-09-15 21:33:38 +0200

    module items validator

--- crates/yavashark_swc_validator/src/lib.rs
+++ crates/yavashark_swc_validator/src/lib.rs
@@ -1,4 +1,4 @@
-use swc_ecma_ast::Stmt;
+use swc_ecma_ast::{ModuleItem, Stmt};
 
 pub fn validate_statements(ast: &[Stmt]) -> Result<(), String> {
     for stmt in ast {
@@ -8,5 +8,20 @@ pub fn validate_statements(ast: &[Stmt]) -> Result<(), String> {
 }
 
 pub fn validate_statement(stmt: &Stmt) -> Result<(), String> {
+    Ok(())
+}
+
+
+pub fn validate_module_items(ast: &[ModuleItem]) -> Result<(), String> {
+    for item in ast {
+        match item {
+            ModuleItem::Stmt(stmt) => validate_statement(stmt)?,
+            ModuleItem::ModuleDecl(item) => validate_module_decl(item)?,
+        }
+    }
+    Ok(())
+}
+
+pub fn validate_module_decl(decl: &swc_ecma_ast::ModuleDecl) -> Result<(), String> {
     Ok(())
 }
