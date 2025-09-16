46b94da6a08c90eefb0f9629607a050e47cb3958
Shark <sharktheone@proton.me>
2025-09-16 20:32:51 +0200

    validate for..of loops

--- crates/yavashark_swc_validator/src/for_of.rs
+++ crates/yavashark_swc_validator/src/for_of.rs
@@ -1,8 +1,32 @@
-use swc_ecma_ast::ForOfStmt;
+use swc_ecma_ast::{ForHead, ForOfStmt};
 use crate::Validator;
 
 impl Validator {
-    pub fn validate_for_of(_for_of: &ForOfStmt) -> Result<(), String> {
-        Ok(())
+    pub fn validate_for_of(for_of: &ForOfStmt) -> Result<(), String> {
+        match &for_of.left {
+            ForHead::VarDecl(var_decl) => {
+                for decl in &var_decl.decls {
+                    if let Some(ref init) = decl.init {
+                        Self::validate_expr(init)?;
+                    }
+                    Self::validate_pat(&decl.name)?;
+                }
+            }
+            ForHead::UsingDecl(using_decl) => {
+                for decl in &using_decl.decls {
+                    if let Some(ref init) = decl.init {
+                        Self::validate_expr(init)?;
+                    }
+                    Self::validate_pat(&decl.name)?;
+                }
+            }
+            ForHead::Pat(pat) => Self::validate_pat(pat)?,
+        }
+
+
+        Self::validate_expr(&for_of.right)?;
+
+        Self::validate_statement(&for_of.body)
+
     }
 }
