6ecb58b02e507cd1cf8b7ecc163c65843e97ba68
Shark <sharktheone@proton.me>
2025-09-16 20:34:24 +0200

    validate while try stmt

--- crates/yavashark_swc_validator/src/try_.rs
+++ crates/yavashark_swc_validator/src/try_.rs
@@ -2,7 +2,18 @@ use swc_ecma_ast::TryStmt;
 use crate::Validator;
 
 impl Validator {
-    pub fn validate_try(_try: &TryStmt) -> Result<(), String> {
+    pub fn validate_try(try_: &TryStmt) -> Result<(), String> {
+        Self::validate_block(&try_.block)?;
+        if let Some(handler) = &try_.handler {
+            if let Some(param) = &handler.param {
+                Self::validate_pat(param)?;
+            }
+
+            Self::validate_block(&handler.body)?;
+        }
+        if let Some(finalizer) = &try_.finalizer {
+            Self::validate_block(finalizer)?;
+        }
         Ok(())
     }
 }
