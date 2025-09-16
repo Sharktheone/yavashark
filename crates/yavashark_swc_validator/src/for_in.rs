55c1eb0816124bc197f8c10830eff4949f37714c
Shark <sharktheone@proton.me>
2025-09-16 20:32:45 +0200

    validate for..in loops

--- crates/yavashark_swc_validator/src/for_in.rs
+++ crates/yavashark_swc_validator/src/for_in.rs
@@ -1,8 +1,32 @@
-use swc_ecma_ast::ForInStmt;
+use swc_ecma_ast::{ForHead, ForInStmt};
 use crate::Validator;
 
 impl Validator {
-    pub fn validate_for_in(_for_in: &ForInStmt) -> Result<(), String> {
-        Ok(())
+    pub fn validate_for_in(for_in: &ForInStmt) -> Result<(), String> {
+        match &for_in.left {
+            ForHead::VarDecl(var_decl) => {
+                for decl in &var_decl.decls {
+                    if let Some(ref init) = decl.init {
+                        Self::validate_expr(init)?;
+                    }
+                    Self::validate_pat(&decl.name)?;
+                }
+            }
+            ForHead::UsingDecl(using_decl) => {
+                for decl in &using_decl.decls {
+                    if let Some(ref init) = decl.init {
+                        Self::validate_expr(init)?;
+                    }
+                    Self::validate_pat(&decl.name)?;
+                }
+            }
+            ForHead::Pat(pat) => Self::validate_pat(pat)?,
+        }
+        
+        
+        Self::validate_expr(&for_in.right)?;
+        
+        Self::validate_statement(&for_in.body)
+        
     }
 }
