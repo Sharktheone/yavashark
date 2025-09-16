5f509536a5e29a3ae4ca68d3ae624486a421efd0
Shark <sharktheone@proton.me>
2025-09-16 20:34:21 +0200

    validate while return

--- crates/yavashark_swc_validator/src/ret.rs
+++ crates/yavashark_swc_validator/src/ret.rs
@@ -2,7 +2,11 @@ use swc_ecma_ast::ReturnStmt;
 use crate::Validator;
 
 impl Validator {
-    pub fn validate_return(_ret: &ReturnStmt) -> Result<(), String> {
+    pub fn validate_return(ret: &ReturnStmt) -> Result<(), String> {
+        if let Some(arg) = &ret.arg {
+            Self::validate_expr(arg)?;
+        }
+        
         Ok(())
     }
 }
