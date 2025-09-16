180bcc65470f71e8ef26648d94323458aff5db51
Shark <sharktheone@proton.me>
2025-09-16 20:32:40 +0200

    validate for loops

--- crates/yavashark_swc_validator/src/for_.rs
+++ crates/yavashark_swc_validator/src/for_.rs
@@ -1,8 +1,33 @@
-use swc_ecma_ast::ForStmt;
+use swc_ecma_ast::{ForStmt, VarDeclOrExpr};
 use crate::Validator;
 
 impl Validator {
-    pub fn validate_for(_for: &ForStmt) -> Result<(), String> {
-        Ok(())
+    pub fn validate_for(for_: &ForStmt) -> Result<(), String> {
+        if let Some(init) = &for_.init {
+            match init {
+                VarDeclOrExpr::VarDecl(var_decl) => {
+                    for decl in &var_decl.decls {
+                        Self::validate_pat(&decl.name)?;
+
+                        if let Some(init) = &decl.init {
+                            Self::validate_expr(init)?;
+                        }
+                    }
+                }
+                VarDeclOrExpr::Expr(expr) => {
+                    Self::validate_expr(expr)?;
+                }
+            }
+        }
+
+        if let Some(test) = &for_.test {
+            Self::validate_expr(test)?;
+        }
+
+        if let Some(update) = &for_.update {
+            Self::validate_expr(update)?;
+        }
+
+        Self::validate_statement(&for_.body)
     }
 }
