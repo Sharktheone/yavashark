115343ac0d7238e67e78893d8c847570a69071ac
Shark <sharktheone@proton.me>
2025-09-16 20:33:10 +0200

    validate switch

--- crates/yavashark_swc_validator/src/switch.rs
+++ crates/yavashark_swc_validator/src/switch.rs
@@ -2,7 +2,14 @@ use swc_ecma_ast::SwitchStmt;
 use crate::Validator;
 
 impl Validator {
-    pub fn validate_switch(_brk: &SwitchStmt) -> Result<(), String> {
+    pub fn validate_switch(brk: &SwitchStmt) -> Result<(), String> {
+        Self::validate_expr(&brk.discriminant)?;
+        for case in &brk.cases {
+            if let Some(test) = &case.test {
+                Self::validate_expr(test)?;
+            }
+            Self::validate_statements(&case.cons)?;
+        }
         Ok(())
     }
 }
