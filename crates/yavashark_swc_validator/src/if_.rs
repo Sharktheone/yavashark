058c9f65e1960e7187d5092a663567457c6b89ef
Shark <sharktheone@proton.me>
2025-09-16 20:33:02 +0200

    validate if

--- crates/yavashark_swc_validator/src/if_.rs
+++ crates/yavashark_swc_validator/src/if_.rs
@@ -2,7 +2,13 @@ use swc_ecma_ast::IfStmt;
 use crate::Validator;
 
 impl Validator {
-    pub fn validate_if(_if: &IfStmt) -> Result<(), String> {
+    pub fn validate_if(if_: &IfStmt) -> Result<(), String> {
+        Self::validate_expr(&if_.test)?;
+        Self::validate_statement(&if_.cons)?;
+        if let Some(alt) = &if_.alt {
+            Self::validate_statement(alt)?;
+        }
+        
         Ok(())
     }
 }
