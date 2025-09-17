9f8a9985286ae3aa6e0c570dd4ead719737cdaf0
Shark <sharktheone@proton.me>
2025-09-17 23:18:58 +0200

    begin object expr validation

--- crates/yavashark_swc_validator/src/expr/object.rs
+++ crates/yavashark_swc_validator/src/expr/object.rs
@@ -1,8 +1,19 @@
-use swc_ecma_ast::ObjectLit;
+use swc_ecma_ast::{ObjectLit, PropOrSpread};
 use crate::Validator;
 
 impl Validator {
     pub fn validate_object_expr(object: &ObjectLit) -> Result<(), String> {
+        for prop in &object.props {
+            match prop {
+                PropOrSpread::Prop(_p) => {
+                    //TODO
+                }
+                PropOrSpread::Spread(spread) => {
+                    Self::validate_expr(&spread.expr)?;
+                }
+            }
+        }
+
         Ok(())
     }
 }
