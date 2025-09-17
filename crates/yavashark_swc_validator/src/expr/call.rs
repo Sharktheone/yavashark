6dab405d88b58ba0f620c8bbf687824da9dc1688
Shark <sharktheone@proton.me>
2025-09-17 23:18:38 +0200

    implement call expr validation

--- crates/yavashark_swc_validator/src/expr/call.rs
+++ crates/yavashark_swc_validator/src/expr/call.rs
@@ -1,8 +1,19 @@
-use swc_ecma_ast::CallExpr;
+use swc_ecma_ast::{CallExpr, Callee};
 use crate::Validator;
 
 impl Validator {
     pub fn validate_call_expr(call: &CallExpr) -> Result<(), String> {
+        match &call.callee {
+            Callee::Expr(expr) => Self::validate_expr(expr)?,
+            Callee::Super(_) => {},
+            Callee::Import(_) => {},
+        }
+        
+        for arg in &call.args {
+            Self::validate_expr(&arg.expr)?;
+        }
+        
         Ok(())
+        
     }
 }
