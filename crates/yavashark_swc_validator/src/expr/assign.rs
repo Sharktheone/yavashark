33aa02c6610ae64a8233e06037e5ef282cf0c2dd
Shark <sharktheone@proton.me>
2025-09-17 22:59:56 +0200

    implement assign expr validation

--- crates/yavashark_swc_validator/src/expr/assign.rs
+++ crates/yavashark_swc_validator/src/expr/assign.rs
@@ -1,8 +1,34 @@
-use swc_ecma_ast::AssignExpr;
+use swc_ecma_ast::{AssignExpr, AssignTarget, AssignTargetPat, Pat, SimpleAssignTarget};
 use crate::Validator;
 
 impl Validator {
     pub fn validate_assign_expr(assign: &AssignExpr) -> Result<(), String> {
-        Ok(())
+        match &assign.left {
+            AssignTarget::Simple(simple) => {
+                match simple {
+                    SimpleAssignTarget::Ident(ident) => Self::validate_ident(&ident.id)?,
+                    SimpleAssignTarget::Member(member) => Self::validate_member_expr(member)?,
+                    SimpleAssignTarget::SuperProp(prop) => {
+                        Self::validate_super_prop_expr(prop)?;
+                    }
+                    SimpleAssignTarget::Paren(paren) => {
+                        Self::validate_expr(&paren.expr)?;
+                    }
+                    SimpleAssignTarget::OptChain(optchain) => {
+                        Self::validate_opt_chain_expr(optchain)?;
+                    }
+                    _ => return Err("Unsupported simple assignment target".to_string()),
+                }
+            },
+            AssignTarget::Pat(pat) => {
+                match pat {
+                    AssignTargetPat::Object(obj) => Self::validate_pat(&Pat::Object(obj.clone()))?,
+                    AssignTargetPat::Array(arr) => Self::validate_pat(&Pat::Array(arr.clone()))?,
+                    AssignTargetPat::Invalid(_) => Err("Invalid assignment target pattern".to_string())?,
+                }
+            }
+        }
+
+        Self::validate_expr(&assign.right)
     }
 }
