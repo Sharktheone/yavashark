d6f42485a18a451f6dd0565d1f91d82b1288ef62
Shark <sharktheone@proton.me>
2025-09-18 21:14:04 +0200

    validate unary

--- crates/yavashark_swc_validator/src/expr/unary.rs
+++ crates/yavashark_swc_validator/src/expr/unary.rs
@@ -3,6 +3,6 @@ use crate::Validator;
 
 impl Validator {
     pub fn validate_unary_expr(unary: &UnaryExpr) -> Result<(), String> {
-        Ok(())
+        Self::validate_expr(&unary.arg)
     }
 }
