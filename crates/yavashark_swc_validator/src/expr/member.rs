3aaa1451932900d386b1caec8908e57b0a850d99
Shark <sharktheone@proton.me>
2025-09-18 21:11:28 +0200

    validate super prop and member

--- crates/yavashark_swc_validator/src/expr/member.rs
+++ crates/yavashark_swc_validator/src/expr/member.rs
@@ -1,8 +1,14 @@
-use swc_ecma_ast::MemberExpr;
+use swc_ecma_ast::{MemberExpr, MemberProp};
 use crate::Validator;
 
 impl Validator {
     pub fn validate_member_expr(member: &MemberExpr) -> Result<(), String> {
+        Self::validate_expr(&member.obj)?;
+
+        if let MemberProp::Computed(computed) = &member.prop {
+            Self::validate_expr(&computed.expr)?;
+        }
+
         Ok(())
     }
 }
--- crates/yavashark_swc_validator/src/expr/super_prop.rs
+++ crates/yavashark_swc_validator/src/expr/super_prop.rs
@@ -1,8 +1,12 @@
-use swc_ecma_ast::SuperPropExpr;
+use swc_ecma_ast::{SuperProp, SuperPropExpr};
 use crate::Validator;
 
 impl Validator {
     pub fn validate_super_prop_expr(super_prop: &SuperPropExpr) -> Result<(), String> {
+        if let SuperProp::Computed(c) = &super_prop.prop {
+            Self::validate_expr(&c.expr)?;
+        }
+
         Ok(())
     }
 }
