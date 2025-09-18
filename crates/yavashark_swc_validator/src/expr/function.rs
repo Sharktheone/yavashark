2c0f774c3d2b9c81c4389ebec6144b5e92c4ca38
Shark <sharktheone@proton.me>
2025-09-18 21:08:28 +0200

    validate function

--- crates/yavashark_swc_validator/src/expr/function.rs
+++ crates/yavashark_swc_validator/src/expr/function.rs
@@ -1,8 +1,22 @@
-use swc_ecma_ast::FnExpr;
+use swc_ecma_ast::{FnExpr, Function};
 use crate::Validator;
 
 impl Validator {
     pub fn validate_function_expr(function: &FnExpr) -> Result<(), String> {
+        Self::validate_function(&function.function)
+    }
+    
+    pub fn validate_function(function: &Function) -> Result<(), String> {
+        for param in &function.params {
+            Self::validate_pat(&param.pat)?;
+        }
+        
+        
+        if let Some(body) = &function.body {
+            Self::validate_block(body)?;
+        }
+        
+        
         Ok(())
     }
 }
