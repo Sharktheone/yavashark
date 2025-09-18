b18e8f276b19857c1e9c8b497acc402242f72b56
Shark <sharktheone@proton.me>
2025-09-18 21:14:01 +0200

    validate tpl

--- crates/yavashark_swc_validator/src/expr/tpl.rs
+++ crates/yavashark_swc_validator/src/expr/tpl.rs
@@ -3,6 +3,9 @@ use crate::Validator;
 
 impl Validator {
     pub fn validate_tpl_expr(tpl: &Tpl) -> Result<(), String> {
+        for expr in &tpl.exprs {
+            Self::validate_expr(expr)?;
+        }
         Ok(())
     }
 }
