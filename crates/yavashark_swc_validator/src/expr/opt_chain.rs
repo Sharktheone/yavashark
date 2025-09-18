8e31551238d05344731aec48d1fc95d877c8e7a4
Shark <sharktheone@proton.me>
2025-09-18 21:09:37 +0200

    validate opt chain stuff

--- crates/yavashark_swc_validator/src/expr/opt_chain.rs
+++ crates/yavashark_swc_validator/src/expr/opt_chain.rs
@@ -1,8 +1,22 @@
-use swc_ecma_ast::OptChainExpr;
+use swc_ecma_ast::{OptChainBase, OptChainExpr};
 use crate::Validator;
 
 impl Validator {
     pub fn validate_opt_chain_expr(opt_chain: &OptChainExpr) -> Result<(), String> {
+        match &*opt_chain.base {
+            OptChainBase::Member(member_expr) => {
+                Self::validate_member_expr(member_expr)?;
+            }
+            OptChainBase::Call(call) => {
+                Self::validate_expr(&call.callee)?;
+                
+                for arg in &call.args {
+                    Self::validate_expr(&arg.expr)?;
+                }
+            }
+        }
+
+
         Ok(())
     }
 }
