be3db168c4fac62da1ae47189873e4fd7b01b299
Shark <sharktheone@proton.me>
2025-09-18 20:44:37 +0200

    implement array expr validation

--- crates/yavashark_swc_validator/src/expr/array.rs
+++ crates/yavashark_swc_validator/src/expr/array.rs
@@ -3,6 +3,10 @@ use crate::Validator;
 
 impl Validator {
     pub fn validate_array_expr(array: &ArrayLit) -> Result<(), String> {
+        for elem in array.elems.iter().flatten() {
+            Self::validate_expr(&elem.expr)?;
+        }
+
         Ok(())
     }
 }
