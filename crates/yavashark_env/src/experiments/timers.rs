1d585f11b5e106bffa341f1b2eb0d0c55371bded
Shark <sharktheone@proton.me>
2025-09-20 23:49:57 +0200

    fix timer

--- crates/yavashark_env/src/experiments/timers.rs
+++ crates/yavashark_env/src/experiments/timers.rs
@@ -1,6 +1,6 @@
 use crate::builtins::{GcPromise, Promise};
 use crate::conversion::downcast_obj;
-use crate::task_queue::AsyncTask;
+use crate::task_queue::{AsyncTask, AsyncTaskQueue};
 use crate::{Error, NativeFunction, ObjectHandle, Realm, Res, Value};
 use pin_project::pin_project;
 use std::future::Future;
@@ -45,7 +45,7 @@ impl TimeoutTask {
 
         let this = Self { timer, promise, cb };
 
-        realm.queue.queue_task(this);
+        AsyncTaskQueue::queue_task(this, realm);
 
         Ok(promise_obj)
     }
