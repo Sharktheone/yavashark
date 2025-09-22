2aa5b47f3eb012da7ad206a23f775701b080b3c3
Shark <sharktheone@proton.me>
2025-09-22 23:55:28 +0200

    weak set size

--- crates/yavashark_env/src/builtins/weakset.rs
+++ crates/yavashark_env/src/builtins/weakset.rs
@@ -197,6 +197,14 @@ impl WeakSet {
         Ok(Self::with_set(realm, set).into_value())
     }
 
+
+    #[get("size")]
+    fn size(&self) -> usize {
+        let inner = self.inner.borrow();
+
+        inner.set.len()
+    }
+
     // fn entries(&self, #[realm] realm: &mut Realm) -> ValueResult {
     //     let inner = self.inner.borrow();
     //
