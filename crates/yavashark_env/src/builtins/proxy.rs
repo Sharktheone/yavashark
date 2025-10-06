117c40bc9edb7dfc027e92ab4efd790e55c7be5e
Shark <sharktheone@proton.me>
2025-10-06 23:54:21 +0200

    implement obj for proxy

--- crates/yavashark_env/src/builtins/proxy.rs
+++ crates/yavashark_env/src/builtins/proxy.rs
@@ -1,7 +1,7 @@
 #![allow(unused)]
 use crate::array::Array;
 use crate::value::{BoxedObj, DefinePropertyResult, Obj, Property};
-use crate::{Error, InternalPropertyKey, NativeFunction, Object, ObjectHandle, ObjectOrNull, ObjectProperty, PropertyKey, Realm, Res, Value, Variable};
+use crate::{Error, InternalPropertyKey, NativeFunction, Object, ObjectHandle, ObjectOrNull, ObjectProperty, PrimitiveValue, PropertyKey, Realm, Res, Value, Variable};
 use std::any::TypeId;
 use std::cell::Cell;
 use std::ops::Deref;
@@ -17,296 +17,241 @@ pub struct Proxy {
     revoke: Cell<bool>,
 }
 
-
-#[allow(unused)]
 impl Obj for Proxy {
-    fn define_property(&self, name: InternalPropertyKey, value: crate::value::Value, realm: &mut Realm) -> Res<DefinePropertyResult> {
-        todo!()
+    fn define_property(&self, name: InternalPropertyKey, value: Value, realm: &mut Realm) -> Res<DefinePropertyResult> {
+        if self.revoke.get() {
+            return self.inner.define_property(name, value, realm);
+        }
+
+        Err(Error::new("not yet implemented"))
     }
 
-    fn define_property_attributes(&self, name: InternalPropertyKey, value: crate::value::Variable, realm: &mut Realm) -> Res<DefinePropertyResult> {
-        todo!()
+    fn define_property_attributes(&self, name: InternalPropertyKey, value: Variable, realm: &mut Realm) -> Res<DefinePropertyResult> {
+        if self.revoke.get() {
+            return self.inner.define_property_attributes(name, value, realm);
+        }
+
+        Err(Error::new("not yet implemented"))
     }
 
-    fn resolve_property(&self, name: InternalPropertyKey, realm: &mut Realm) -> Res<Option<Property>> {
-        todo!()
+    fn resolve_property(&self, name: InternalPropertyKey, realm: &mut Realm) -> Result<Option<Property>, Error> {
+        if self.revoke.get() {
+            return self.inner.deref().resolve_property(name, realm);
+        }
+
+        Err(Error::new("not yet implemented"))
     }
 
-    fn get_own_property(&self, name: InternalPropertyKey, realm: &mut Realm) -> Res<Option<Property>> {
-        todo!()
+    fn get_own_property(&self, name: InternalPropertyKey, realm: &mut Realm) -> Result<Option<Property>, Error> {
+        if self.revoke.get() {
+            return self.inner.deref().get_own_property(name, realm);
+        }
+
+        Err(Error::new("not yet implemented"))
     }
 
-    fn define_getter(&self, name: InternalPropertyKey, callback: ObjectHandle, realm: &mut Realm) -> Res {
-        todo!()
+    fn define_getter(&self, name: InternalPropertyKey, value: ObjectHandle, realm: &mut Realm) -> Res {
+        if self.revoke.get() {
+            return self.inner.define_getter(name, value, realm);
+        }
+
+        Err(Error::new("not yet implemented"))
     }
 
-    fn define_setter(&self, name: InternalPropertyKey, callback: ObjectHandle, realm: &mut Realm) -> Res {
-        todo!()
+    fn define_setter(&self, name: InternalPropertyKey, value: ObjectHandle, realm: &mut Realm) -> Res {
+        if self.revoke.get() {
+            return self.inner.define_setter(name, value, realm);
+        }
+
+        Err(Error::new("not yet implemented"))
     }
 
     fn delete_property(&self, name: InternalPropertyKey, realm: &mut Realm) -> Res<Option<Property>> {
-        todo!()
+        if self.revoke.get() {
+            return self.inner.delete_property(name, realm);
+        }
+
+        Err(Error::new("not yet implemented"))
     }
 
-    fn contains_own_key(&self, name: InternalPropertyKey, realm: &mut Realm) -> Res<bool> {
-        todo!()
+    fn contains_key(&self, name: InternalPropertyKey, realm: &mut Realm) -> Result<bool, Error> {
+        if self.revoke.get() {
+            return self.inner.contains_key(name, realm);
+        }
+
+        Err(Error::new("not yet implemented"))
     }
 
-    fn contains_key(&self, name: InternalPropertyKey, realm: &mut Realm) -> Res<bool> {
-        todo!()
+    fn cotnains_own_key(&self, name: InternalPropertyKey, realm: &mut Realm) -> Result<bool, Error> {
+        if self.revoke.get() {
+            return self.inner.contains_own_key(name, realm);
+        }
+
+        Err(Error::new("not yet implemented"))
     }
 
-    fn properties(&self, realm: &mut Realm) -> Res<Vec<(PropertyKey, crate::value::Value)>> {
-        todo!()
+    fn name(&self) -> String {
+        self.inner.name()
+    }
+
+    // fn to_string(&self, realm: &mut Realm) -> Result<YSString, Error> {
+    //     self.inner.to_string(realm)
+    // }
+    //
+    // fn to_string_internal(&self) -> Result<YSString, Error> {
+    //     self.inner.to_string_internal()
+    // }
+
+    fn properties(&self, realm: &mut Realm) -> Res<Vec<(PropertyKey, Value)>> {
+        if self.revoke.get() {
+            return self.inner.properties(realm);
+        }
+
+        Err(Error::new("not yet implemented"))
     }
 
     fn keys(&self, realm: &mut Realm) -> Res<Vec<PropertyKey>> {
-        todo!()
+        if self.revoke.get() {
+            return self.inner.keys(realm);
+        }
+
+        Err(Error::new("not yet implemented"))
     }
 
-    fn values(&self, realm: &mut Realm) -> Res<Vec<crate::value::Value>> {
-        todo!()
+    fn values(&self, realm: &mut Realm) -> Res<Vec<Value>> {
+        if self.revoke.get() {
+            return self.inner.values(realm);
+        }
+
+        Err(Error::new("not yet implemented"))
     }
 
-    fn enumerable_properties(&self, realm: &mut Realm) -> Res<Vec<(PropertyKey, crate::value::Value)>> {
-        todo!()
+    fn get_array_or_done(&self, index: usize, realm: &mut Realm) -> Result<(bool, Option<Value>), Error> {
+        if self.revoke.get() {
+            return self.inner.get_array_or_done(index, realm);
+        }
+
+        Err(Error::new("not yet implemented"))
     }
 
-    fn enumerable_keys(&self, realm: &mut Realm) -> Res<Vec<PropertyKey>> {
-        todo!()
+    fn call(&self, args: Vec<Value>, this: Value, realm: &mut Realm) -> Result<Value, Error> {
+        if self.revoke.get() {
+            return self.inner.call(args, this, realm);
+        }
+
+        if let Some(apply) = self.handler.get_opt("apply", realm)? {
+            let apply = apply.to_object()?;
+
+            let arguments = Array::with_elements(realm, args)?;
+            apply.call(
+                vec![self.inner.clone().into(), this, arguments.into_value()],
+                self.handler.clone().into(),
+                realm,
+            )
+        } else {
+            self.inner.call(args, this, realm)
+        }
     }
 
-    fn enumerable_values(&self, realm: &mut Realm) -> Res<Vec<crate::value::Value>> {
-        todo!()
+    fn is_callable(&self) -> bool {
+        self.inner.is_callable()
     }
 
-    fn clear_properties(&self, realm: &mut Realm) -> Res {
-        todo!()
-    }
-
-    fn get_array_or_done(&self, idx: usize, realm: &mut Realm) -> Res<(bool, Option<crate::value::Value>)> {
-        todo!()
+    fn primitive(&self, realm: &mut Realm) -> Res<Option<PrimitiveValue>> {
+        self.inner.primitive(realm)
     }
 
     fn prototype(&self, realm: &mut Realm) -> Res<ObjectOrNull> {
-        todo!()
+        if self.revoke.get() {
+            return self.inner.prototype(realm);
+        }
+
+        Err(Error::new("not yet implemented"))
     }
 
-    fn set_prototype(&self, prototype: ObjectOrNull, realm: &mut Realm) -> Res {
-        todo!()
+    fn set_prototype(&self, proto: ObjectOrNull, realm: &mut Realm) -> Res {
+        if self.revoke.get() {
+            return self.inner.set_prototype(proto, realm);
+        }
+
+        Err(Error::new("not yet implemented"))
+    }
+
+    fn class_name(&self) -> &'static str {
+        self.inner.class_name()
+    }
+
+    fn construct(&self, args: Vec<Value>, realm: &mut Realm) -> Result<ObjectHandle, Error> {
+        if self.revoke.get() {
+            return self.inner.construct(args, realm);
+        }
+
+        if let Some(construct) = self.handler.get_opt("construct", realm)? {
+            let construct = construct.to_object()?;
+            let arguments = Array::with_elements(realm, args)?;
+            construct.call(
+                vec![self.inner.clone().into(), arguments.into_value()],
+                self.handler.clone().into(),
+                realm,
+            )?.to_object()
+        } else {
+            self.inner.construct(args, realm)
+        }
+    }
+
+    fn is_constructable(&self) -> bool {
+        self.inner.is_constructable()
+    }
+
+    unsafe fn inner_downcast(&self, ty: TypeId) -> Option<NonNull<()>> {
+        self.inner.inner_downcast(ty)
+    }
+
+    fn contains_own_key(&self, name: InternalPropertyKey, realm: &mut Realm) -> Res<bool> {
+        if self.revoke.get() {
+            return self.inner.contains_own_key(name, realm);
+        }
+
+        Err(Error::new("not yet implemented"))
+    }
+
+    fn enumerable_properties(&self, realm: &mut Realm) -> Res<Vec<(PropertyKey, crate::value::Value)>> {
+        if self.revoke.get() {
+            return self.inner.enumerable_properties(realm);
+        }
+
+        Err(Error::new("not yet implemented"))
+
+    }
+
+    fn enumerable_keys(&self, realm: &mut Realm) -> Res<Vec<PropertyKey>> {
+        if self.revoke.get() {
+            return self.inner.enumerable_keys(realm);
+        }
+
+        Err(Error::new("not yet implemented"))
+    }
+
+    fn enumerable_values(&self, realm: &mut Realm) -> Res<Vec<crate::value::Value>> {
+        if self.revoke.get() {
+            return self.inner.enumerable_values(realm);
+        }
+
+        Err(Error::new("not yet implemented"))
+    }
+
+    fn clear_properties(&self, realm: &mut Realm) -> Res {
+        if self.revoke.get() {
+            return self.inner.clear_properties(realm);
+        }
+
+        Err(Error::new("not yet implemented"))
     }
 
     fn gc_refs(&self) -> Vec<GcRef<BoxedObj>> {
-        todo!()
+        self.inner.gc_refs()
     }
 }
-
-// impl Obj for Proxy {
-//     fn define_property(&self, name: Value, value: Value) -> Res {
-//         if self.revoke.get() {
-//             return self.inner.define_property(name, value);
-//         }
-//
-//         Err(Error::new("not yet implemented"))
-//     }
-//
-//     fn define_variable(&self, name: Value, value: Variable) -> Res {
-//         if self.revoke.get() {
-//             return self.inner.define_variable(name, value);
-//         }
-//
-//         Err(Error::new("not yet implemented"))
-//     }
-//
-//     fn resolve_property(&self, name: &Value) -> Result<Option<ObjectProperty>, Error> {
-//         if self.revoke.get() {
-//             return self.inner.deref().resolve_property(name);
-//         }
-//
-//         Err(Error::new("not yet implemented"))
-//     }
-//
-//     fn get_property(&self, name: &Value) -> Result<Option<ObjectProperty>, Error> {
-//         if self.revoke.get() {
-//             return self.inner.deref().get_property(name);
-//         }
-//
-//         Err(Error::new("not yet implemented"))
-//     }
-//
-//     fn define_getter(&self, name: Value, value: Value) -> Res {
-//         if self.revoke.get() {
-//             return self.inner.define_getter(name, value);
-//         }
-//
-//         Err(Error::new("not yet implemented"))
-//     }
-//
-//     fn define_setter(&self, name: Value, value: Value) -> Res {
-//         if self.revoke.get() {
-//             return self.inner.define_setter(name, value);
-//         }
-//
-//         Err(Error::new("not yet implemented"))
-//     }
-//
-//     fn delete_property(&self, name: &Value) -> Result<Option<Value>, Error> {
-//         if self.revoke.get() {
-//             return self.inner.delete_property(name);
-//         }
-//
-//         Err(Error::new("not yet implemented"))
-//     }
-//
-//     fn contains_key(&self, name: &Value) -> Result<bool, Error> {
-//         if self.revoke.get() {
-//             return self.inner.contains_key(name);
-//         }
-//
-//         Err(Error::new("not yet implemented"))
-//     }
-//
-//     fn has_key(&self, name: &Value) -> Result<bool, Error> {
-//         if self.revoke.get() {
-//             return self.inner.has_key(name);
-//         }
-//
-//         Err(Error::new("not yet implemented"))
-//     }
-//
-//     fn name(&self) -> String {
-//         self.inner.name()
-//     }
-//
-//     fn to_string(&self, realm: &mut Realm) -> Result<YSString, Error> {
-//         self.inner.to_string(realm)
-//     }
-//
-//     fn to_string_internal(&self) -> Result<YSString, Error> {
-//         self.inner.to_string_internal()
-//     }
-//
-//     fn properties(&self) -> Result<Vec<(Value, Value)>, Error> {
-//         if self.revoke.get() {
-//             return self.inner.properties();
-//         }
-//
-//         Err(Error::new("not yet implemented"))
-//     }
-//
-//     fn keys(&self) -> Result<Vec<Value>, Error> {
-//         if self.revoke.get() {
-//             return self.inner.keys();
-//         }
-//
-//         Err(Error::new("not yet implemented"))
-//     }
-//
-//     fn values(&self) -> Result<Vec<Value>, Error> {
-//         if self.revoke.get() {
-//             return self.inner.values();
-//         }
-//
-//         Err(Error::new("not yet implemented"))
-//     }
-//
-//     fn get_array_or_done(&self, index: usize) -> Result<(bool, Option<Value>), Error> {
-//         if self.revoke.get() {
-//             return self.inner.get_array_or_done(index);
-//         }
-//
-//         Err(Error::new("not yet implemented"))
-//     }
-//
-//     fn clear_values(&self) -> Res {
-//         if self.revoke.get() {
-//             return self.inner.clear_values();
-//         }
-//
-//         Err(Error::new("not yet implemented"))
-//     }
-//
-//     fn call(&self, realm: &mut Realm, args: Vec<Value>, this: Value) -> Result<Value, Error> {
-//         if self.revoke.get() {
-//             return self.inner.call(realm, args, this);
-//         }
-//
-//         if let Some(apply) = self.handler.get_opt("apply", realm)? {
-//             let apply = apply.to_object()?;
-//
-//             let arguments = Array::with_elements(realm, args)?;
-//             apply.call(
-//                 realm,
-//                 vec![self.inner.clone().into(), this, arguments.into_value()],
-//                 self.handler.clone().into(),
-//             )
-//         } else {
-//             self.inner.call(realm, args, this)
-//         }
-//     }
-//
-//     fn is_function(&self) -> bool {
-//         self.inner.is_function()
-//     }
-//
-//     fn primitive(&self) -> Option<Value> {
-//         self.inner.primitive()
-//     }
-//
-//     fn prototype(&self) -> Result<ObjectProperty, Error> {
-//         if self.revoke.get() {
-//             return self.inner.prototype();
-//         }
-//
-//         Err(Error::new("not yet implemented"))
-//     }
-//
-//     fn set_prototype(&self, proto: ObjectProperty) -> Res {
-//         if self.revoke.get() {
-//             return self.inner.set_prototype(proto);
-//         }
-//
-//         Err(Error::new("not yet implemented"))
-//     }
-//
-//     fn constructor(&self) -> Result<ObjectProperty, Error> {
-//         self.inner.constructor()
-//     }
-//
-//     unsafe fn custom_gc_refs(&self) -> Vec<GcRef<BoxedObj>> {
-//         vec![self.inner.get_ref(), self.handler.get_ref()]
-//     }
-//
-//     fn class_name(&self) -> &'static str {
-//         self.inner.class_name()
-//     }
-//
-//     fn construct(&self, realm: &mut Realm, args: Vec<Value>) -> Result<Value, Error> {
-//         if self.revoke.get() {
-//             return self.inner.construct(realm, args);
-//         }
-//
-//         if let Some(construct) = self.handler.get_opt("construct", realm)? {
-//             let construct = construct.to_object()?;
-//             let arguments = Array::with_elements(realm, args)?;
-//             construct.call(
-//                 realm,
-//                 vec![self.inner.clone().into(), arguments.into_value()],
-//                 self.handler.clone().into(),
-//             )
-//         } else {
-//             self.inner.construct(realm, args)
-//         }
-//     }
-//
-//     fn is_constructor(&self) -> bool {
-//         self.inner.is_constructor()
-//     }
-//
-//     unsafe fn inner_downcast(&self, ty: TypeId) -> Option<NonNull<()>> {
-//         self.inner.inner_downcast(ty)
-//     }
-// }
 
 #[props]
 impl Proxy {
