9e4acf40197adebe3fe7b93c02e16829fd170160
Shark <sharktheone@proton.me>
2025-10-12 21:50:27 +0200

    remove global object ref from Intrinsic

--- crates/yavashark_env/src/realm/initialize.rs
+++ crates/yavashark_env/src/realm/initialize.rs
@@ -5,7 +5,7 @@ pub trait Intrinsic {
     fn initialize(&self, realm: &mut Realm) -> Res;
     fn get_prototype(&self, realm: &Realm) -> ObjectHandle;
 
-    fn setup_global(&self, realm: &mut Realm, global: &ObjectHandle) -> Res {
+    fn setup_global(&self, realm: &mut Realm) -> Res {
         Ok(())
     }
 }
