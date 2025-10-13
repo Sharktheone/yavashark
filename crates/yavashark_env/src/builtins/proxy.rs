72e3be315a7e32f0e846d3eb4620ebeea5747e24
Shark <sharktheone@proton.me>
2025-10-14 00:26:58 +0200

    more proxy stuff

--- crates/yavashark_env/src/builtins/proxy.rs
+++ crates/yavashark_env/src/builtins/proxy.rs
@@ -1,12 +1,9 @@
 #![allow(unused)]
 use crate::array::Array;
-use crate::value::{BoxedObj, DefinePropertyResult, Obj, Property};
-use crate::{
-    Error, InternalPropertyKey, NativeFunction, Object, ObjectHandle, ObjectOrNull, ObjectProperty,
-    PrimitiveValue, PropertyKey, Realm, Res, Value, Variable,
-};
+use crate::value::{Attributes, BoxedObj, DefinePropertyResult, Obj, Property, PropertyDescriptor, WeakObject};
+use crate::{Error, InternalPropertyKey, NativeFunction, Object, ObjectHandle, ObjectOrNull, ObjectProperty, PrimitiveValue, PropertyKey, Realm, Res, Value, Variable, WeakObjectHandle};
 use std::any::TypeId;
-use std::cell::Cell;
+use std::cell::{Cell, RefCell};
 use std::ops::Deref;
 use std::ptr::NonNull;
 use yavashark_garbage::GcRef;
@@ -17,6 +14,7 @@ use yavashark_string::YSString;
 pub struct Proxy {
     inner: ObjectHandle,
     handler: ObjectHandle,
+    this: RefCell<Option<WeakObjectHandle>>,
     revoke: Cell<bool>,
 }
 
@@ -31,7 +29,30 @@ impl Obj for Proxy {
             return self.inner.define_property(name, value, realm);
         }
 
-        Err(Error::new("not yet implemented"))
+        if let Some(define_property) = self.handler.get_opt("set", realm)? {
+            let define_property = define_property.to_object()?;
+
+
+            let result = define_property.call(
+                vec![
+                    self.inner.clone().into(),
+                    name.into(),
+                    value,
+                    self.this(),
+                ],
+                self.handler.clone().into(),
+                realm,
+            )?;
+
+            if result.is_truthy() {
+                Ok(DefinePropertyResult::Handled)
+            } else {
+                Err(Error::ty("Proxy handler's defineProperty method returned false"))
+            }
+        } else {
+            self.inner.define_property(name, value, realm)
+        }
+
     }
 
     fn define_property_attributes(
@@ -44,7 +65,31 @@ impl Obj for Proxy {
             return self.inner.define_property_attributes(name, value, realm);
         }
 
-        Err(Error::new("not yet implemented"))
+        if let Some(define_property) = self.handler.get_opt("set", realm)? {
+            let define_property = define_property.to_object()?;
+
+            let result = define_property.call(
+                vec![
+                    self.inner.clone().into(),
+                    name.into(),
+                    value.value,
+                    self.this(),
+                ],
+                self.handler.clone().into(),
+                realm,
+            )?;
+
+            if result.is_truthy() {
+                Ok(DefinePropertyResult::Handled)
+            } else {
+                Err(Error::ty("Proxy handler's defineProperty method returned false"))
+            }
+        } else {
+            self.inner.define_property_attributes(name, value, realm)
+        }
+
+
+
     }
 
     fn resolve_property(
@@ -56,7 +101,23 @@ impl Obj for Proxy {
             return self.inner.deref().resolve_property(name, realm);
         }
 
-        Err(Error::new("not yet implemented"))
+        if let Some(get) = self.handler.get_opt("get", realm)? {
+            let get = get.to_object()?;
+            let result = get.call(
+                vec![
+                    self.inner.clone().into(),
+                    name.into(),
+                    self.handler.clone().into(),
+                    self.this(),
+                ],
+                self.handler.clone().into(),
+                realm,
+            )?;
+            Ok(Some(Property::Value(result, Attributes::new())))
+        } else {
+            self.inner.deref().resolve_property(name, realm)
+        }
+
     }
 
     fn get_own_property(
@@ -68,7 +129,27 @@ impl Obj for Proxy {
             return self.inner.deref().get_own_property(name, realm);
         }
 
-        Err(Error::new("not yet implemented"))
+        if let Some(get_own_property) = self.handler.get_opt("get", realm)? {
+            let get_own_property = get_own_property.to_object()?;
+            let result = get_own_property.call(
+                vec![
+                    self.inner.clone().into(),
+                    name.into(),
+                    self.this(),
+                ],
+                self.handler.clone().into(),
+                realm,
+            )?;
+            if result.is_undefined() {
+                Ok(None)
+            } else {
+                let desc: PropertyDescriptor = result.into();
+                Ok(Some(desc.into()))
+            }
+        } else {
+            self.inner.deref().get_own_property(name, realm)
+        }
+
     }
 
     fn define_getter(
@@ -77,11 +158,7 @@ impl Obj for Proxy {
         value: ObjectHandle,
         realm: &mut Realm,
     ) -> Res {
-        if self.revoke.get() {
-            return self.inner.define_getter(name, value, realm);
-        }
-
-        Err(Error::new("not yet implemented"))
+        self.inner.define_getter(name, value, realm)
     }
 
     fn define_setter(
@@ -90,11 +167,8 @@ impl Obj for Proxy {
         value: ObjectHandle,
         realm: &mut Realm,
     ) -> Res {
-        if self.revoke.get() {
-            return self.inner.define_setter(name, value, realm);
-        }
+        self.inner.define_setter(name, value, realm)
 
-        Err(Error::new("not yet implemented"))
     }
 
     fn delete_property(
@@ -106,7 +180,27 @@ impl Obj for Proxy {
             return self.inner.delete_property(name, realm);
         }
 
-        Err(Error::new("not yet implemented"))
+        if let Some(delete_property) = self.handler.get_opt("deleteProperty", realm)? {
+            let delete_property = delete_property.to_object()?;
+            let result = delete_property.call(
+                vec![
+                    self.inner.clone().into(),
+                    name.into(),
+                    self.this(),
+                ],
+                self.handler.clone().into(),
+                realm,
+            )?;
+
+            if result.is_truthy() {
+                Ok(Some(Property::Value(Value::Undefined, Attributes::new())))
+            } else {
+                Err(Error::ty("Proxy handler's deleteProperty method returned false"))
+            }
+        } else {
+            self.inner.delete_property(name, realm)
+        }
+
     }
 
     fn contains_own_key(
@@ -118,7 +212,23 @@ impl Obj for Proxy {
             return self.inner.contains_own_key(name, realm);
         }
 
-        Err(Error::new("not yet implemented"))
+        if let Some(has) = self.handler.get_opt("has", realm)? {
+            let has = has.to_object()?;
+            let result = has.call(
+                vec![
+                    self.inner.clone().into(),
+                    name.into(),
+                    self.this(),
+                ],
+                self.handler.clone().into(),
+                realm,
+            )?;
+
+            Ok(result.is_truthy())
+        } else {
+            self.inner.contains_own_key(name, realm)
+        }
+
     }
 
     fn contains_key(&self, name: InternalPropertyKey, realm: &mut Realm) -> Result<bool, Error> {
@@ -126,7 +236,23 @@ impl Obj for Proxy {
             return self.inner.contains_key(name, realm);
         }
 
-        Err(Error::new("not yet implemented"))
+        if let Some(has) = self.handler.get_opt("has", realm)? {
+            let has = has.to_object()?;
+            let result = has.call(
+                vec![
+                    self.inner.clone().into(),
+                    name.into(),
+                    self.this(),
+                ],
+                self.handler.clone().into(),
+                realm,
+            )?;
+
+            Ok(result.is_truthy())
+        } else {
+            self.inner.contains_key(name, realm)
+        }
+
     }
 
     // fn to_string(&self, realm: &mut Realm) -> Result<YSString, Error> {
@@ -138,62 +264,38 @@ impl Obj for Proxy {
     // }
 
     fn properties(&self, realm: &mut Realm) -> Res<Vec<(PropertyKey, Value)>> {
-        if self.revoke.get() {
-            return self.inner.properties(realm);
-        }
+        self.inner.properties(realm)
 
-        Err(Error::new("not yet implemented"))
     }
 
     fn keys(&self, realm: &mut Realm) -> Res<Vec<PropertyKey>> {
-        if self.revoke.get() {
-            return self.inner.keys(realm);
-        }
-
-        Err(Error::new("not yet implemented"))
+        self.inner.keys(realm)
     }
 
     fn values(&self, realm: &mut Realm) -> Res<Vec<Value>> {
-        if self.revoke.get() {
-            return self.inner.values(realm);
-        }
-
-        Err(Error::new("not yet implemented"))
+        self.inner.values(realm)
     }
 
     fn enumerable_properties(
         &self,
         realm: &mut Realm,
     ) -> Res<Vec<(PropertyKey, crate::value::Value)>> {
-        if self.revoke.get() {
-            return self.inner.enumerable_properties(realm);
-        }
-
-        Err(Error::new("not yet implemented"))
+        self.inner.enumerable_properties(realm)
     }
 
     fn enumerable_keys(&self, realm: &mut Realm) -> Res<Vec<PropertyKey>> {
-        if self.revoke.get() {
-            return self.inner.enumerable_keys(realm);
-        }
+        self.inner.enumerable_keys(realm)
 
-        Err(Error::new("not yet implemented"))
     }
 
     fn enumerable_values(&self, realm: &mut Realm) -> Res<Vec<crate::value::Value>> {
-        if self.revoke.get() {
-            return self.inner.enumerable_values(realm);
-        }
+        self.inner.enumerable_values(realm)
 
-        Err(Error::new("not yet implemented"))
     }
 
     fn clear_properties(&self, realm: &mut Realm) -> Res {
-        if self.revoke.get() {
-            return self.inner.clear_properties(realm);
-        }
+        self.inner.clear_properties(realm)
 
-        Err(Error::new("not yet implemented"))
     }
 
     fn get_array_or_done(
@@ -205,7 +307,23 @@ impl Obj for Proxy {
             return self.inner.get_array_or_done(index, realm);
         }
 
-        Err(Error::new("not yet implemented"))
+        if let Some(get) = self.handler.get_opt("get", realm)? {
+            let get = get.to_object()?;
+            let result = get.call(
+                vec![
+                    self.inner.clone().into(),
+                    (index as u32).into(),
+                    self.handler.clone().into(),
+                    self.this(),
+                ],
+                self.handler.clone().into(),
+                realm,
+            )?;
+            Ok((true, Some(result)))
+        } else {
+            self.inner.get_array_or_done(index, realm)
+        }
+
     }
 
     fn call(&self, args: Vec<Value>, this: Value, realm: &mut Realm) -> Result<Value, Error> {
@@ -218,7 +336,7 @@ impl Obj for Proxy {
 
             let arguments = Array::with_elements(realm, args)?;
             apply.call(
-                vec![self.inner.clone().into(), this, arguments.into_value()],
+                vec![self.inner.clone().into(), this, arguments.into_value(), self.this()],
                 self.handler.clone().into(),
                 realm,
             )
@@ -240,7 +358,23 @@ impl Obj for Proxy {
             return self.inner.prototype(realm);
         }
 
-        Err(Error::new("not yet implemented"))
+        if let Some(get_prototype) = self.handler.get_opt("getPrototypeOf", realm)? {
+            let get_prototype = get_prototype.to_object()?;
+            let result = get_prototype.call(
+                vec![self.inner.clone().into(), self.this()],
+                self.handler.clone().into(),
+                realm,
+            )?;
+
+            if result.is_null() || result.is_object() {
+                result.try_into()
+            } else {
+                Err(Error::ty("Proxy handler's getPrototypeOf method did not return an object or null"))
+            }
+        } else {
+            self.inner.prototype(realm)
+        }
+
     }
 
     fn set_prototype(&self, proto: ObjectOrNull, realm: &mut Realm) -> Res {
@@ -248,7 +382,23 @@ impl Obj for Proxy {
             return self.inner.set_prototype(proto, realm);
         }
 
-        Err(Error::new("not yet implemented"))
+        if let Some(set_prototype) = self.handler.get_opt("setPrototypeOf", realm)? {
+            let set_prototype = set_prototype.to_object()?;
+            let result = set_prototype.call(
+                vec![self.inner.clone().into(), proto.into(), self.this()],
+                self.handler.clone().into(),
+                realm,
+            )?;
+
+            if result.is_truthy() {
+                Ok(())
+            } else {
+                Err(Error::ty("Proxy handler's setPrototypeOf method returned false"))
+            }
+        } else {
+            self.inner.set_prototype(proto, realm)
+        }
+
     }
 
     fn construct(&self, args: Vec<Value>, realm: &mut Realm) -> Result<ObjectHandle, Error> {
@@ -261,7 +411,7 @@ impl Obj for Proxy {
             let arguments = Array::with_elements(realm, args)?;
             construct
                 .call(
-                    vec![self.inner.clone().into(), arguments.into_value()],
+                    vec![self.inner.clone().into(), arguments.into_value(), self.this()],
                     self.handler.clone().into(),
                     realm,
                 )?
@@ -284,25 +434,37 @@ impl Obj for Proxy {
     }
 
     unsafe fn inner_downcast(&self, ty: TypeId) -> Option<NonNull<()>> {
+        if ty == TypeId::of::<Self>() {
+            return Some(NonNull::from(self).cast());
+        }
+
         self.inner.inner_downcast(ty)
     }
 
     fn gc_refs(&self) -> Vec<GcRef<BoxedObj>> {
-        self.inner.gc_refs() //TODO: this is not correct
+        vec![self.inner.get_ref(), self.handler.get_ref()]
     }
 }
 
 #[props]
 impl Proxy {
     #[constructor]
-    #[must_use]
-    pub fn construct(target: ObjectHandle, handler: ObjectHandle) -> ObjectHandle {
-        Self {
+    pub fn construct(target: ObjectHandle, handler: ObjectHandle) -> Res<ObjectHandle> {
+        let this = Self {
             inner: target,
             handler,
             revoke: Cell::new(false),
+            this: RefCell::new(None),
         }
-        .into_object()
+        .into_object();
+
+        if let Some(proxy) = this.downcast::<Self>() {
+            proxy.this.replace(Some(WeakObjectHandle::new(&this)));
+        } else {
+            return Err(Error::ty("Failed to create proxy"));
+        }
+
+        Ok(this)
     }
 
     pub fn revocable(
@@ -314,8 +476,15 @@ impl Proxy {
             inner: target,
             handler,
             revoke: Cell::new(false),
+            this: RefCell::new(None),
         }
         .into_object();
+
+        if let Some(p) = proxy.downcast::<Self>() {
+            p.this.replace(Some(WeakObjectHandle::new(&proxy)));
+        } else {
+            return Err(Error::ty("Failed to create proxy"));
+        }
 
         let mut p = proxy.clone();
 
@@ -337,5 +506,12 @@ impl Proxy {
         ret.set("revoke", revoke, realm)?;
 
         Ok(ret)
+    }
+
+    fn this(&self) -> Value {
+        self.this
+            .borrow()
+            .as_ref()
+            .map_or(Value::Undefined, |w| w.upgrade().map_or(Value::Undefined, Into::into))
     }
 }
