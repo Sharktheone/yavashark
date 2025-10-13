08420c79e179ff5710d8eecf11f0df443ad8e871
Shark <sharktheone@proton.me>
2025-10-13 23:36:15 +0200

    Intl.DateTimeFormat methods

--- crates/yavashark_env/src/builtins/intl/date_time_format.rs
+++ crates/yavashark_env/src/builtins/intl/date_time_format.rs
@@ -1,6 +1,8 @@
 use std::cell::RefCell;
 use yavashark_macro::{object, props};
-use crate::{MutObject, Realm};
+use crate::{MutObject, Object, ObjectHandle, Realm};
+use crate::array::Array;
+use crate::value::Obj;
 
 #[object]
 #[derive(Debug)]
@@ -20,6 +22,36 @@ impl DateTimeFormat {
 #[props]
 impl DateTimeFormat {
     #[call_constructor]
-    fn construct() {}
+    fn construct(_locales: Option<String>, _options: Option<ObjectHandle>, realm: &mut Realm) -> ObjectHandle {
+        Self::new(realm).into_object()
+    }
 
+    #[prop("supportedLocalesOf")]
+    fn supported_locales_of(_locales: Option<String>, _options: Option<ObjectHandle>, realm: &Realm) -> ObjectHandle {
+        Array::from_realm(realm).into_object()
+    }
+
+    fn format(&self) -> String {
+        String::new()
+    }
+
+    #[prop("formatRange")]
+    fn format_range(&self, _start: String, _end: String) -> String {
+        String::new()
+    }
+
+    #[prop("formatRangeToParts")]
+    fn format_range_to_parts(&self, _start: String, _end: String, realm: &Realm) -> ObjectHandle {
+        Array::from_realm(realm).into_object()
+    }
+
+    #[prop("formatToParts")]
+    fn format_to_parts(&self, _date: String, realm: &Realm) -> ObjectHandle {
+        Array::from_realm(realm).into_object()
+    }
+
+    #[prop("resolvedOptions")]
+    fn resolved_options(&self, realm: &Realm) -> ObjectHandle {
+        Object::new(realm)
+    }
 }
