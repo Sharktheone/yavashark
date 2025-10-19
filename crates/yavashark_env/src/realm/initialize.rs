808d8f78fae7e64b26306efaee6e1364bdb342c7
Shark <sharktheone@proton.me>
2025-10-19 21:24:26 +0200

    update `Intrinsic` trait

--- crates/yavashark_env/src/realm/initialize.rs
+++ crates/yavashark_env/src/realm/initialize.rs
@@ -1,11 +1,11 @@
-use crate::{ObjectHandle, Realm, Res};
+use crate::{Object, ObjectHandle, Realm, Res};
 
 #[allow(unused_variables)]
 pub trait Intrinsic {
-    fn initialize(&self, realm: &mut Realm) -> Res;
-    fn get_prototype(&self, realm: &Realm) -> ObjectHandle;
+    fn initialize(realm: &mut Realm) -> Res<ObjectHandle>;
+    fn get_intrinsic(realm: &mut Realm) -> Res<ObjectHandle>;
 
-    fn setup_global(&self, realm: &mut Realm) -> Res {
-        Ok(())
+    fn get_global(realm: &mut Realm) -> Res<ObjectHandle> {
+        Ok(Object::null())
     }
 }
