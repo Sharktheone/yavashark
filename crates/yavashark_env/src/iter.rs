a209393ef57d87855243c9765a8d9bab3a0712e4
Shark <sharktheone@proton.me>
2025-10-24 22:38:32 +0200

    use boxed iter for NativeIterator

--- crates/yavashark_env/src/iter.rs
+++ crates/yavashark_env/src/iter.rs
@@ -3,6 +3,7 @@ use std::fmt::Debug;
 use yavashark_macro::object;
 use crate::{MutObject, ObjectHandle, Realm, Res, Value};
 use crate::conversion::TryIntoValue;
+use crate::value::Obj;
 
 pub trait IntoJsIter {
     fn into_js_iter(self, realm: &mut Realm) -> Res<ObjectHandle>;
@@ -27,32 +28,20 @@ impl<T: Iterator<Item = I> + 'static, I: TryIntoValue> JSIterator for T {
 
 
 #[object]
-pub struct NativeIterator<I: JSIterator + ?Sized = dyn JSIterator> {
-    iter: RefCell<I>,
+pub struct NativeIterator {
+    iter: RefCell<Box<dyn JSIterator>>,
 }
 
 
-impl<I: JSIterator + 'static> NativeIterator<I> {
-    pub fn new(iter: I, proto: ObjectHandle) -> Self {
+impl NativeIterator {
+    pub fn new(iter: impl JSIterator, proto: ObjectHandle) -> Self {
         Self {
             inner: RefCell::new(MutableNativeIterator {
                 object: MutObject::with_proto(proto),
             }),
 
-            iter: RefCell::new(iter),
+            iter: RefCell::new(Box::new(iter)),
         }
-    }
-
-    pub fn as_dyn(&self) -> &NativeIterator {
-        self
-    }
-
-    pub fn to_dyn_boxed(self) -> Box<NativeIterator> {
-        Box::new(self)
-    }
-
-    pub fn to_handle(self) -> ObjectHandle {
-        ObjectHandle::from_boxed_obj(self.to_dyn_boxed())
     }
 }
 
@@ -60,7 +49,7 @@ impl Debug for NativeIterator {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("NativeIterator")
             .field("inner", &self.inner)
-            .finish()
+            .finish_non_exhaustive()
     }
 }
 
@@ -72,7 +61,6 @@ impl<I: JSIterator> IntoJsIter for I {
     }
 
     fn into_js_iter_proto(self, proto: ObjectHandle, _realm: &mut Realm) -> Res<ObjectHandle> {
-        let native_iter = NativeIterator::new(self, proto);
-        Ok(native_iter.to_handle())
+        Ok(NativeIterator::new(self, proto).into_object())
     }
 }
