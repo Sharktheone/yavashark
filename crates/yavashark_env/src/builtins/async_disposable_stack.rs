c81473c671840912a624cbf24e37cabde67d3ff9
Shark <sharktheone@proton.me>
2025-10-27 23:52:16 +0100

    AsyncDisposableStack methods

--- crates/yavashark_env/src/builtins/async_disposable_stack.rs
+++ crates/yavashark_env/src/builtins/async_disposable_stack.rs
@@ -1,5 +1,5 @@
 use yavashark_macro::{object, props};
-use crate::Realm;
+use crate::{Object, ObjectHandle, Realm, Value, Symbol};
 
 #[object]
 #[derive(Debug)]
@@ -8,4 +8,31 @@ pub struct AsyncDisposableStack {}
 
 #[props(intrinsic_name = async_disposable_stack)]
 impl AsyncDisposableStack {
+
+    pub fn adopt(&self, value: Value, on_dispose: ObjectHandle) -> Value {
+        value
+    }
+
+    pub fn defer(&self, on_dispose: ObjectHandle) {
+
+    }
+
+    #[prop("disposeAsync")]
+    pub fn dispose_async(&self) {
+
+    }
+
+    pub fn move_(&self) -> ObjectHandle {
+        Object::null()
+    }
+
+    #[prop("use")]
+    pub fn use_(&self, value: Value) -> Value {
+        value
+    }
+
+    #[prop(Symbol::ASYNC_DISPOSE)]
+    pub fn symbol_async_dispose(&self) {
+
+    }
 }
