46921b5dd9a5e1a53165e9c29c6c1251717b241e
Shark <sharktheone@proton.me>
2025-12-03 22:48:28 +0100

    native object!

--- crates/yavashark_env/src/native_obj.rs
+++ crates/yavashark_env/src/native_obj.rs
@@ -3,18 +3,45 @@
 use crate::conversion::TryIntoValue;
 use crate::inline_props::{InlineObject, PropertiesHook};
 use crate::realm::Intrinsic;
-use crate::value::IntoValue;
+use crate::value::{IntoValue, ObjectImpl};
 use crate::{MutObject, ObjectHandle, Realm, Res, Value};
 use std::cell::RefCell;
 use std::fmt::Debug;
+use crate::partial_init::Initializer;
 
-// TODO: maybe this is possible at some point - sigh >'_'<
-// default impl<T: Intrinsic + PropertiesHook + Debug + 'static> TryIntoValue for T {
-//     fn try_into_value(self, realm: &mut Realm) -> Res<Value> {
-//         let proto = T::get_intrinsic(realm)?;
-//
-//         let obj = InlineObject::with_proto(self, proto);
-//
-//         Ok(obj.into_value())
-//     }
-// }
+#[derive(Debug)]
+pub struct NativeObject<T: ?Sized> {
+    pub inner: RefCell<MutObject>,
+    pub native: T,
+}
+
+
+impl<T: ?Sized + Intrinsic> Intrinsic for NativeObject<T> {
+    fn initialize(realm: &mut Realm) -> Res<ObjectHandle> {
+        T::initialize(realm)
+    }
+
+    fn get_intrinsic(realm: &mut Realm) -> Res<ObjectHandle> {
+        T::get_intrinsic(realm)
+    }
+
+    fn get_global(realm: &mut Realm) -> Res<ObjectHandle> {
+        T::get_global(realm)
+    }
+}
+
+impl<T: ?Sized + Debug + 'static> ObjectImpl for NativeObject<T> {
+    type Inner = MutObject;
+
+    fn get_wrapped_object(&self) -> impl std::ops::DerefMut<Target = impl crate::value::MutObj> {
+        self.inner.borrow_mut()
+    }
+
+    fn get_inner(&self) -> impl std::ops::Deref<Target = Self::Inner> {
+        self.inner.borrow()
+    }
+
+    fn get_inner_mut(&self) -> impl std::ops::DerefMut<Target = Self::Inner> {
+        self.inner.borrow_mut()
+    }
+}
