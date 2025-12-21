fc710cef48e57daa718a79c940898de8ba148f27
Shark <sharktheone@proton.me>
2025-12-21 01:47:57 +0100

    calendar with objects

--- crates/yavashark_env/src/builtins/temporal/utils/calendar.rs
+++ crates/yavashark_env/src/builtins/temporal/utils/calendar.rs
@@ -2,18 +2,46 @@ use std::str::FromStr;
 use temporal_rs::Calendar;
 use crate::conversion::FromValueOutput;
 use crate::{Error, Realm, Res, Value};
+use crate::builtins::{PlainDate, PlainDateTime, PlainMonthDay, PlainYearMonth, ZonedDateTime};
 
 impl FromValueOutput for Calendar {
     type Output = Self;
 
-    fn from_value_out(value: Value, realm: &mut Realm) -> Res<Self::Output> {
-        let Value::String(calendar) = value else {
-            return Err(Error::ty(
-                "Calendar value must be a string",
-            ));
-        };
+    fn from_value_out(value: Value, _: &mut Realm) -> Res<Self::Output> {
+        match value {
+            Value::Object(obj) => {
+                if let Some(calendar_like) = obj.downcast::<PlainDate>() {
+                    return Ok(calendar_like.date.calendar().clone());
+                }
 
-        Self::from_str(&calendar)
-            .map_err(Error::from_temporal)
+                if let Some(calendar_like) = obj.downcast::<PlainDateTime>() {
+                    return Ok(calendar_like.date.calendar().clone());
+                }
+
+                if let Some(calendar_like) = obj.downcast::<PlainYearMonth>() {
+                    return Ok(calendar_like.year_month.calendar().clone());
+                }
+
+                if let Some(calendar_like) = obj.downcast::<PlainMonthDay>() {
+                    return Ok(calendar_like.month_day.calendar().clone());
+                }
+
+                if let Some(calendar_like) = obj.downcast::<ZonedDateTime>() {
+                    return Ok(calendar_like.date.calendar().clone());
+                }
+
+                Err(Error::ty(
+                    "Calendar object must be a 'calendar like' Temporal object",
+                ))
+            }
+            Value::String(s) => {
+                Self::from_str(&s)
+                    .map_err(Error::from_temporal)
+            }
+            _ => Err(Error::ty(
+                "Calendar value must be a string or a 'calendar like' value",
+            )),
+        }
+
     }
 }
