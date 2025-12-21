07634e878ddf856b780473303ab93da610d0d022
Shark <sharktheone@proton.me>
2025-12-21 03:52:19 +0100

    allow downcasting to native part

--- crates/yavashark_env/src/native_obj.rs
+++ crates/yavashark_env/src/native_obj.rs
@@ -1,15 +1,17 @@
 #![allow(unused)]
 
+use std::any::TypeId;
 use std::borrow::Borrow;
 use crate::conversion::TryIntoValue;
 use crate::inline_props::{InlineObject, PropertiesHook};
 use crate::partial_init::Initializer;
 use crate::realm::Intrinsic;
-use crate::value::{IntoValue, ObjectImpl};
+use crate::value::{IntoValue, Obj, ObjectImpl, MutObj};
 use crate::{MutObject, ObjectHandle, Realm, Res, Value};
 use std::cell::RefCell;
 use std::fmt::Debug;
 use std::ops::Deref;
+use std::ptr::NonNull;
 
 #[derive(Debug)]
 pub struct NativeObject<T: ?Sized> {
@@ -53,6 +55,20 @@ impl<T: ?Sized + Debug + 'static> ObjectImpl for NativeObject<T> {
 
     fn get_inner_mut(&self) -> impl std::ops::DerefMut<Target = Self::Inner> {
         self.inner.borrow_mut()
+    }
+
+
+    /// # Safety
+    /// - Caller and implementer must ensure that the pointer is a valid pointer to the type which the type id represents
+    /// - Caller and implementer must ensure that the pointer is valid for the same lifetime of self
+    unsafe fn inner_downcast(&self, ty: TypeId) -> Option<NonNull<()>> {
+        if ty == TypeId::of::<Self>() {
+            Some(NonNull::from(self).cast())
+        } else if ty == TypeId::of::<T>() {
+            Some(NonNull::from(&self.native).cast())
+        } else {
+            self.get_wrapped_object().inner_downcast(ty)
+        }
     }
 }
 
