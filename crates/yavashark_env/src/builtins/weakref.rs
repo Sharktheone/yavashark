78b7f29a77a3db5af8a61af99ab9442629778429
Shark <sharktheone@proton.me>
2025-12-25 23:43:50 +0100

    allow `WeakRef` to store Symbols

--- crates/yavashark_env/src/builtins/weakref.rs
+++ crates/yavashark_env/src/builtins/weakref.rs
@@ -2,15 +2,16 @@ use crate::value::Obj;
 use crate::{MutObject, ObjectHandle, Realm, Res, Value, WeakObjectHandle};
 use std::cell::RefCell;
 use yavashark_macro::{object, props};
+use crate::or_symbol::OrSymbol;
 
 #[object]
 #[derive(Debug)]
 pub struct WeakRef {
-    handle: WeakObjectHandle,
+    handle: OrSymbol<WeakObjectHandle>,
 }
 
 impl WeakRef {
-    pub fn new(handle: WeakObjectHandle, realm: &mut Realm) -> Res<Self> {
+    pub fn new(handle: OrSymbol<WeakObjectHandle>, realm: &mut Realm) -> Res<Self> {
         Ok(Self {
             inner: RefCell::new(MutableWeakRef {
                 object: MutObject::with_proto(
@@ -25,13 +26,16 @@ impl WeakRef {
 #[props(intrinsic_name = weak_ref)]
 impl WeakRef {
     #[constructor]
-    pub fn construct(handle: &ObjectHandle, realm: &mut Realm) -> Res<ObjectHandle> {
-        Ok(Self::new(handle.downgrade(), realm)?.into_object())
+    pub fn construct(handle: OrSymbol<ObjectHandle>, realm: &mut Realm) -> Res<ObjectHandle> {
+        let handle = handle.map(|h| h.downgrade());
+
+        Ok(Self::new(handle, realm)?.into_object())
     }
 
     pub fn deref(&self) -> Value {
         self.handle
-            .upgrade()
+            .as_ref()
+            .try_map(|h| h.upgrade().ok_or(()))
             .map(Into::into)
             .unwrap_or(Value::Undefined)
     }
