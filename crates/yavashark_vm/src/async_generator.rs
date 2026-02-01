433f1bfa5385c03c94411c040a93d6cd626772bc
Shark <sharktheone@proton.me>
2026-02-01 17:51:42 +0100

    fix this in async generator function

--- crates/yavashark_vm/src/async_generator.rs
+++ crates/yavashark_vm/src/async_generator.rs
@@ -134,8 +134,8 @@ impl AsyncGeneratorFunction {
 }
 
 impl Func for AsyncGeneratorFunction {
-    fn call(&self, realm: &mut Realm, args: Vec<Value>, _this: Value) -> ValueResult {
-        let scope = &mut Scope::with_parent(&self.scope)?;
+    fn call(&self, realm: &mut Realm, args: Vec<Value>, this: Value) -> ValueResult {
+        let scope = &mut Scope::with_parent_this(&self.scope, this)?;
         scope.state_set_returnable()?;
         scope.set_strict_mode()?;
 
