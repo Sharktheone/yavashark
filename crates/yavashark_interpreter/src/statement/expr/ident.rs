c708644c4675af9867b07c733effb66c75bfaec6
Shark <sharktheone@proton.me>
2026-03-01 20:55:56 +0100

    Revert "no reference error in sloppy mode for undefined variable"
    This reverts commit ddb81cccd3a7e3ea00f6478ef7d4acae4d440c5f.

--- crates/yavashark_interpreter/src/statement/expr/ident.rs
+++ crates/yavashark_interpreter/src/statement/expr/ident.rs
@@ -9,13 +9,9 @@ impl Interpreter {
         let value = scope.resolve(&ident, realm)?;
         value.map_or_else(
             || {
-                if scope.is_strict_mode()? {
-                    Err(ControlFlow::error_reference(format!(
-                        "{ident} is not defined"
-                    )))
-                } else {
-                    Ok(yavashark_env::Value::Undefined)
-                }
+                Err(ControlFlow::error_reference(format!(
+                    "{ident} is not defined"
+                )))
             },
             Ok,
         )
