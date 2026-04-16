b724e6ac0bc68f75d9275c1445072243bf172b4f
Shark <sharktheone@proton.me>
2026-04-16 23:46:09 +0200

    initial IR stuff

--- crates/yavashark_ir/src/lib.rs
+++ crates/yavashark_ir/src/lib.rs
@@ -1,1 +1,13 @@
 
+
+
+/// This IR will be used for many optimizations in the YS interpreter and compiler.
+pub struct YavasharkIr {
+
+}
+
+
+
+trait IRTranslator {
+    fn translate(&self, input: &str) -> YavasharkIr;
+}
