a9fe6383da9db3522519471a2beae473b70d4303
Shark <sharktheone@proton.me>
2026-04-25 23:41:21 +0200

    only accept valid timezone

--- crates/yavashark_env/src/builtins/temporal/utils/timezone.rs
+++ crates/yavashark_env/src/builtins/temporal/utils/timezone.rs
@@ -5,8 +5,8 @@ use temporal_rs::TimeZone;
 impl FromValueOutput for TimeZone {
     type Output = Self;
 
-    fn from_value_out(value: Value, realm: &mut Realm) -> Res<Self::Output> {
-        let tz_str = value.to_string(realm)?;
+    fn from_value_out(value: Value, _realm: &mut Realm) -> Res<Self::Output> {
+        let tz_str = value.as_string()?;
 
         Self::try_from_str(&tz_str.as_str_lossy()).map_err(Error::from_temporal)
     }
