f390bce1e98c6ea92bd6035c11ada779b43b5d2b
Shark <sharktheone@proton.me>
2026-05-04 23:50:42 +0200

    oops

--- crates/yavashark_ir/src/lib.rs
+++ crates/yavashark_ir/src/lib.rs
@@ -1,16 +1,22 @@
 use yavashark_string::YSString;
+use bumpalo::collections::Vec;
+use crate::statement::Statement;
+
+mod statement;
 
 /// This IR will be used for many optimizations in the YS interpreter and compiler.
 pub struct YavasharkIr<'alloc> {
-    functions: bumpalo::collections::Vec<'alloc, Function<'alloc>>,
+    functions: Vec<'alloc, Function<'alloc>>,
 
+
+    root: Statement<'alloc>,
 }
 
 
 
 pub struct Function<'alloc> {
     name: YSString,
-    parameters: Vec<YSString>,
+    parameters: Vec<'alloc, YSString>,
     body: Statement<'alloc>,
 }
 
@@ -18,7 +24,7 @@ pub struct Function<'alloc> {
 
 
 pub struct YavasharkBlock<'alloc> {
-    statements: bumpalo::collections::Vec<'alloc, YavasharkStatement<'alloc>>,
+    statements: Vec<'alloc, Statement<'alloc>>,
 }
 
 
