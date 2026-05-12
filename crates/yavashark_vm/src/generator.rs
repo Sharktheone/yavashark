f4ed05ebdb2744648112b7fdeaa115d9b9c1a6b7
Shark <sharktheone@proton.me>
2026-05-12 08:47:07 +0200

    generator, throw error when already running

--- crates/yavashark_vm/src/generator.rs
+++ crates/yavashark_vm/src/generator.rs
@@ -1,6 +1,6 @@
 use crate::params::VMParams;
 use crate::{GeneratorPoll, ResumableVM, VmState};
-use std::cell::RefCell;
+use std::cell::{Cell, RefCell};
 use std::fmt::Debug;
 use std::path::PathBuf;
 use std::rc::Rc;
@@ -156,6 +156,7 @@ impl Func for GeneratorFunction {
 #[object]
 pub struct Generator {
     state: RefCell<Option<VmState>>,
+    running: Cell<bool>,
 }
 
 impl Generator {
@@ -173,6 +174,7 @@ impl Generator {
                 ),
             }),
             state: RefCell::new(Some(state)),
+            running: Cell::new(false),
         })
     }
 
@@ -195,12 +197,17 @@ impl Generator {
 
     #[length(1)]
     fn next(&self, #[realm] realm: &mut Realm) -> Res<ObjectHandle> {
+        if self.running.replace(true) {
+            return Err(Error::new("Generator is already running"));
+        }
+
         let Some(state) = self.state.take() else {
             let obj = Object::new(realm);
 
             obj.define_property("done".into(), true.into(), realm)?;
             obj.define_property("value".into(), Value::Undefined, realm)?;
 
+            self.running.set(false);
             return Ok(obj);
         };
 
@@ -215,6 +222,7 @@ impl Generator {
                 obj.define_property("done".into(), false.into(), realm)?;
                 obj.define_property("value".into(), val, realm)?;
 
+                self.running.set(false);
                 Ok(obj)
             }
             GeneratorPoll::Ret(res) => {
@@ -225,6 +233,7 @@ impl Generator {
                 obj.define_property("done".into(), true.into(), realm)?;
                 obj.define_property("value".into(), val, realm)?;
 
+                self.running.set(false);
                 Ok(obj)
             }
         }
