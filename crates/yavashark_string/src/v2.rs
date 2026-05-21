5b0a87c269ce98ab70fccb9e106d4c80d1756588
Shark <sharktheone@proton.me>
2026-05-21 14:15:58 +0200

    new string version with slicing, still 24b

--- crates/yavashark_string/src/v2.rs
+++ crates/yavashark_string/src/v2.rs
@@ -1,15 +1,19 @@
+use std::cell::UnsafeCell;
+
 pub struct YSString {
-    ptr: *const (),
-    len: u32,
-    ptr_offset: u32,
-    len_offset: u32,
-    ty: Type,
+    inner: UnsafeCell<Inner>,
+}
+
+enum Inner {
+    Heap(HeapString),
+    InlineAscii(InlineAscii),
+    InlineWtf16(InlineWtf16),
+    Rope(RopeString),
 }
 
 enum Type {
     Ascii,
     Wtf16,
-    Rope,
 }
 
 enum Storage {
@@ -17,4 +21,60 @@ enum Storage {
     Rc,
     Rope,
     Static,
+}
+
+#[repr(Rust, packed)]
+struct HeapString {
+    ptr: *const (), //actual ptr = ptr - ptr_offset
+    len: u32,
+    ptr_offset: u32,
+    len_offset: u32, //len + len_offset = capacity
+    ty: Type,
+    storage: Storage,
+}
+
+struct InlineAscii {
+    len: InlineLen,
+    bytes: [u8; 23],
+}
+
+#[repr(Rust, packed)]
+struct InlineWtf16 {
+    len: InlineLen,
+    bytes: [u16; 11],
+}
+
+struct RopeString {
+    ptr1: *const YSString,
+    ptr2: *const YSString
+}
+
+/// Length enum for inline strings (0-23 bytes).
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u8)]
+pub enum InlineLen {
+    Empty = 0,
+    Len1,
+    Len2,
+    Len3,
+    Len4,
+    Len5,
+    Len6,
+    Len7,
+    Len8,
+    Len9,
+    Len10,
+    Len11,
+    Len12,
+    Len13,
+    Len14,
+    Len15,
+    Len16,
+    Len17,
+    Len18,
+    Len19,
+    Len20,
+    Len21,
+    Len22,
+    Len23,
 }
