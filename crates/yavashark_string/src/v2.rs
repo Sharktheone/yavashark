bc97139ea5191b940fa322d8320a5193121b6c06
Shark <sharktheone@proton.me>
2026-05-27 23:56:26 +0200

    create new YSString

--- crates/yavashark_string/src/v2.rs
+++ crates/yavashark_string/src/v2.rs
@@ -18,6 +18,66 @@ enum Inner {
     Rope(RopeString),
 }
 
+
+impl From<HeapString> for YSString {
+    fn from(heap: HeapString) -> Self {
+        Self::from_inner(Inner::Heap(heap))
+    }
+}
+
+impl From<InlineAscii> for YSString {
+    fn from(inline: InlineAscii) -> Self {
+        Self::from_inner(Inner::InlineAscii(inline))
+    }
+}
+
+
+impl From<InlineWtf16> for YSString {
+    fn from(inline: InlineWtf16) -> Self {
+        Self::from_inner(Inner::InlineWtf16(inline))
+    }
+}
+
+impl From<RopeString> for YSString {
+    fn from(rope: RopeString) -> Self {
+        Self::from_inner(Inner::Rope(rope))
+    }
+}
+
+
+
+impl YSString {
+    fn from_inner(inner: Inner) -> Self {
+        Self {
+            inner: UnsafeCell::new(inner),
+        }
+    }
+    
+    pub fn from_static_ascii(s: &'static str) -> Self {
+        HeapString::from_static_ascii(s).into()
+    }
+    
+    pub fn from_static_wtf16(s: &'static [u16]) -> Self {
+        HeapString::from_static_wtf16(s).into()
+    }
+    
+    pub fn from_ascii(s: &str) -> Self {
+        if let Some(inline) = InlineAscii::try_from_str(s) {
+            inline.into()
+        } else {
+            HeapString::from_str(s).into()
+        }
+    }
+
+    pub fn from_wtf16(s: &[u16]) -> Self {
+        if let Some(inline) = InlineWtf16::try_from_slice(s) {
+            inline.into()
+        } else {
+            HeapString::from_wtf16(s).into()
+        }
+    }
+}
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum Type {
     Ascii,
