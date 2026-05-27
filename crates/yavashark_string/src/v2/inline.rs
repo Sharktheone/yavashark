805b02d6cabc713bb409fd2842bf731276f82e21
Shark <sharktheone@proton.me>
2026-05-27 23:47:58 +0200

    inline len conversion

--- crates/yavashark_string/src/v2/inline.rs
+++ crates/yavashark_string/src/v2/inline.rs
@@ -53,3 +53,64 @@ pub enum InlineLenWtf {
     Len10,
     Len11,
 }
+
+
+impl InlineLen {
+    pub fn from_usize(len: usize) -> Option<Self> {
+        match len {
+            0 => Some(Self::Empty),
+            1 => Some(Self::Len1),
+            2 => Some(Self::Len2),
+            3 => Some(Self::Len3),
+            4 => Some(Self::Len4),
+            5 => Some(Self::Len5),
+            6 => Some(Self::Len6),
+            7 => Some(Self::Len7),
+            8 => Some(Self::Len8),
+            9 => Some(Self::Len9),
+            10 => Some(Self::Len10),
+            11 => Some(Self::Len11),
+            12 => Some(Self::Len12),
+            13 => Some(Self::Len13),
+            14 => Some(Self::Len14),
+            15 => Some(Self::Len15),
+            16 => Some(Self::Len16),
+            17 => Some(Self::Len17),
+            18 => Some(Self::Len18),
+            19 => Some(Self::Len19),
+            20 => Some(Self::Len20),
+            21 => Some(Self::Len21),
+            22 => Some(Self::Len22),
+            23 => Some(Self::Len23),
+            _ => None,
+        }
+    }
+
+    pub fn to_usize(self) -> usize {
+        self as usize
+    }
+}
+
+impl InlineLenWtf {
+    pub fn from_usize(len: usize) -> Option<Self> {
+        match len {
+            0 => Some(Self::Empty),
+            1 => Some(Self::Len1),
+            2 => Some(Self::Len2),
+            3 => Some(Self::Len3),
+            4 => Some(Self::Len4),
+            5 => Some(Self::Len5),
+            6 => Some(Self::Len6),
+            7 => Some(Self::Len7),
+            8 => Some(Self::Len8),
+            9 => Some(Self::Len9),
+            10 => Some(Self::Len10),
+            11 => Some(Self::Len11),
+            _ => None,
+        }
+    }
+
+    pub fn to_usize(self) -> usize {
+        self as usize
+    }
+}
