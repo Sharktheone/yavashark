2e8f8d69b420566a7639931556729b0fd1d4ef60
Shark <sharktheone@proton.me>
2026-05-28 18:27:34 +0200

    rope string slice

--- crates/yavashark_string/src/v2/rope.rs
+++ crates/yavashark_string/src/v2/rope.rs
@@ -11,3 +11,20 @@ struct RopeStringInner {
     b: YSString,
 }
 
+impl RopeString {
+    pub fn len(&self) -> u32 {
+        self.to - self.from
+    }
+
+    pub fn slice(mut self, start: u32, end: u32) -> Option<Self> {
+        if start > end || end > self.len() {
+            return None;
+        }
+
+        self.from += start;
+        self.to = self.from + (end - start);
+
+
+        Some(self)
+    }
+}
