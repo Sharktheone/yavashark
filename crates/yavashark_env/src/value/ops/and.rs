d07fab464e864041e6fa3c55209d86fee8d3a88d
Shark <sharktheone@proton.me>
2026-07-06 23:39:44 +0200

    add fast-paths to and

--- crates/yavashark_env/src/value/ops/and.rs
+++ crates/yavashark_env/src/value/ops/and.rs
@@ -1,3 +1,4 @@
+use num_traits::Zero;
 use crate::Realm;
 use crate::error::Error;
 use crate::value::Value;
@@ -5,6 +6,16 @@ use crate::value::ops::BigIntOrNumber;
 
 impl Value {
     pub fn and(&self, other: &Self, realm: &mut Realm) -> Result<Self, Error> {
+        match (self, other) {
+            (Self::Number(left), Self::Number(right)) => {
+                Self::from(left as i64 & right as i64)
+            }
+            (Self::BigInt(left), Self::BigInt(right)) => {
+                Self::from((&*left) & (&*right))
+            }
+            _ => {}
+        }
+
         //TODO: maybe in the future we could make this more performant by just matching against both types (just like the old Add trait), but this is what the spec says
         let left_num = self.to_numeric(realm)?;
         let right_num = other.to_numeric(realm)?;
