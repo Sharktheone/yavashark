3b9d37a7100e21e8369d9fbe80a61b3442032786
Shark <sharktheone@proton.me>
2026-07-07 22:51:51 +0200

    fast path for exp

--- crates/yavashark_env/src/value/ops/exp.rs
+++ crates/yavashark_env/src/value/ops/exp.rs
@@ -6,6 +6,20 @@ use num_traits::ToPrimitive;
 
 impl Value {
     pub fn exp(&self, other: &Self, realm: &mut Realm) -> Result<Self, Error> {
+        match (self, other) {
+            (Self::Number(left), Self::Number(right)) => {
+                return Ok(left.powf(*right).into())
+            }
+            (Self::BigInt(left), Self::BigInt(right)) => {
+                let Some(right) = right.to_u32() else {
+                    return Err(Error::range("exponent too large"));
+                };
+
+                return Ok(left.pow(right).into())
+            }
+            _ => {}
+        }
+
         //TODO: maybe in the future we could make this more performant by just matching against both types (just like the old Add trait), but this is what the spec says
         let left = self.to_primitive(Hint::None, realm)?;
         let right = other.to_primitive(Hint::None, realm)?;
