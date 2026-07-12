5e0d5ec628a8598806d8ca379048b4d68baee25f
Shark <sharktheone@proton.me>
2026-07-12 20:00:17 +0200

    fix exp behavior for strings

--- crates/yavashark_env/src/value/ops/exp.rs
+++ crates/yavashark_env/src/value/ops/exp.rs
@@ -1,7 +1,7 @@
 use crate::Realm;
 use crate::error::Error;
 use crate::value::ops::BigIntOrNumber;
-use crate::value::{Hint, Value};
+use crate::value::Value;
 use num_traits::ToPrimitive;
 
 impl Value {
@@ -18,32 +18,22 @@ impl Value {
             _ => {}
         }
 
-        //TODO: maybe in the future we could make this more performant by just matching against both types (just like the old Add trait), but this is what the spec says
-        let left = self.to_primitive(Hint::None, realm)?;
-        let right = other.to_primitive(Hint::None, realm)?;
+        let left_num = self.to_numeric(realm)?;
+        let right_num = other.to_numeric(realm)?;
 
-        Ok(if left.is_string() || right.is_string() {
-            let left_str = left.into_string(realm)?;
-            let right_str = right.into_string(realm)?;
-            Self::from(left_str + &right_str)
-        } else {
-            let left_num = left.to_numeric(realm)?;
-            let right_num = right.to_numeric(realm)?;
-
-            match (left_num, right_num) {
-                (BigIntOrNumber::Number(left), BigIntOrNumber::Number(right)) => {
-                    Self::from(left.powf(right))
-                }
-                (BigIntOrNumber::BigInt(left), BigIntOrNumber::BigInt(right)) => {
-                    let Some(right) = right.to_u32() else {
-                        return Err(Error::range("exponent too large"));
-                    };
-
-                    Self::from(left.pow(right))
-                }
-
-                _ => return Err(Error::ty("cannot mix BigInt and Number")),
+        Ok(match (left_num, right_num) {
+            (BigIntOrNumber::Number(left), BigIntOrNumber::Number(right)) => {
+                Self::from(left.powf(right))
             }
+            (BigIntOrNumber::BigInt(left), BigIntOrNumber::BigInt(right)) => {
+                let Some(right) = right.to_u32() else {
+                    return Err(Error::range("exponent too large"));
+                };
+
+                Self::from(left.pow(right))
+            }
+
+            _ => return Err(Error::ty("cannot mix BigInt and Number")),
         })
     }
 }
