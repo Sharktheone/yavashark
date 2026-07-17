683816ae70ddce188d16a95f37c63125f43f93d0
Shark <sharktheone@proton.me>
2026-07-17 23:04:15 +0200

    scaffold rc string types

--- crates/yavashark_string/src/rc_string.rs
+++ crates/yavashark_string/src/rc_string.rs
@@ -1,8 +1,24 @@
+use std::alloc::Layout;
+use std::marker::PhantomData;
+use std::ptr::NonNull;
 
-
-
-struct RcStringBoxHeader {
+#[repr(C)]
+struct Header {
     count: usize,
     capacity: u32,
     init_to: u32,
+}
+
+
+struct RcAsciiString {
+    header: NonNull<Header>,
+    len: u32,
+    phantom: PhantomData<[u8]>,
+}
+
+
+struct RcWtf16String {
+    header: NonNull<Header>,
+    len: u32,
+    phantom: PhantomData<[u16]>,
 }
