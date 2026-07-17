d3dbd7c03733f8327c60f4cb01a273e649bd007c
Shark <sharktheone@proton.me>
2026-07-17 23:05:18 +0200

    alloc rc box / header

--- crates/yavashark_string/src/rc_string.rs
+++ crates/yavashark_string/src/rc_string.rs
@@ -22,3 +22,43 @@ struct RcWtf16String {
     len: u32,
     phantom: PhantomData<[u16]>,
 }
+
+
+
+
+impl Header {
+    unsafe fn alloc<T>(capacity: u32) -> NonNull<Self> {
+        #[allow(clippy::expect_used)]
+        let layout = Layout::new::<Self>()
+            .extend(Layout::array::<T>(capacity as usize).expect("layout failed"))
+            .expect("layout failed")
+            .0
+            .pad_to_align();
+
+
+        #[allow(clippy::cast_ptr_alignment)]
+        let ptr = unsafe { std::alloc::alloc(layout).cast::<Self>() };
+
+        let Some(ptr) = NonNull::new(ptr) else {
+            std::alloc::handle_alloc_error(layout);
+        };
+
+        unsafe {
+            std::ptr::write(ptr.as_ptr(), Self {
+                count: 1,
+                capacity,
+                init_to: 0,
+            });
+        }
+
+        ptr
+    }
+
+    fn alloc_u8(capacity: u32) -> NonNull<Self> {
+        unsafe { Self::alloc::<u8>(capacity) }
+    }
+
+    fn alloc_u16(capacity: u32) -> NonNull<Self> {
+        unsafe { Self::alloc::<u16>(capacity) }
+    }
+}
