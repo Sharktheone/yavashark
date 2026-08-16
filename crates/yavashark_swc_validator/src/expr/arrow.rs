a935364211798bc1dc0d8e7d6942d0786a248fbe
Shark <sharktheone@proton.me>
2026-08-16 20:00:42 +0200

    validator arrow function

--- crates/yavashark_swc_validator/src/expr/arrow.rs
+++ crates/yavashark_swc_validator/src/expr/arrow.rs
@@ -1,13 +1,13 @@
 use crate::Validator;
-use crate::utils::block_has_use_strict;
-use swc_ecma_ast::{ArrowExpr, BlockStmtOrExpr};
+use crate::utils::{block_has_use_strict, statements_have_use_strict};
+use swc_ecma_ast::{ArrowExpr, ArrowFunctionBody, BlockStmtOrExpr};
 
 impl<'a> Validator<'a> {
     pub fn validate_arrow_expr(&mut self, arrow: &'a ArrowExpr) -> Result<(), String> {
         let scope = self.enter_function_context(arrow.is_async, false);
 
-        if let BlockStmtOrExpr::BlockStmt(block) = &*arrow.body
-            && block_has_use_strict(block)
+        if let ArrowFunctionBody::FunctionBody(block) = &*arrow.body
+            && statements_have_use_strict(&block.stmts)
         {
             self.set_current_function_strict();
         }
@@ -29,8 +29,8 @@ impl<'a> Validator<'a> {
         }
 
         let res = match &*arrow.body {
-            BlockStmtOrExpr::BlockStmt(block) => self.validate_block_with_shadow(block, false),
-            BlockStmtOrExpr::Expr(expr) => self.validate_expr(expr),
+            ArrowFunctionBody::FunctionBody(block) => self.validate_block_with_shadow(&block.stmts, false),
+            ArrowFunctionBody::Expr(expr) => self.validate_expr(expr),
         };
 
         scope.exit(self);
