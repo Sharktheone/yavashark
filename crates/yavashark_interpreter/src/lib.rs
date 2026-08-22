7f3eaa70e2fa62e42a7b3eb3e26a19539cb6cfc2
Shark <sharktheone@proton.me>
2026-08-22 22:29:36 +0200

    allow to supply a slice and not a full vec for Interpreter::run_test

--- crates/yavashark_interpreter/src/lib.rs
+++ crates/yavashark_interpreter/src/lib.rs
@@ -94,7 +94,7 @@ impl Interpreter {
 
     #[cfg(test)]
     #[allow(clippy::missing_panics_doc)]
-    pub fn run_test(script: &Vec<Stmt>) -> (ValueResult, Rc<RefCell<yavashark_env::tests::State>>) {
+    pub fn run_test(script: &[Stmt]) -> (ValueResult, Rc<RefCell<yavashark_env::tests::State>>) {
         let mut context = &mut Realm::new().unwrap();
         #[cfg(feature = "vm")]
         yavashark_vm::init(context).unwrap();
