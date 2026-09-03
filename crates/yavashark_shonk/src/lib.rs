25c150ce6b0bc7506cc0d30b10df345a51c4a325
Shark <sharktheone@proton.me>
2026-09-03 23:16:08 +0200

    add yavashark shonk for testing

---
+++ crates/yavashark_shonk/src/lib.rs
@@ -0,0 +1,10 @@
+
+// Testing ground for new YS to improve perf!
+
+// We need to test:
+// - new Object API with shapes
+// - NaN boxing model
+// - Realm and Scope APIs
+// - Ability for async Scopes
+// - How we handle escaping variables
+// - Small micro benches to compare against different engines and current Yavashark
