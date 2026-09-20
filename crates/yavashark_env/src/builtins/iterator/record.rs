851003329db3d3260bc91db178ec285f9b3fb0ce
Shark <sharktheone@proton.me>
2026-09-20 22:02:03 +0200

    iterator next method

--- crates/yavashark_env/src/builtins/iterator/record.rs
+++ crates/yavashark_env/src/builtins/iterator/record.rs
@@ -12,14 +12,14 @@ pub struct IteratorRecord {
     /// The iterator object
     pub iterator: ObjectHandle,
     /// Cached next method (avoids repeated property lookup)
-    pub next_method: ObjectHandle,
+    pub next_method: Value,
 }
 
 impl IteratorRecord {
     /// 7.4.2 GetIteratorDirect ( obj )
     pub fn new(iterator: ObjectHandle, realm: &mut Realm) -> Res<Self> {
         // 1. Let nextMethod be ? Get(obj, "next").
-        let next_method = iterator.get("next", realm)?.to_object()?;
+        let next_method = iterator.get("next", realm)?;
         // 2. Let iteratorRecord be the Iterator Record { [[Iterator]]: obj, [[NextMethod]]: nextMethod, [[Done]]: false }.
         Ok(Self {
             iterator,
@@ -31,7 +31,7 @@ impl IteratorRecord {
     pub fn from_parts(iterator: ObjectHandle, next_method: ObjectHandle) -> Self {
         Self {
             iterator,
-            next_method,
+            next_method: next_method.into(),
         }
     }
 
@@ -41,7 +41,7 @@ impl IteratorRecord {
         // 1. Let result be ? Call(iteratorRecord.[[NextMethod]], iteratorRecord.[[Iterator]]).
         let result = self
             .next_method
-            .call(vec![], self.iterator.clone().into(), realm)?;
+            .call(realm, vec![], self.iterator.clone().into())?;
 
         // 2. If Type(result) is not Object, throw a TypeError exception.
         let result_obj = result.to_object()?;
@@ -69,9 +69,10 @@ impl IteratorRecord {
         let return_method = self.iterator.get("return", realm)?;
 
         // 4. If innerResult.[[Value]] is not undefined, then
-        if return_method.is_callable() {
-            // a. Set innerResult to Completion(Call(innerResult.[[Value]], iterator)).
-            return_method.call(realm, vec![], self.iterator.clone().into())?;
+        if !return_method.is_undefined() {
+            return_method
+                .call(realm, vec![], self.iterator.clone().into())?
+                .to_object()?;
         }
 
         Ok(())
