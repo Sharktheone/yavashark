f30c549124bfee34a2a29ba28508c5acf54988e5
Shark <sharktheone@proton.me>
2026-09-23 23:14:32 +0200

    close iterator in error paths in Iterator.some

--- crates/yavashark_env/src/builtins/iterator.rs
+++ crates/yavashark_env/src/builtins/iterator.rs
@@ -557,6 +557,7 @@ impl Iterator {
 
         // 2. If IsCallable(predicate) is false, throw a TypeError exception.
         if !predicate.is_callable() {
+            let _ = close_iterator_object(&o, realm);
             return Err(Error::ty("predicate is not callable"));
         }
         let predicate = predicate.to_object()?;
@@ -594,7 +595,7 @@ impl Iterator {
 
             // e. If ToBoolean(result) is true, return ? IteratorClose(iterated, NormalCompletion(true)).
             if result.is_truthy() {
-                let _ = iterated.close(realm);
+                iterated.close(realm)?;
                 return Ok(true);
             }
 
